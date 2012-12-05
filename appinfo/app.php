cc0ff8611fd75c0efb487876c0ca4eb704018f9b
Claus-Justus Heine <himself@claus-justus-heine.de>
2012-12-05 07:41:02 +0000

    Start converting to classes (with static methods, just to encapsulate the stuff)

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,5 +1,10 @@
 <?php
 
+OC::$CLASSPATH['CAFEV_DB_Config'] = 'apps/cafevdb/lib/config.php.inc';
+OC::$CLASSPATH['CAFEV_DB_Util'] = 'apps/cafevdb/lib/functions.php.inc';
+OC::$CLASSPATH['CAFEV_DB_Instruments'] = 'apps/cafevdb/lib/Instruments.php';
+OC::$CLASSPATH['CAFEV_DB_Projects'] = 'apps/cafevdb/lib/Projekte.php';
+
 OCP\App::registerAdmin( 'cafevdb', 'settings' );
 
 OCP\App::addNavigationEntry( array( 
--- lib/Instruments.php
+++ lib/Instruments.php
@@ -1,176 +1,178 @@
 <?php
 
-require_once('functions.php.inc');
-
-// Sort the given list of instruments according to orchestral ordering
-// as defined in the Instrumente table.
-function sortInstrumentsOrchestral($list, $handle)
+class CAFEV_DB_Instruments
 {
-  $query = 'SELECT `Instrument`,`Sortierung` FROM `Instrumente` WHERE  1 ORDER BY `Sortierung` ASC';
-  $result = CAFEVmyquery($query, $handle);
+
+  // Sort the given list of instruments according to orchestral ordering
+  // as defined in the Instrumente table.
+  public static function sortInstrumentsOrchestral($list, $handle)
+  {
+    $query = 'SELECT `Instrument`,`Sortierung` FROM `Instrumente` WHERE  1 ORDER BY `Sortierung` ASC';
+    $result = CAFEV_DB_Util::myquery($query, $handle);
   
-  $final = array();
-  while ($line = CAFEVmyfetch($result)) {
-    $tblInst = $line['Instrument'];
-    if (array_search($tblInst, $list) !== false) {
-      $final[] = $tblInst;
+    $final = array();
+    while ($line = CAFEV_DB_Util::myfetch($result)) {
+      $tblInst = $line['Instrument'];
+      if (array_search($tblInst, $list) !== false) {
+        $final[] = $tblInst;
+      }
     }
+
+    return $final;
   }
 
-  return $final;
-}
+  // Fetch the instruments required for a specific project
+  public static function fetchProjectInstruments($ProjektId, $handle) {
 
-// Fetch the instruments required for a specific project
-function fetchProjectInstruments($ProjektId, $handle) {
+    $query = 'SELECT `Besetzung` FROM `Projekte` WHERE `Id` = '.$ProjektId;
+    $result = CAFEV_DB_Util::myquery($query);
 
-  $query = 'SELECT `Besetzung` FROM `Projekte` WHERE `Id` = '.$ProjektId;
-  $result = CAFEVmyquery($query);
-
-  // Ok there should be only one row
-  if (!($line = CAFEVmyfetch($result))) {
-    CAFEVerror("Could not fetch instruments for project", true);
-  }
-  $ProjInsts = explode(',',$line['Besetzung']);
-
-  // Now sort it in "natural" order
-  return sortInstrumentsOrchestral($ProjInsts, $handle);
-}
-
-// Fetch the project-instruments of the project musicians, possibly to
-// do some sanity checks with the project's instrumentation, or simply
-// to add all instruments to the projects instrumentation list.
-function fetchProjectMusiciansInstruments($ProjektId, $handle)
-{
-  $query = 'SELECT DISTINCT `Instrument` FROM `Besetzungen` WHERE `ProjektId` = '.$ProjektId;
-  $result = CAFEVmyquery($query);
-  
-  $instruments = array();
-  while ($line = CAFEVmyfetch($result)) {
-    $instruments[] = $line['Instrument'];
-  }
-
-  // Now sort it in "natural" order
-  return sortInstrumentsOrchestral($instruments, $handle);
-}
-
-// Fetch all instruments of the musicians subscribed to the project
-// and add them to the instrumentation. If $replace is true, then
-// remove the old instrumentation, otherwise the new instrumentation
-// is the union of the old instrumentation and the instruments
-// actually subscribed to the project.
-function updateProjectInstrumentationFromMusicians($ProjektId, $handle, $replace = false)
-{
-  $musinst = fetchProjectMusiciansInstruments($ProjektId, $handle);
-  if ($replace) {
-    $prjinst = $musinst;
-  } else {
-    $prjinst = fetchProjectInstruments($ProjektId, $handle);
-  }
-  $prjinst = array_unique(array_merge($musinst, $prjinst));
-  $prjinst = sortInstrumentsOrchestral($prjinst, $handle);
-
-  $query = "UPDATE `Projekte` SET `Besetzung`='".implode(',',$prjinst)."' WHERE `Id` = $ProjektId";
-  CAFEVmyquery($query, $handle);
-  //CAFEVerror($query, false);
-  
-  return $prjinst;
-}
-
-// Fetch the instruments and sort them according to Instruments.Sortierung
-function fetchInstruments($handle) {
-
-  $Instruments = sql_multikeys('Musiker', 'Instrumente', $handle);
-
-  $query = 'SELECT `Instrument`,`Sortierung` FROM `Instrumente` WHERE  1 ORDER BY `Sortierung` ASC';
-  $result = CAFEVmyquery($query, $handle);
-  
-  $final = array();
-  while ($line = CAFEVmyfetch($result)) {
-    //CAFEVerror("huh".$line['Instrument'],false);
-    $tblInst = $line['Instrument'];
-    if (array_search($tblInst, $Instruments) === false) {
-      CAFEVerror('"'.$tblInst.'" not found in '.implode(',',$Instruments), true);
+    // Ok there should be only one row
+    if (!($line = CAFEV_DB_Util::myfetch($result))) {
+      CAFEVerror("Could not fetch instruments for project", true);
     }
-    array_push($final, $tblInst);
+    $ProjInsts = explode(',',$line['Besetzung']);
+
+    // Now sort it in "natural" order
+    return sortInstrumentsOrchestral($ProjInsts, $handle);
   }
 
-  return $final;
-}
+  // Fetch the project-instruments of the project musicians, possibly to
+  // do some sanity checks with the project's instrumentation, or simply
+  // to add all instruments to the projects instrumentation list.
+  public static function fetchProjectMusiciansInstruments($ProjektId, $handle)
+  {
+    $query = 'SELECT DISTINCT `Instrument` FROM `Besetzungen` WHERE `ProjektId` = '.$ProjektId;
+    $result = CAFEV_DB_Util::myquery($query);
+  
+    $instruments = array();
+    while ($line = CAFEV_DB_Util::myfetch($result)) {
+      $instruments[] = $line['Instrument'];
+    }
 
-// Check for consistency
-function checkInstruments($handle) {
+    // Now sort it in "natural" order
+    return sortInstrumentsOrchestral($instruments, $handle);
+  }
 
-  $InstrumentsSet  = sql_multikeys('Musiker', 'Instrumente', $handle);
-  $InstrumentsEnum = sql_multikeys('Besetzungen', 'Instrument', $handle);
+  // Fetch all instruments of the musicians subscribed to the project
+  // and add them to the instrumentation. If $replace is true, then
+  // remove the old instrumentation, otherwise the new instrumentation
+  // is the union of the old instrumentation and the instruments
+  // actually subscribed to the project.
+  public static function updateProjectInstrumentationFromMusicians($ProjektId, $handle, $replace = false)
+  {
+    $musinst = fetchProjectMusiciansInstruments($ProjektId, $handle);
+    if ($replace) {
+      $prjinst = $musinst;
+    } else {
+      $prjinst = fetchProjectInstruments($ProjektId, $handle);
+    }
+    $prjinst = array_unique(array_merge($musinst, $prjinst));
+    $prjinst = sortInstrumentsOrchestral($prjinst, $handle);
+
+    $query = "UPDATE `Projekte` SET `Besetzung`='".implode(',',$prjinst)."' WHERE `Id` = $ProjektId";
+    CAFEV_DB_Util::myquery($query, $handle);
+    //CAFEVerror($query, false);
+  
+    return $prjinst;
+  }
+
+  // Fetch the instruments and sort them according to Instruments.Sortierung
+  public static function fetchInstruments($handle) {
+
+    $Instruments = CAFEV_DB_Util::sqlMultikeys('Musiker', 'Instrumente', $handle);
+
+    $query = 'SELECT `Instrument`,`Sortierung` FROM `Instrumente` WHERE  1 ORDER BY `Sortierung` ASC';
+    $result = CAFEV_DB_Util::myquery($query, $handle);
+  
+    $final = array();
+    while ($line = CAFEV_DB_Util::myfetch($result)) {
+      //CAFEVerror("huh".$line['Instrument'],false);
+      $tblInst = $line['Instrument'];
+      if (array_search($tblInst, $Instruments) === false) {
+        CAFEVerror('"'.$tblInst.'" not found in '.implode(',',$Instruments), true);
+      }
+      array_push($final, $tblInst);
+    }
+
+    return $final;
+  }
+
+  // Check for consistency
+  public static function checkInstruments($handle) {
+
+    $InstrumentsSet  = CAFEV_DB_Util::sqlMultikeys('Musiker', 'Instrumente', $handle);
+    $InstrumentsEnum = CAFEV_DB_Util::sqlMultikeys('Besetzungen', 'Instrument', $handle);
 
   
-  sort($InstrumentsSet);
-  sort($InstrumentsEnum);
+    sort($InstrumentsSet);
+    sort($InstrumentsEnum);
 
-  $cnt1 = count($InstrumentsSet);
-  $cnt2 = count($InstrumentsEnum);
+    $cnt1 = count($InstrumentsSet);
+    $cnt2 = count($InstrumentsEnum);
 
-  if ($cnt1 != $cnt2) {
-    echo "<P><HR/>
+    if ($cnt1 != $cnt2) {
+      echo "<P><HR/>
 <H4>Anzahl der Instrumente in \"Musiker\" und \"Besetzungen\" stimmen nicht &uuml;berein!</H4>
 <HR/><P>
 ";
-  }
+    }
 
-  $cntmax = max($cnt1,$cnt2);
-  $cntmin = min($cnt1,$cnt2);
+    $cntmax = max($cnt1,$cnt2);
+    $cntmin = min($cnt1,$cnt2);
 
-  for ($i = 0; $i < $cntmin; $i++) {
-    if ($InstrumentsSet[$i] != $InstrumentsEnum[$i]) {
-      echo "<P><HR/>
+    for ($i = 0; $i < $cntmin; $i++) {
+      if ($InstrumentsSet[$i] != $InstrumentsEnum[$i]) {
+        echo "<P><HR/>
 <H4>Instrumente in \"Musiker\" und \"Besetzungen\" stimmen nicht &uuml;berein: \""
-        . $InstrumentsSet[$i] . "\" != \"" . $InstrumentsEnum[$i] . "\"!</H4>
+          . $InstrumentsSet[$i] . "\" != \"" . $InstrumentsEnum[$i] . "\"!</H4>
 <HR/><P>
 ";
+      }
     }
-  }
-  $excess = $cnt1 < $cnt2 ? $InstrumentsEnum : $InstrumentsSet;
-  echo "<P><HR/>
+    $excess = $cnt1 < $cnt2 ? $InstrumentsEnum : $InstrumentsSet;
+    echo "<P><HR/>
 <H4>&Uuml;bersch&uuml;ssige Instrumente: ";
-  for (; $i < $cntmax; $i++) {
-    echo $excess[$i] . " ";
-  }
-  echo "<HR/><P>\n";
-}
-
-// Make sure the Instrumente table as all instruments used in the Musiker
-// table. Delete everything else.
-function sanitizeInstrumentsTable($handle) {
-
-  $Instrumente = sql_multikeys('Musiker', 'Instrumente', $handle);
-
-  foreach ($Instrumente as $value) {
-    $query = "INSERT IGNORE INTO `Instrumente` (`Instrument`) VALUES ('$value')";
-    $result = CAFEVmyquery($query, $handle);
+    for (; $i < $cntmax; $i++) {
+      echo $excess[$i] . " ";
+    }
+    echo "<HR/><P>\n";
   }
 
-  // Now the table contains at least all instruments, now remove excess elements.
+  // Make sure the Instrumente table as all instruments used in the Musiker
+  // table. Delete everything else.
+  public static function sanitizeInstrumentsTable($handle) {
+
+    $Instrumente = CAFEV_DB_Util::sqlMultikeys('Musiker', 'Instrumente', $handle);
+
+    foreach ($Instrumente as $value) {
+      $query = "INSERT IGNORE INTO `Instrumente` (`Instrument`) VALUES ('$value')";
+      $result = CAFEV_DB_Util::myquery($query, $handle);
+    }
+
+    // Now the table contains at least all instruments, now remove excess elements.
 
 
-  // Build SQL Query  
-  $query = "SELECT `Instrument` FROM `Instrumente` WHERE 1";
+    // Build SQL Query  
+    $query = "SELECT `Instrument` FROM `Instrumente` WHERE 1";
 
-  // Fetch the result or die
-  $result = CAFEVmyquery($query, $query);
+    // Fetch the result or die
+    $result = CAFEV_DB_Util::myquery($query, $query);
 
-  $dropList = array();
-  while ($line = CAFEVmyfetch($result)) {
-    $tblInst = $line['Instrument'];
-    if (array_search($tblInst, $Instrumente) === false) {
-      $dropList[$tblInst] = true;
+    $dropList = array();
+    while ($line = CAFEV_DB_Util::myfetch($result)) {
+      $tblInst = $line['Instrument'];
+      if (array_search($tblInst, $Instrumente) === false) {
+        $dropList[$tblInst] = true;
+      }
+    }
+
+    foreach ($dropList as $key => $value) {
+      $query = "DELETE FROM `Instrumente` WHERE `Instrument` = '$key'";
+      $result = CAFEV_DB_Util::myquery($query);
     }
   }
 
-  foreach ($dropList as $key => $value) {
-    $query = "DELETE FROM `Instrumente` WHERE `Instrument` = '$key'";
-    $result = CAFEVmyquery($query);
-  }
-}
-
+};
 
 ?>
