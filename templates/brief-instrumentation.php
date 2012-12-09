95537976e497df4b8d81f34d5da4da4af05d9446
Claus-Justus Heine <himself@claus-justus-heine.de>
2012-12-09 23:04:50 +0000

    Restructure.

--- templates/brief-instrumentation.php
+++ templates/brief-instrumentation.php
@@ -4,7 +4,12 @@
     <input type="hidden" name="Action" value="-1"/>
     <input type="hidden" name="Template" value="projects"/>
   </form>
+  <form name="cafevdb-form-projects" method="post">
+    <input type="submit" name="" value="View all Projects"/>
+    <input type="hidden" name="Action" value="DetailedInstrumentation"/>
+    <input type="hidden" name="Template" value="detailed-instrumentation"/>
+  </form>
 </div>
 <div class="cafevdb-general">
-  <?php CAFEVDB\Instrumentation::display(); ?>
+  <?php CAFEVDB\BriefInstrumentation::display(); ?>
 </div>
