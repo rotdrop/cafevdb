4ce2047b96186b5728c36149d9ea10be030870c3
Claus-Justus Heine <himself@claus-justus-heine.de>
2012-12-11 18:24:43 +0000

    Cosmetics.

--- templates/settings.php
+++ templates/settings.php
@@ -1,6 +1,6 @@
 <form id="cafevdb">
 	<fieldset class="personalblock">
-		<strong>App Template</strong><br />
+		<strong>Camerata DB</strong><br />
 		<input type="text" name="somesetting" id="somesetting" value="<?php echo $_['url']; ?>" placeholder="<?php echo $l->t('Some Setting');?>" />
 		<br />
 		<span class="msg"></span>
