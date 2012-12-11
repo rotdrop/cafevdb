c5df62d75679b3eb018178946741661b2719b987
Claus-Justus Heine <himself@claus-justus-heine.de>
2012-12-11 18:25:55 +0000

    Personal setting, initial.

---
+++ templates/personal-settings.php
@@ -0,0 +1,8 @@
+<form id="cafevdb">
+	<fieldset class="personalblock">
+		<strong>Camerata DB</strong><br />
+		<input type="text" name="somesetting" id="somesetting" value="<?php echo $_['url']; ?>" placeholder="<?php echo $l->t('Some Setting');?>" />
+		<br />
+		<span class="msg"></span>
+	</fieldset>
+</form>
