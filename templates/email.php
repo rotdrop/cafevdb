80f2d1b8fabbb547962ca76f2fd9b5d634e5b30a
Claus-Justus Heine <himself@claus-justus-heine.de>
2012-12-12 23:39:54 +0000

    Use translations.

--- templates/email.php
+++ templates/email.php
@@ -1,20 +1,16 @@
 <div id="controls">
 <?php
+CAFEVDB\Navigation::setTranslation($l);
 $project = CAFEVDB\Util::cgiValue('Project');
 $projectId = CAFEVDB\Util::cgiValue('ProjectId');
 if ($projectId >= 0) {
   echo CAFEVDB\Navigation::button('projectlabel', $project, $projectId);
   echo CAFEVDB\Navigation::button('projects');
   echo CAFEVDB\Navigation::button('emailhistory', $project, $projectId);
-  //echo CAFEVDB\Navigation::button('brief', $project, $projectId);
   echo CAFEVDB\Navigation::button('detailed', $project, $projectId);
-  //echo CAFEVDB\Navigation::button('projectinstruments', $project, $projectId);
-  //echo CAFEVDB\Navigation::button('instruments', $project, $projectId);
 } else {
   echo CAFEVDB\Navigation::button('projects');
   echo CAFEVDB\Navigation::button('emailhistory');
-  //echo CAFEVDB\Navigation::button('projectinstruments');
-  //echo CAFEVDB\Navigation::button('instruments');
   echo CAFEVDB\Navigation::button('all');
 }
 ?>
