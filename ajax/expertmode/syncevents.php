e15b0c635f7a8eca8364d81bdc2f7e94a705aec6
Claus-Justus Heine <himself@claus-justus-heine.de>
2012-12-23 12:46:09 +0000

    Re-attach all events to their respective projects; meant for the case where the ProjectEvents table got somehow out of sync or is damaged. TODO: if the project-events table does not exist, then we should create it on the server.

---
+++ ajax/expertmode/syncevents.php
@@ -0,0 +1,64 @@
+<?php
+
+// Init owncloud
+
+$l = OC_L10N::get('cafevdb');
+
+// Check if we are a user
+OCP\JSON::checkLoggedIn();
+OCP\JSON::checkAppEnabled('cafevdb');
+OCP\JSON::checkAppEnabled('calendar');
+
+// Loop over all calendars the calendar user shares with the orchestra
+// group.
+$calendaruser  = CAFEVDB\Config::getSetting('calendaruser', CAFEVDB\Config::getValue('dbuser'));
+$calendargroup = \OC_AppConfig::getValue('cafevdb', 'usergroup', '');
+$calendars     = OC_Calendar_Calendar::allCalendars($calendaruser);
+
+$dfltnames = array('concerts', 'rehearsals', 'other');
+$dfltcals = array();
+foreach ($dfltnames as $name) {
+  $dfltcals[] = CAFEVDB\Config::getSetting($name.'calendar', $l->t($name));
+}
+
+CAFEVDB\Config::init();
+$handle = CAFEVDB\mySQL::connect(CAFEVDB\Config::$pmeopts);
+
+foreach ($calendars as $calendar) {
+  $calId = $calendar['id'];
+  if (!OCP\Share::getItemSharedWithBySource('calendar', $calId)) {
+    if (in_array($calendar['displayname'], $dfltcals)) {
+      // force sharing
+      $olduser = $_SESSION['user_id'];
+      $_SESSION['user_id'] = $calendaruser;
+      try {
+        $token = OCP\Share::shareItem('calendar', $calId,
+                                      OCP\Share::SHARE_TYPE_GROUP,
+                                      $calendargroup,
+                                      OCP\Share::PERMISSION_CREATE|
+                                      OCP\Share::PERMISSION_READ|
+                                      OCP\Share::PERMISSION_UPDATE|
+                                      OCP\Share::PERMISSION_DELETE);
+      } catch (Exception $exception) {
+        $_SESSION['user_id'] = $olduser;
+        OC_JSON::error(array('data' => array('message' => $exception->getMessage())));
+        return;
+      }
+      $_SESSION['user_id'] = $olduser;
+    } else {
+      continue;
+    }
+  }
+
+  // Ok, now we should have access. Do the sync.
+  $events = \OC_Calendar_Object::all($calId);
+  foreach ($events as $event) {
+    CAFEVDB\Events::syncEvent($event['id'], $handle);
+  }
+}
+
+CAFEVDB\mySQL::close($handle);
+
+echo '<span class="bold">'.$l->t('Done.').'</span>';
+
+?>
