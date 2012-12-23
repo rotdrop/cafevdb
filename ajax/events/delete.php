167a03b71ebcd3a20e05d5dd0420b2a108339a38
Claus-Justus Heine <himself@claus-justus-heine.de>
2012-12-23 17:34:44 +0000

    Convert to a general purpose relisting utility.

--- ajax/events/delete.php
+++ ajax/events/delete.php
@@ -13,36 +13,60 @@ use CAFEVDB\L;
 $lang = \OC_L10N::findLanguage('cafevdb');
 $locale = $lang.'_'.strtoupper($lang).'.UTF-8';
 
-$projectId   = $_POST['ProjectId'];
-$projectName = $_POST['ProjectName'];
-$eventId     = $_POST['EventId'];
-$action      = $_POST['Action'];
+$projectId   = CAFEVDB\Util::cgiValue('ProjectId', -1);
+$projectName = CAFEVDB\Util::cgiValue('ProjectName', '');
 
-
-switch ($action) {
- case 'delete':
-   // Delete it. The kill-hook will clean up after us.
-   \OC_Calendar_Object::delete($eventId);
-   break;
- case 'detach':
-   // Keep the event in the calendar, but detach it
-   CAFEVDB\Events::unchain($projectId, $eventId);
-   break;
- default:
-   OCP\JSON::error(array('data' => array('message' => L::t('Invalid operation: ').$action)));
-   return false;
+if ($projectId < 0 ||
+    ($projectName == '' &&
+     ($projectName = CAFEVDB\Projects::fetchName($projectId)) == '')) {
+  die('<script type="text/javascript">document.location = oc_webroot;</script>');
 }
 
+$eventId = CAFEVDB\Util::cgiValue('EventId', -1);
+$action  = CAFEVDB\Util::cgiValue('Action', '');
+
 $events = CAFEVDB\Projects::events($projectId);
-
-// Remember the events selected for email transmission
 $emailEvents = CAFEVDB\Util::cgiValue('email-check', array());
+$selected = array(); // array marking selected events
 
-$selected = array();
-foreach ($emailEvents as $event) {
-  if ($event != $eventId) {
+switch ($action) {
+case 'delete':
+  // Delete it. The kill-hook will clean up after us.
+  \OC_Calendar_Object::delete($eventId);
+  foreach ($emailEvents as $event) {
+    if ($event != $eventId) {
+      $selected[$event] = true;
+    }
+  }
+  break;
+case 'detach':
+  // Keep the event in the calendar, but detach it
+  CAFEVDB\Events::unchain($projectId, $eventId);
+  foreach ($emailEvents as $event) {
+    if ($event != $eventId) {
+      $selected[$event] = true;
+    }
+  }
+  break;
+case 'relist':
+  // Just remember the events selected for email transmission
+  foreach ($emailEvents as $event) {
     $selected[$event] = true;
   }
+  break;
+case 'select':
+  // Mark all events as selected.
+  foreach ($events as $event) {
+    $selected[$event['calEventId']] = true;
+  }
+  break;
+case 'deselect':
+  // Just do nothing, leave all events unmarked.
+  break;
+default:
+  OCP\JSON::error(array('data' => array('contents' => '',
+                                        'debug' => L::t('Invalid operation: ').$action)));
+  return false;
 }
 
 // Now generate the html-fragment
@@ -58,7 +82,7 @@ $tmpl->assign('Selected', $selected);
 
 $html = $tmpl->fetchPage();
 
-OCP\JSON::success(array('data' => array('message' => $html,
+OCP\JSON::success(array('data' => array('contents' => $html,
                                         'debug' => $debugtext)));
 
 return true;
