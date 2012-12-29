ddcfb9f90b32e660daf8c56f62058aea1ceaccb2
Claus-Justus Heine <himself@claus-justus-heine.de>
2012-12-29 05:09:13 +0000

    Small fix.

--- templates/not-a-member.php
+++ templates/not-a-member.php
@@ -1,1 +1,2 @@
-<strong><?php echo L::t('Camerata DB: Error: Not a member of the dedicated orchestra group'); ?></strong>
+<?php use CAFEVDB\L; ?>
+<strong><?php echo L::t('Camerata DB: Error: Not a member of the dedicated orchestra group, you are ').'"'.\OCP\User::getUser().'"'; ?></strong>
