07713bbdc071838afc3b46800f39018bb336027f
Claus-Justus Heine <himself@claus-justus-heine.de>
2013-01-05 15:50:26 +0000

    Cleaner support for the collapsed header block.

--- js/pme-helper.js
+++ js/pme-helper.js
@@ -1,84 +1,62 @@
-/**Collapse the somewhat lengthy text at the head of the email page.
- */
-collapsePMEPageHeader = function()
-{
-  var box    = $('div[class$="-pme-header-box"]');
-  var header = $('div[class$="-pme-header"]');
-  var body   = $('form.pme-form');    
+CAFEVDB.PME = {
+  /**Collapse the somewhat lengthy text at the head of the PME-tables
+   */
+  collapsePageHeader: function() {
+    var pfx    = 'div.'+CAFEVDB.name+'-pme-';
+    var box    = $(pfx+'header-box');
+    var header = $(pfx+'header');
+    var body   = $(pfx+'body');
+    var button = $(pfx+'header-box #viewtoggle');
+    
+    box.removeClass('expanded').addClass('collapsed');
+    header.removeClass('expanded').addClass('collapsed');
+    body.removeClass('expanded').addClass('collapsed');
+    button.removeClass('expanded').addClass('collapsed');
 
-  if (box.data('CAFEVDBheaderboxheight') === undefined) {
-    box.data('CAFEVDBheaderboxheight', box.css('height'));
-    box.data('CAFEVDBheaderheight', header.css('height'));
-    box.data('CAFEVDBbodypadding', body.css('padding-top'));
+    CAFEVDB.broadcastHeaderVisibility('collapsed');
+  },
+  /**Expand the somewhat lengthy text at the head of the email page.
+   */
+  expandPageHeader: function() {
+    var pfx    = 'div.'+CAFEVDB.name+'-pme-';
+    var box    = $(pfx+'header-box');
+    var header = $(pfx+'header');
+    var body   = $(pfx+'body');
+    var button = $(pfx+'header-box #viewtoggle');
+
+    box.addClass('expanded').removeClass('collapsed');
+    header.addClass('expanded').removeClass('collapsed');
+    body.addClass('expanded').removeClass('collapsed');
+    button.addClass('expanded').removeClass('collapsed');
+
+    CAFEVDB.broadcastHeaderVisibility('expanded');
   }
-  box.css('height','4ex');
-  header.css('height','3ex');
-  body.css('padding-top', '12ex');
-  box.data('CAFEVDBheadermodheight', box.css('height'));
-  $('input[name="headervisibility"]').each(function (idx) {
-    $(this).val('collapsed');
-  });
-  $('#viewtoggle-img').attr(
-    'src', OC.filePath('', 'core/img/actions', 'download.svg'));
-}
-
-/**Expand the somewhat lengthy text at the head of the email page.
- */
-expandPMEPageHeader = function()
-{
-  var box    = $('div[class$="-pme-header-box"]');
-  var header = $('div[class$="-pme-header"]');
-  var body   = $('form.pme-form');    
-
-  var boxheight = box.data('CAFEVDBheaderboxheight');
-  var height    = box.data('CAFEVDBheaderheight');
-  var padding   = box.data('CAFEVDBbodypadding');
-  box.css('height', boxheight);
-  header.css('height', height);
-  body.css('padding-top', padding);
-  $('input[name="headervisibility"]').each(function (idx) {
-    $(this).val('expanded');
-  });
-  $('#viewtoggle-img').attr(
-    'src', OC.filePath('', 'core/img/actions', 'delete.svg'));
-}
+};
 
 $(document).ready(function(){
 
-    // $('td[class$="-money"]').filter(function() {
-    //     return true; /*$.trim($(this).text()).indexOf('-') == 0;*/
-    // }).addClass('negative')​;
+  $('input[class^="pme-input-"][class$="-birthday"]').datepicker({
+    dateFormat : 'dd.mm.yy'
+  });
 
-    $('input[class^="pme-input-"][class$="-birthday"]').datepicker({
-        dateFormat : 'dd.mm.yy'
-    });
-
-    $('td[class$="-money"]').filter(function() {
-        return $.trim($(this).text()).indexOf("-") == 0;
-    }).addClass("negative");
-
-//     $('input[class^="pme-input-"][class$="-datetime"]').datetimepicker({
-// //        dateFormat : 'dd-mm-yy'
-// //			    showPeriodLabels: false
-//     });
-
-  if (headervisibility == 'collapsed') {
-    collapsePMEPageHeader();
-  }
-
-  $('div[class$="-pme-header-box"] :button.viewtoggle').click(function(event) {
+  $('td[class$="-money"]').filter(function() {
+    return $.trim($(this).text()).indexOf("-") == 0;
+  }).addClass("negative");
+  
+  $('div.cafevdb-pme-header-box :button.viewtoggle').click(function(event) {
     event.preventDefault();
-    var box    = $('div[class$="-pme-header-box"]');
-    var header = $('div[class$="-pme-header"]');
-      var body   = $('form.pme-form');    
 
-    if (box.data('CAFEVDBheaderboxheight') === undefined) {
-      collapsePMEPageHeader();
-    } else if (box.css('height') == box.data('CAFEVDBheadermodheight')) {
-      expandPMEPageHeader();
+    var pfx    = 'div.'+CAFEVDB.name+'-pme-';
+    var box    = $(pfx+'header-box');
+    var header = $(pfx+'pme-header');
+    var body   = $(pfx+'body');
+
+    if (CAFEVDB.headervisibility == 'collapsed') {
+      CAFEVDB.PME.expandPageHeader();
     } else {
-      collapsePMEPageHeader();
+      CAFEVDB.PME.collapsePageHeader();
     }
+
     return false;
   });
 
