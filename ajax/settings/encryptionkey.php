aca72d3d51ac539c88dfd07d186b9280b16773c7
Claus-Justus Heine <himself@claus-justus-heine.de>
2013-01-15 10:29:40 +0100

    Used wrong keys into $_POST.

--- ajax/settings/encryptionkey.php
+++ ajax/settings/encryptionkey.php
@@ -10,32 +10,47 @@ OCP\JSON::checkAppEnabled('cafevdb');
 OCP\JSON::callCheck();
 
 // Get data
-if (isset($_POST['userkey']) && isset($_POST['password'])) {
+if (isset($_POST['encryptionkey']) && isset($_POST['password'])) {
   $user          = OCP\USER::getUser();
   $password      = $_POST['password'];
-  $encryptionkey = $_POST['userkey'];
+  $encryptionkey = $_POST['encryptionkey'];
 
-  // Re-validate the user
-  if (\OC_User::checkPassword($user, $password) !== $user) {
-    OCP\JSON::error(array('data' => array('message' => L::t('Invalid password for ')."$user")));
+  try {
+    // Re-validate the user
+    if (\OC_User::checkPassword($user, $password) !== $user) {
+      OCP\JSON::error(array('data' => array('message' => L::t('Invalid password for `%s\'',
+                                                              array($user)))));
+      return false;
+    }
+
+    // Then check whether the key is correct
+    if (!CAFEVDB\Config::encryptionKeyValid($encryptionkey)) {
+      OCP\JSON::error(array('data' => array('message' => L::t('Invalid encryption key.'))));
+      return false;
+    }
+
+    // So generate a new key-pair and store the key.
+    CAFEVDB\Config::recryptEncryptionKey($user, $password, $encryptionkey);
+
+    // Then store the key in the session as it is the valid key
+    CAFEVDB\Config::setEncryptionKey($encryptionkey);
+
+  } catch (\Exception $e) {
+    OC_JSON::error(
+      array(
+        "data" => array(
+          "message" => L::t("Exception:").$exception->getMessage())));
     return false;
   }
-
-  // Then check whether the key is correct
-  if (!CAFEVDB\Config::encryptionKeyValid($encryptionkey)) {
-    OCP\JSON::error(array('data' => array('message' => L::t('Invalid encryption key.'))));
-    return false;
-  }
-
-  // So generate a new key-pair and store the key.
-  CAFEVDB\Config::recryptEncryptionKey($user, $password, $encryptionkey);
-
-  // Then store the key in the session as it is the valid key
-  CAFEVDB\Config::setEncryptionKey($encryptionkey);
 
   OCP\JSON::success(array('data' => array('message' => L::t('Encryption key stored.'))));
 
   return true;
 }
+
+OCP\JSON::error(array('data' => array('message' => L::t('Unknown request: `%s\'',
+                                                        array(print_r($_POST,true))))));
+
+return false;
 
 ?>
