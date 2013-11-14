8a0d8bbfd41d0e26b6f3916021a91aaad9f07ee5
Claus-Justus Heine <himself@claus-justus-heine.de>
2013-11-15 00:21:27 +0100

    Add the language from OC.

--- js/config.php
+++ js/config.php
@@ -14,12 +14,14 @@ header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
 
 $headervisibility = Util::cgiValue('headervisibility', 'expanded');
-$tooltips         = OCP\Config::getUserValue(OCP\USER::getUser(), 'cafevdb', 'tooltips','');
+$tooltips         = OCP\Config::getUserValue(OCP\USER::getUser(), 'cafevdb', 'tooltips', '');
+$language         = OCP\Config::getUserValue(OCP\User::getUser(), 'core', 'lang', 'en');
 
 $array = array(
   "CAFEVDB.headervisibility" => "'".$headervisibility."'",
   "CAFEVDB.toolTips" => ($tooltips == "off" ? 'false' : 'true'),
-  "CAFEVDB.wysiwygEditor" => "'".Config::opts['editor']."'"
+  "CAFEVDB.wysiwygEditor" => "'".Config::$opts['editor']."'",
+  "CAFEVDB.language" => "'".$language."'",
   );
 
 // Echo it
