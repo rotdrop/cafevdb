0a89929821146cf6629280b4174b536c332251c6
Claus-Justus Heine <himself@claus-justus-heine.de>
2013-11-24 15:48:08 +0100

    Check for enabled address book and calendar.

--- templates/errorpage.php
+++ templates/errorpage.php
@@ -1,5 +1,26 @@
 <?php
 
+/**Orchestra member, musicion and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2011-2013 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 use CAFEVDB\L;
 
 echo '<div class="cafevdb cfgerror error">';
@@ -28,6 +49,12 @@ case 'exception':
   '.$trace.'
 </div>';
   }
+  break;
+case 'nocalendar':
+  echo L::t('CamerataDB Error: We need the calendar app to be enabled.');
+  break;
+case 'nocontacts':
+  echo L::t('CamerataDB Error: We need the contacts app to be enabled.');
   break;
 default:
   echo L::t('Something is wrong, but what?');
