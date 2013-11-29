6abdadf715c524218c18b8f7ca853edaa475e741
Claus-Justus Heine <himself@claus-justus-heine.de>
2013-11-29 16:27:29 +0100

    Set the dialog_holder div to be hidden until the popup appears.

--- templates/part.common.footer.php
+++ templates/part.common.footer.php
@@ -8,6 +8,6 @@ $css_pfx = $_['css-prefix'];
   <div class="debug" id="<?php echo $css_pfx; ?>-debug"></div>
 </div> <!-- cafevdb-general -->
 <div id="fullcalendar"></div>
-<div id="dialog_holder"></div>
+<div id="dialog_holder" class="popup topleft hidden"></div>
 <div id="appsettings" class="popup topright hidden"></div>
 
