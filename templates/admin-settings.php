23df4208d16eebaa449d89cdfc692ead250e4547
Claus-Justus Heine <himself@claus-justus-heine.de>
2013-12-11 12:51:38 +0100

    Use SVG logo if possible.

--- templates/admin-settings.php
+++ templates/admin-settings.php
@@ -5,7 +5,7 @@ use CAFEVDB\Config;
 <div class="personalblock">
   <form id="cafevdbadmin">
     <legend>
-      <img class="svg" src="<?php echo OCP\Util::imagePath(Config::APP_NAME, 'logo-greyf16x16.png'); ?>" >
+      <img class="svg cafevdblogo" src="<?php echo OCP\Util::imagePath(Config::APP_NAME, 'logo-greyf.svg'); ?>" >
       <strong>Camerata DB</strong><br />
     </legend>
     <input type="text" name="CAFEVgroup" id="CAFEVgroup" value="<?php echo $_['usergroup']; ?>" placeholder="<?php echo L::t('Group');?>" />
