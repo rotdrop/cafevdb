b7b70efd6d28f005b2ac4e097d01f3b809fca457
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-09-15 12:29:09 +0200

    $debugText was not set but referened by the exeception handling.

--- ajax/projects/verifyName.php
+++ ajax/projects/verifyName.php
@@ -1,4 +1,24 @@
 <?php
+/**Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2011-2014 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 use CAFEVDB\L;
 use CAFEVDB\Config;
@@ -12,6 +32,8 @@ if(!OCP\User::isLoggedIn()) {
 
 OCP\JSON::checkAppEnabled(Config::APP_NAME);
 
+$debugText = print_r($_POST, true);
+
 try {
 
   ob_start();
@@ -19,8 +41,6 @@ try {
   Error::exceptions(true);
   
   Config::init();
-
-  $debugText = print_r($_POST, true);
 
   // We simply expect to get the entire project form here.
   $projectValues = Util::getPrefixCGIData();
