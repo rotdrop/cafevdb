875063b36335835feb080cef3d58c1b83ccde284
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-09-15 12:31:12 +0200

    Cosmetics.

--- js/instrumentation.js
+++ js/instrumentation.js
@@ -183,57 +183,54 @@ var CAFEVDB = CAFEVDB || {};
 
 $(document).ready(function(){
 
-  PHPMYEDIT.addTableLoadCallback('BriefInstrumentation',
-                                 {
-                                   callback: function(selector, resizeCB) {
-                                     CAFEVDB.exportMenu(selector);
-                                     $(selector).find('input.pme-email').addClass('formsubmit');
-                                     CAFEVDB.SepaDebitMandate.popupInit(selector);
-                                     this.ready(selector);
-                                     resizeCB();
-                                   },
-                                   context: CAFEVDB.Instrumentation,
-                                   parameters: []
-                                 });
+  PHPMYEDIT.addTableLoadCallback('BriefInstrumentation', {
+    callback: function(selector, resizeCB) {
+      CAFEVDB.exportMenu(selector);
+      $(selector).find('input.pme-email').addClass('formsubmit');
+      CAFEVDB.SepaDebitMandate.popupInit(selector);
+      this.ready(selector);
+      resizeCB();
+    },
+    context: CAFEVDB.Instrumentation,
+    parameters: []
+  });
 
-  PHPMYEDIT.addTableLoadCallback('DetailedInstrumentation',
-                                 {
-                                   callback: function(selector, resizeCB) {
-                                     var container = $(selector);
-                                     CAFEVDB.exportMenu(selector);
-                                     container.find('input.pme-email').addClass('formsubmit');
-                                     container.find('span.photo').click(function(event) {
-                                       event.preventDefault();
-                                       CAFEVDB.Photo.popup(this);
-                                       return false;
-                                     });
-                                     if (container.find('#file_upload_target').length > 0) {
-                                       var idField = $(selector).find('input[name="PME_data_MusikerId"]');
-                                       var recordId = -1;
-                                       if (idField.length > 0) {
-                                         recordId = idField.val();
-                                       }
-                                       CAFEVDB.Photo.ready(recordId, resizeCB);
-                                     } else {
-                                       container.find('span.photo').imagesLoaded(resizeCB);
-                                     }
-                                   },
-                                   context: CAFEVDB,
-                                   parameters: []
-                                 });
+  PHPMYEDIT.addTableLoadCallback('DetailedInstrumentation', {
+    callback: function(selector, resizeCB) {
+      var container = $(selector);
+      CAFEVDB.exportMenu(selector);
+      container.find('input.pme-email').addClass('formsubmit');
+      container.find('span.photo, #cafevdb_inline_image_wrapper').click(function(event) {
+        event.preventDefault();
+        CAFEVDB.Photo.popup(this);
+        return false;
+      });
+      if (container.find('#file_upload_target').length > 0) {
+        var idField = $(selector).find('input[name="PME_data_MusikerId"]');
+        var recordId = -1;
+        if (idField.length > 0) {
+          recordId = idField.val();
+        }
+        CAFEVDB.Photo.ready(recordId, resizeCB);
+      } else {
+        container.find('span.photo').imagesLoaded(resizeCB);
+      }
+    },
+    context: CAFEVDB,
+    parameters: []
+  });
 
-  PHPMYEDIT.addTableLoadCallback('BulkAddMusicians',
-                                 {
-                                   callback: function(selector, resizeCB) {
-                                     CAFEVDB.exportMenu(selector);
-                                     $(selector).find('input.pme-email').addClass('formsubmit');
-                                     CAFEVDB.SepaDebitMandate.popupInit(selector);
-                                     this.ready(selector);
-                                     resizeCB();
-                                   },
-                                   context: CAFEVDB.Instrumentation,
-                                   parameters: []
-                                 });
+  PHPMYEDIT.addTableLoadCallback('BulkAddMusicians', {
+    callback: function(selector, resizeCB) {
+      CAFEVDB.exportMenu(selector);
+      $(selector).find('input.pme-email').addClass('formsubmit');
+      CAFEVDB.SepaDebitMandate.popupInit(selector);
+      this.ready(selector);
+      resizeCB();
+    },
+    context: CAFEVDB.Instrumentation,
+    parameters: []
+  });
 
   $(PHPMYEDIT.defaultSelector+' input.pme-email').addClass('formsubmit');
   $(PHPMYEDIT.defaultSelector+' input.pme-bulkcommit').addClass('formsubmit');
@@ -244,4 +241,6 @@ $(document).ready(function(){
 
 // Local Variables: ***
 // js-indent-level: 2 ***
+// js3-indent-level: 2 ***
+// js3-label-indent-offset: -2 ***
 // End: ***
