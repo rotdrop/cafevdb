bddc85b25eaa5fefac54c45545d5d33748e36a72
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-09-22 12:30:15 +0200

    Event handlers for the email popup

--- js/email.js
+++ js/email.js
@@ -111,9 +111,142 @@ CAFEVDB.Email = CAFEVDB.Email || {};
     CAFEVDB.broadcastHeaderVisibility('expanded');
   };
 
+  /**Add handlers to the control elements, and call the AJAX sciplets
+   * for validation to update the recipients selection tab accordingly.
+   * 
+   * @param fieldset The field-set enclosing the recipients selection part
+   * 
+   * @param dialogHolder The div holding the jQuery dialog for everything
+   * 
+   * @param panelHolder The div enclosing the fieldset
+   * 
+   */
+  Email.emailFormRecipientsHandlers = function(fieldset, form, dialogHolder, panelHolder, layoutCB) {
+    var Email = this;
+
+    layoutCB();
+
+    var recipientsSelect   = fieldset.find('select#recipients-select');
+    var missingAddresses   = fieldset.find('#missing-email-addresses-wrapper');
+    var filterHistoryInput = fieldset.find('#recipients-filter-history');
+    var debugOutput        = form.find('#emailformdebug');
+
+    // Apply the instruments filter
+    var applyRecipientsFilter = function(event) {
+      event.preventDefault();
+      
+      var post = fieldset.serialize();
+      post += '&'+form.find('fieldset.form-data').serialize();
+      if ($(this).is(':button')) {
+        var tmp = {};
+        tmp[$(this).attr('name')] = $(this).val();
+        post += '&'+$.param(tmp);
+      }
+      $.post(OC.filePath('cafevdb', 'ajax/email', 'recipients.php'),
+             post,
+             function(data) {
+               if (!CAFEVDB.ajaxErrorHandler(data, [
+                 'recipientsOptions', 'missingEmailAddresses', 'filterHistory'
+               ])) {
+                 return false;
+               }
+               if (typeof data.data.contents != 'undefined' && data.data.contents.length > 0) {
+                 // replace the entire tab.
+                 $('.tipsy').remove();
+                 panelHolder.html(data.data.contents);
+                 fieldset = panelHolder.find('fieldset.email-recipients.page');
+                 Email.emailFormRecipientsHandlers(fieldset,
+                                                   form,
+                                                   dialogHolder,
+                                                   panelHolder,
+                                                   layoutCB);
+               } else {
+                 // Here goes the real work
+                 // We only need to update the select-element and the list
+                 // of musicians which should be possible recipients but
+                 // do not have an email address.
+                 recipientsSelect.html(data.data.recipientsOptions);
+                 recipientsSelect.bootstrapDualListbox('refresh', true);
+                 filterHistoryInput.val(data.data.filterHistory);
+                 missingAddresses.html(data.data.missingEmailAddresses);
+               }
+
+               var filterHistory = $.parseJSON(data.data.filterHistory);
+               if (filterHistory.historyPosition >= 0 &&
+                   filterHistory.historyPosition < filterHistory.historySize-1) {
+                 // enable the undo button
+                 fieldset.find('#instruments-filter-undo').prop('disabled', false);
+               } else {
+                 fieldset.find('#instruments-filter-undo').prop('disabled', true);
+               }
+               if (filterHistory.historyPosition > 0) {
+                 // enable the redo button as well
+                 fieldset.find('#instruments-filter-redo').prop('disabled', false);
+               } else {
+                 fieldset.find('#instruments-filter-redo').prop('disabled', true);
+               }
+
+               var debugText = '';
+               if (typeof data.data.debug != 'undefined') {
+                 debugText = data.data.debug;
+               }
+               debugOutput.html('<pre>'+$('<div></div>').text(debugText).html()+'</pre>'+
+                                '<pre>'+$('<div></div>').text(data.data.recipientsOptions).html()+'</pre>'+
+                                data.data.missingEmailAddresses+'</br>'+
+                                $('<div></div>').text(CAFEVDB.urldecode(post)).html())
+               return false;
+             });
+      return false;
+    };
+
+    // Attach above function to almost every sensible control :)
+
+    // Controls :..
+    var controlsContainer = fieldset.find('.filter-controls.container');
+
+    // Instruments filter
+    if (true) {
+      var instrumentsFilter = fieldset.find('.instruments-filter.container');
+      instrumentsFilter.on('dblclick', applyRecipientsFilter);
+    } else {
+      var instrumentsFilter = fieldset.find('.instruments-filter.container select');
+      instrumentsFilter.off('change');
+      instrumentsFilter.on('change', applyRecipientsFilter);
+    }
+
+    // Member status filter
+    var memberStatusFilter = fieldset.find('select.member-status-filter');
+    memberStatusFilter.off('change');
+    memberStatusFilter.on('change', applyRecipientsFilter);
+
+    // Basic set
+    var basicRecipientsSet = fieldset.find('.basic-recipients-set.container input[type="checkbox"]');
+    basicRecipientsSet.off('change');
+    basicRecipientsSet.on('change', applyRecipientsFilter);
+
+    // "submit" when hitting any of the control buttons
+    controlsContainer.off('click', '**');
+    controlsContainer.on('click', 'input', applyRecipientsFilter);
+  };
+
+  /**Add handlers to the control elements, and call the AJAX sciplets
+   * for validation to update the message composition tab accordingly.
+   * 
+   * @param fieldset The field-set enclosing the recipients selection part
+   * 
+   * @param dialogHolder The div holding the jQuery dialog for everything
+   * 
+   * @param panelHolder The div enclosing the fieldset
+   * 
+   */
+  Email.emailFormCompositionHandlers = function(fieldset, form, dialogHolder, panelHolder) {
+    var Email = this;
+  };    
+
   /**Open the mass-email form in a popup window
    */
   Email.emailFormPopup = function(post) {
+    var Email = this;
     $.post(OC.filePath('cafevdb', 'ajax/email', 'emailform.php'),
            post,
            function(data) {
@@ -141,12 +274,19 @@ CAFEVDB.Email = CAFEVDB.Email || {};
                }
                OC.dialogs.alert(info, t('cafevdb', 'Error'));
                if (data.data.debug != '') {
-                 OC.dialogs.alert(data.data.debug, t('cafevdb', 'Debug Information'), null, true);
+                 OC.dialogs.alert(data.data.debug, t('cafevdb', 'Debug Information'), undefined, true);
                }
                return false;
              }
+             var dlgTitle = '';
+             if (data.data.projectId >= 0) {
+               dlgTitle = t('cafevdb', 'Em@il Form for {ProjectName}',
+                            { ProjectName: data.data.projectName });
+             } else {
+               dlgTitle = t('cafevdb', 'Em@il Form');
+             }
              var popup = dialogHolder.dialog({
-               title: t('cafevdb', 'Em@il Form'), // todo: add project etc. if appropriate
+               title: dlgTitle,
                position: { my: "middle top",
                            at: "middle bottom+50px",
                            of: "#header" },
@@ -164,29 +304,74 @@ CAFEVDB.Email = CAFEVDB.Email || {};
                    dialogHolder.dialog('close');
                    return false;
                  });
+                 var dialogWidget = dialogHolder.dialog('widget');
                  dialogHolder.tabs({
                    active: 0,
-                   heightStyle: 'content' });
+                   heightStyle: 'content',
+                   activate: function(event, ui) {
+                     var panel = ui.newPanel;
+                     var newHeight = dialogWidget.height()
+                                   - dialogWidget.find('.ui-dialog-titlebar').outerHeight(true);
+                     newHeight -= $('#emailformtabs').outerHeight(true);
+                     newHeight -= panel.outerHeight(true) - panel.height();
+                     panel.height(newHeight);
+                   }
+                 });
                  CAFEVDB.addEditor(dialogHolder.find('textarea.wysiwygeditor'), undefined, '20em');
                  $('#cafevdb-email-template-selector').chosen({ disable_search_threshold: 10,
                                                                 width: '10em' });
-                 $('form#cafevdb-email-recipients-form #member-status-filter').chosen();
+                 var emailForm = $('form#cafevdb-email-form');
 
-                 $('form#cafevdb-email-recipients-form #instruments-filter option[value="*"]').remove();
-                 $('form#cafevdb-email-recipients-form #instruments-filter').chosen();
-                 $('form#cafevdb-email-recipients-form #recipients-select').bootstrapDualListbox(
-                   {
-                     // moveOnSelect: false,
-                     // preserveSelectionOnMove : 'all',
-                     nonSelectedListLabel: t('cafevdb', 'Remaining Recipients'),
-                     selectedListLabel   : t('cafevdb', 'Selected Recipients'),
-                     infoText            : '&nbsp;', // t('cafevdb', 'Showing all {0}'),
-                     infoTextTiltered    : '<span class="label label-warning">'+t('cafevdb', 'Filtered')+'</span> {0} '+t('cafevdb', 'from')+' {1}',
-                     infoTextEmpty       : '&nbsp;', // t('cafevdb', 'Empty list'),
-                     filterPlaceHolder   : t('cafevdb', 'Filter'),
-                     filterTextClear     : t('cafevdb', 'show all')
-                   }
-                 );
+                 var layoutRecipientsFilter = function() {
+                   var emailForm = $('form#cafevdb-email-form');
+                   emailForm.find('#member-status-filter').chosen();
+                   emailForm.find('#member-status-filter').chosen();
+                   emailForm.find('#instruments-filter option[value="*"]').remove();
+                   emailForm.find('#instruments-filter option[value=""]').remove();
+                   emailForm.find('#instruments-filter').chosen();
+                   emailForm.find('#recipients-select').bootstrapDualListbox(
+                     {
+                       // moveOnSelect: false,
+                       // preserveSelectionOnMove : 'all',
+                       moveAllLabel: t('cafevdb', 'Move all'),
+                       moveSelectedLabel: t('cafevdb', 'Move selected'),
+                       removeSelectedLabel: t('cafevdb', 'Remove selected'),
+                       removeAllLabel: t('cafevdb', 'Remove all'),
+                       nonSelectedListLabel: t('cafevdb', 'Remaining Recipients'),
+                       selectedListLabel   : t('cafevdb', 'Selected Recipients'),
+                       infoText            : '&nbsp;', // t('cafevdb', 'Showing all {0}'),
+                       infoTextFiltered    : '<span class="label label-warning">'
+                                           + t('cafevdb', 'Filtered')
+                                           + '</span> {0} '
+                                           + t('cafevdb', 'from')
+                                           +' {1}',
+                       infoTextEmpty       : t('cafevdb', 'Empty list'),
+                       filterPlaceHolder   : t('cafevdb', 'Filter'),
+                       filterTextClear     : t('cafevdb', 'show all')
+                     }
+                   );
+                   var dualSelect = emailForm.find('div.bootstrap-duallistbox-container select');
+                   dualSelect.attr(
+                     'title',
+                     t('cafevdb', 'Click on the names to move the respective person to the other box'));
+                   dualSelect.addClass('tipsy-s');
+
+                   CAFEVDB.tipsy(dialogHolder.find('div#emailformrecipients'));
+                 };
+
+                 // Fine, now add handlers and AJAX callbacks. We can
+                 // probably move some of the code above to the
+                 // respective tab-handler.
+                 Email.emailFormRecipientsHandlers(emailForm.find('fieldset.email-recipients.page'),
+                                                   emailForm,
+                                                   dialogHolder,
+                                                   dialogHolder.find('div#emailformrecipients'),
+                                                   layoutRecipientsFilter);
+                 Email.emailFormCompositionHandlers(emailForm.find('fieldset.email-composition.page'),
+                                                    emailForm,
+                                                    dialogHolder,
+                                                    dialogHolder.find('div#emailformmessage'));
+
                  CAFEVDB.tipsy(dialogHolder);
                },
                close: function() {
