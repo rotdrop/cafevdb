b8b66df0cd41db1ab39707485f98d23dcebcc5ca
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-09-29 03:33:39 +0200

    Re-enable the leave-page warning.

--- js/file-upload.js
+++ js/file-upload.js
@@ -212,7 +212,7 @@ CAFEVDB.FileUpload = CAFEVDB.FileUpload || {};
       return size;
     };
 
-    if (false) {
+    if (true) {
       // warn user not to leave the page while upload is in progress
       $(window).bind('beforeunload', function(e) {
         if ($.assocArraySize(CAFEVDB.FileUpload.uploadingFiles) > 0) {
