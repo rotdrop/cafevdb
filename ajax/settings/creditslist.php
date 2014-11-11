3fd8d8bf1bc38a23725be649ce45baaec10ba7a6
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-11-11 14:32:30 +0100

    Replace erroneous comment.

--- ajax/settings/creditslist.php
+++ ajax/settings/creditslist.php
@@ -22,19 +22,8 @@
 
 /**@file
  *
- * This file is responsible to load the main data-base views as
- * requested by the internal navigation buttons. It operates in two
- * modes:
- *
- * - in regular mode it records the current post data in the history
- * and loads respective template from the template folder, as
- * specified b y the Template post-variable.
- *
- * - in history restore mode, it restores a snapshot from the history,
- * replaces the $_POST variable with the history snapshot and then
- * goes on as usual. History restore is triggered by the post-variable
- * 'HistoryOffset' which, if present, triggers restoration of the
- * history and gives the offset into the current history.
+ * Hall-of-fame support.
+ * 
  */
 
 namespace CAFEVDB 
