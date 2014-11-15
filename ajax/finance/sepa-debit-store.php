a585565e9730552ee759866de378790e5ef59999
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-11-15 18:53:08 +0100

    Use Finance::validateSepaMandate(). Do not allow empty fields to destroy data.

--- ajax/finance/sepa-debit-store.php
+++ ajax/finance/sepa-debit-store.php
@@ -20,74 +20,107 @@
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-\OCP\JSON::checkLoggedIn();
-\OCP\JSON::checkAppEnabled('cafevdb');
-\OCP\JSON::callCheck();
+namespace CAFEVDB {
 
-use \CAFEVDB\L;
-use \CAFEVDB\Config;
-use \CAFEVDB\Util;
-use \CAFEVDB\Projects;
-use \CAFEVDB\Finance;
+  \OCP\JSON::checkLoggedIn();
+  \OCP\JSON::checkAppEnabled('cafevdb');
+  \OCP\JSON::callCheck();
 
-$_GET = array(); // disable GET
+  $debugText = '';
+  $errorMessage = '';
+  $infoMessage = '';
 
-$requiredKeys = array('ProjectId', 'MusicianId', 'mandateReference');
-foreach ($requiredKeys as $required) {
-  if (!Util::cgiValue($required, null, false)) {
-    OC_JSON::error(
-      array("data" => array(
-              "message" => L::t("Required information `%s' not provided.", array($required)).print_r($_POST, true))));
+  ob_start();
+
+  try {
+
+    Error::exceptions(true);
+
+    $_GET = array(); // disable GET
+
+    $requiredKeys = array('ProjectId', 'MusicianId', 'mandateReference');
+    foreach ($requiredKeys as $required) {
+      if (!Util::cgiValue($required, null, false)) {
+        \OC_JSON::error(
+          array("data" => array(
+                  "message" => L::t("Required information `%s' not provided.", array($required)).print_r($_POST, true))));
+        return false;
+      }
+    }
+
+    // Compose the mandate
+    $mandate = array('mandateReference' => Util::cgiValue('mandateReference'),
+                     'nonrecurring' => Util::cgiValue('nonrecurring'),
+                     'musicianId' => Util::cgiValue('MusicianId'),
+                     'projectId' => Util::cgiValue('ProjectId'),
+                     'mandateDate' => Util::cgiValue('mandateDate'),
+                     'lastUsedDate' => Util::cgiValue('lastUsedDate'),
+                     'IBAN' => Util::cgiValue('bankAccountIBAN'),
+                     'BIC' => Util::cgiValue('bankAccountBIC'),
+                     'BLZ' => Util::cgiValue('bankAccountBLZ'),
+                     'bankAccountOwner' => Util::cgiValue('bankAccountOwner'));
+
+    // this will throw on error
+    Finance::validateSepaMandate($mandate);
+
+    if (Finance::storeSepaMandate($mandate)) {
+      \OC_JSON::success(
+        array("data" => array(
+                'message' => L::t('SEPA debit mandate stored in data-base.'))));
+      return true;
+    } else {
+      \OC_JSON::error(
+        array("data" => array(
+                'message' => L::t('Unable to store SEPA debit mandate in data-base.'))));
+      return false;
+    }
+
+  } catch (\InvalidArgumentException $e) {
+    $debugText = ob_get_contents();
+    @ob_end_clean();
+
+    $exceptionText = $e->getFile().'('.$e->getLine().'): '.$e->getMessage();
+
+    \OCP\JSON::error(
+      array(
+        'data' => array(
+          'caption' => L::t('Invalid Argument Exception'),
+          'error' => 'exception',
+          'exception' => $exceptionText,
+          'trace' => '',
+          'message' => L::t('Invalid arguments'),
+          'debug' => htmlspecialchars($debugText))));
+    return false;
+  } catch (\Exception $e) {
+    
+    $debugText .= ob_get_contents();
+    @ob_end_clean();
+
+    $exceptionText = $e->getFile().'('.$e->getLine().'): '.$e->getMessage();
+    $trace = $e->getTraceAsString();
+
+    $admin = Config::adminContact();
+
+    $mailto = $admin['email'].
+      '?subject='.rawurlencode('[CAFEVDB-Exception] Exceptions from Email-Form').
+      '&body='.rawurlencode($exceptionText."\r\n".$trace);
+    $mailto = '<span class="error email"><a href="mailto:'.$mailto.'">'.$admin['name'].'</a></span>';
+
+    \OCP\JSON::error(
+      array(
+        'data' => array(
+          'caption' => L::t('PHP Exception Caught'),
+          'error' => 'exception',
+          'exception' => $exceptionText,
+          'trace' => $trace,
+          'message' => L::t('Error, caught an exception. '.
+                            'Please copy the displayed text and send it by email to %s.',
+                            array($mailto)),
+          'debug' => htmlspecialchars($debugText))));
+
     return false;
   }
-}
 
-// Compose the mandate
-$mandate = array('mandateReference' => Util::cgiValue('mandateReference'),
-                 'nonrecurring' => Util::cgiValue('nonrecurring'),
-                 'musicianId' => Util::cgiValue('MusicianId'),
-                 'projectId' => Util::cgiValue('ProjectId'),
-                 'mandateDate' => Util::cgiValue('mandateDate'),
-                 'lastUsedDate' => Util::cgiValue('lastUsedDate'),
-                 'IBAN' => Util::cgiValue('bankAccountIBAN'),
-                 'BIC' => Util::cgiValue('bankAccountBIC'),
-                 'BLZ' => Util::cgiValue('bankAccountBLZ'),
-                 'bankAccountOwner' => Util::cgiValue('bankAccountOwner'));
-
-// Verify IBAN and BIC if non-empty
-$IBAN = $mandate['IBAN'];
-if ($IBAN != '') {
-  $iban = new \IBAN($IBAN);
-  if (!$iban->Verify()) {
-    OC_JSON::error(
-      array("data" => array(
-              'message' => L::t('Value for `%s\' invalid: `%s\'.',
-                                array('IBAN', $value)),
-              'suggestion' => '')));
-    return false;
-  }
-}
-
-$BIC = $mandate['BIC'];
-if ($BIC != '' && !Finance::validateSWIFT($BIC)) {
-  OC_JSON::error(
-    array("data" => array(
-            'message' => L::t('Value for `%s\' invalid: `%s\'.',
-                              array('BIC', $value)),
-            'suggestion' => '')));
-  return false;
-}
-
-if (Finance::storeSepaMandate($mandate)) {
-  OC_JSON::success(
-    array("data" => array(
-            'message' => L::t('SEPA debit mandate stored in data-base.'))));
-  return true;
-} else {
-  OC_JSON::error(
-    array("data" => array(
-            'message' => L::t('Unable to store SEPA debit mandate in data-base.'))));
-  return false;
-}
+} // namespace CAFEVDB
 
 ?>
