25df508eac90831d7df4bf2a4fb25943322bf0f2
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-11-16 01:21:57 +0100

    Well, page layout will be fixed later. Gnah.

--- lib/pdfletter.php
+++ lib/pdfletter.php
@@ -45,7 +45,7 @@ namespace CAFEVDB
     const FONT_HEADER = 10;
     const FONT_RECIPIENT = 12;
     const FOOTER_HEIGHT = 20;
-    const FIRST_FOOTER_HEIGHT = 25;
+    const FIRST_FOOTER_HEIGHT = 28;
     const PT = 0.3527777777777777;
 
     /**Return the font-size converted to mm. */
