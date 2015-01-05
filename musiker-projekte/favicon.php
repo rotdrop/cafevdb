ba96bc5f5a63f822406e0642d34d6ef70525b1b9
Claus-Justus Heine <himself@claus-justus-heine.de>
2015-01-05 03:14:31 +0100

    Remove old cruft from back-merge into master branch.

--- musiker-projekte/favicon.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo '<link rel="shortcut icon" type="image/png" href="images/logo-greyf32x32.png" />';
-?>
--- musiker-projekte/money.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ($value < 0) {
-  return '<span style="color:#FF0000;">'.$value.'&euro;</span>';
-} else {
-  return $value.'&euro;';
-}
-
-?>
