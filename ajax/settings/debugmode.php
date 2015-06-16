e7890120b454636ce7ddccf470e672a6ca263707
Claus-Justus Heine <himself@claus-justus-heine.de>
2015-06-16 22:19:26 +0200

    Use CAFEFDB class.

--- ajax/settings/debugmode.php
+++ ajax/settings/debugmode.php
@@ -1,31 +1,62 @@
 <?php
+/* Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2011-2014 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-// Init owncloud
+/**@file
+ *
+ * Hall-of-fame support.
+ *
+ */
 
-use CAFEVDB\L;
-use CAFEVDB\Util;
-use CAFEVDB\Config;
+namespace CAFEVDB
+{
+  // Check if we are a user
+  \OCP\JSON::checkLoggedIn();
+  \OCP\JSON::checkAppEnabled('cafevdb');
+  \OCP\JSON::callCheck();
 
-// Check if we are a user
-OCP\JSON::checkLoggedIn();
-OCP\JSON::checkAppEnabled('cafevdb');
-OCP\JSON::callCheck();
+  $_GET = array();
 
-$_GET = array();
+  $debugModes = Util::cgiValue('debugModes', array());
+  if (!$debugModes) {
+    $debugModes = array();
+  }
 
-$debugModes = Util::cgiValue('debugModes', array());
+  foreach (Config::$debug as $key => $enabled) {
+    Config::$debug['value'] = false;
+  }
+  foreach ($debugModes as $value) {
+    Config::$debug['value'] = true;
+  }
 
-foreach ($debugModes as $value) {
-  Config::$debug['value'] = true;
-}
+  $debug = implode(',', $debugModes);
 
-$debug = implode(',', $debugModes);
+  Config::setUserValue('debug', $debug);
 
-Config::setUserValue('debug', $debug);
+  \OCP\JSON::success(
+    array(
+      'data' => array( 'message' => L::t("Debug-modes changed to `%s'", array($debug)))));
 
-OCP\JSON::success(
-  array(
-    'data' => array( 'message' => L::t("Debug-modes changed to `%s'", array($debug)))));
-return true;
+  return true;
+
+} // namespace CAFEVDB
 
 ?>
