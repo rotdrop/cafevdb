c64cde70c3e15a34c247b64ee367081ee3400773
Claus-Justus Heine <himself@claus-justus-heine.de>
2016-01-13 23:34:28 +0100

    em <-> pixel conversion and  maxWIdth etc. for a set of elements.

--- js/jquery-extensions.js
+++ js/jquery-extensions.js
@@ -34,7 +34,7 @@ var CAFEVDB = CAFEVDB || {};
   $.fn.cafevDialog = function(argument) {
     if (arguments.length == 1 && typeof argument == 'object' && argument != null) {
       var options = {
-        appendTo: '#cafevdb-general',
+        appendTo: '#cafevdb-general'
         //appendTop: 'body',
       };
       argument = $.extend({}, options, argument);
@@ -217,5 +217,70 @@ var CAFEVDB = CAFEVDB || {};
              }
            });
 
+  /**Compute the maximum width of a set of elements */
+  $.fn.maxWidth = function() {
+    return Math.max.apply(null, this.map(function () {
+                                  return $(this).width();
+                                }).get());
+  };
+
+  /**Compute the maximum width of a set of elements */
+  $.fn.maxOuterWidth = function(extended) {
+    return Math.max.apply(null, this.map(function () {
+                                  return $(this).outerWidth(extended);
+                                }).get());
+  };
+
+  /**Compute the maximum height of a set of elements */
+  $.fn.maxHeight = function() {
+    return Math.max.apply(null, this.map(function () {
+                                  return $(this).height();
+                                }).get());
+  };
+
+  /**Compute the maximum height of a set of elements */
+  $.fn.maxOuterHeight = function(extended) {
+    return Math.max.apply(null, this.map(function () {
+                                  return $(this).outerHeight(extended);
+                                }).get());
+  };
+
+  /*--------------------------------------------------------------------
+   * jQuery pixel/em conversion plugins: toEm() and toPx()
+   * by Scott Jehl (scott@filamentgroup.com), http://www.filamentgroup.com
+   * Copyright (c) Filament Group
+   *
+   * Dual licensed under the MIT
+   * (filamentgroup.com/examples/mit-license.txt) or GPL
+   * (filamentgroup.com/examples/gpl-license.txt) licenses.  Article:
+   * http://www.filamentgroup.com/lab/update_jquery_plugin_for_retaining_scalable_interfaces_with_pixel_to_em_con/
+   *
+   * Options:
+   *   scope: string or jQuery selector for font-size scoping
+   * Usage Example: $(myPixelValue).toEm(); or $(myEmValue).toPx();
+   --------------------------------------------------------------------*/
+
+  $.fn.toEm = function(settings){
+    settings = jQuery.extend({
+      scope: 'body'
+    }, settings);
+    var that = parseInt(this[0],10),
+	scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+	scopeVal = scopeTest.height();
+    scopeTest.remove();
+    return (that / scopeVal).toFixed(8) + 'em';
+  };
+
+
+  $.fn.toPx = function(settings){
+    settings = jQuery.extend({
+      scope: 'body'
+    }, settings);
+    var that = parseFloat(this[0]),
+	scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+	scopeVal = scopeTest.height();
+    scopeTest.remove();
+    return Math.round(that * scopeVal) + 'px';
+  };
 
 })(window, jQuery, CAFEVDB);
