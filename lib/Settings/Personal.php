b3bbc2af7b9ae6da2ced0e056f374ae4b23e7309
Claus-Justus Heine <Claus-Justus.Heine@IANS.Uni-Stuttgart.DE>
2020-09-25 16:40:15 +0200

    Slight cleanup.

--- lib/Settings/Personal.php
+++ lib/Settings/Personal.php
@@ -64,127 +64,126 @@ class Personal implements ISettings {
         [
           'error' => 'notamember',
           'userId' => $this->userId(),
+        ], 'blank');
+    }
+    // Are we a group-admin?
+    $isGroupAdmin = $this->isSubAdminofGroup() && $this->encryptionKeyValid();
+
+    $templateParameters = [
+      'appName' => $this->appName(),
+      'userId' => $this->userId(),
+      'locale' => $this->getLocale(),
+      'localeCountryNames' => $this->localeCountryNames(),
+      'timezone' => $this->getTimezone(),
+      'adminsettings' => $isGroupAdmin,
+      'encryptionkey' => $this->getAppEncryptionKey(),
+      'showToolTips' => $this->getUserValue('tooltips', 'on'),
+      'debugMode' => $this->getUserValue('debug', 0),
+      'pagerows' => $this->getUserValue('pagerows', 20),
+      'toolTips' => $this->toolTipsService,
+      'filtervisibility' => $this->getUserValue('filtervisibility', 'off'),
+      'directchange' => $this->getUserValue('directchange', 'off'),
+      'showdisabled' => $this->getUserValue('showdisabled', 'off'),
+      'expertmode' => $this->getUserValue('expertmode', 'off'),
+      'editor' => $this->getUserValue('editor', self::DEFAULT_EDITOR),
+      'wysiwygOptions' => ConfigService::WYSIWYG_EDITORS,
+    ];
+
+    if ($isGroupAdmin) {
+      $executiveBoardTable = $this->getConfigValue('executiveBoardTable', $this->l->t('ExecutiveBoardMembers'));
+      $executiveBoardTableId = $this->getConfigValue('executiveBoardTableId', -1);
+      $executiveBoardMembers = $this->projectService->participantOptions($executiveBoardTableId, $executiveBoardTable);
+      $templateParameters = array_merge(
+        $templateParameters,
+        [
+          'streetAddressName01' => $this->getConfigValue('streetAddressName01'),
+          'streetAddressName02' => $this->getConfigValue('streetAddressName02'),
+          'streetAddressStreet' => $this->getConfigValue('streetAddressStreet'),
+          'streetAddressHouseNumber' => $this->getConfigValue('streetAddressHouseNumber'),
+          'streetAddressCity' => $this->getConfigValue('streetAddressCity'),
+          'streetAddressZIP' => $this->getConfigValue('streetAddressZIP'),
+          'streetAddressCountry' => $this->getConfigValue('streetAddressCountry'),
+
+          'phoneNumber' => $this->getConfigValue('phoneNumber'),
+
+          'bankAccountOwner' => $this->getConfigValue('bankAccountOwner'),
+          'bankAccountIBAN' => $this->getConfigValue('bankAccountIBAN'),
+          'bankAccountBLZ' => $this->getConfigValue('bankAccountBLZ'),
+          'bankAccountBIC' => $this->getConfigValue('bankAccountBIC'),
+          'bankAccountCreditorIdentifier' => $this->getConfigValue('bankAccountCreditorIdentifier'),
+
+          'memberTable' => $this->getConfigValue('memberTable', $this->l->t('ClubMembers')),
+          'memberTableId' => $this->getConfigValue('memberTableId', -1),
+          'executiveBoardTable' => $executiveBoardTable,
+          'executiveBoardTableId' => $execitiveBoardTableId,
+          'executiveBoardMembers' => $executiveBoardMember,
+          'userGroupMembers' => array_map(function($user) { return $user->getUID(); }, $this->group()->getUsers()),
+          'userGroups' => array_map(function($group) { return $group->getGID(); }, $this->groupManager()->search('')),
+          'orchestra' => $this->getConfigValue('orchestra'),
+
+          'dbserver' => $this->getConfigValue('dbserver'),
+          'dbname' => $this->getConfigValue('dbname'),
+          'dbuser' => $this->getConfigValue('dbuser'),
+          'dbpassword' => $this->getConfigValue('dbpassword'),
+          'encryptionkey' => $this->getConfigValue('encryptionkey'),
+
+          'shareowner', $this->getConfigValue('shareowner', ''),
+          'concertscalendar', $this->getConfigValue('concertscalendar', $this->l->t('concerts')),
+          'rehearsalscalendar', $this->getConfigValue('rehearsalscalendar', $this->l->t('rehearsals')),
+          'othercalendar', $this->getConfigValue('othercalendar', $this->l->t('other')),
+          'managementcalendar', $this->getConfigValue('managementcalendar', $this->l->t('management')),
+          'financecalendar', $this->getConfigValue('financecalendar', $this->l->t('finance')),
+          'eventduration', $this->getConfigValue('eventduration', '180'),
+
+          'sharedaddressbook', $this->getConfigValue('sharedaddressbook', $this->l->t('contacts')),
+
+          'sharedfolder', $this->getConfigValue('sharedfolder',''),
+          'projectsfolder', $this->getConfigValue('projectsfolder',''),
+          'projectsbalancefolder', $this->getConfigValue('projectsbalancefolder',''),
         ]);
-    } else {
-      // Are we a group-admin?
-      $isGroupAdmin = $this->isSubAdminofGroup() && $this->encryptionKeyValid();
 
-      $templateParameters = [
-        'appName' => $this->appName(),
-        'userId' => $this->userId(),
-        'locale' => $this->getLocale(),
-        'localeCountryNames' => $this->localeCountryNames(),
-        'timezone' => $this->getTimezone(),
-        'adminsettings' => $isGroupAdmin,
-        'encryptionkey' => $this->getAppEncryptionKey(),
-        'showToolTips' => $this->getUserValue('tooltips', 'on'),
-        'debugMode' => $this->getUserValue('debug', 0),
-        'pagerows' => $this->getUserValue('pagerows', 20),
-        'toolTips' => $this->toolTipsService,
-        'filtervisibility' => $this->getUserValue('filtervisibility', 'off'),
-        'directchange' => $this->getUserValue('directchange', 'off'),
-        'showdisabled' => $this->getUserValue('showdisabled', 'off'),
-        'expertmode' => $this->getUserValue('expertmode', 'off'),
-        'editor' => $this->getUserValue('editor', self::DEFAULT_EDITOR),
-        'wysiwygOptions' => ConfigService::WYSIWYG_EDITORS,
-      ];
-
-      if ($isGroupAdmin) {
-        $executiveBoardTable = $this->getConfigValue('executiveBoardTable', $this->l->t('ExecutiveBoardMembers'));
-        $executiveBoardTableId = $this->getConfigValue('executiveBoardTableId', -1);
-        $executiveBoardMembers = $this->projectService->participantOptions($executiveBoardTableId, $executiveBoardTable);
-        $templateParameters = array_merge(
-          $templateParameters,
-          [
-            'streetAddressName01' => $this->getConfigValue('streetAddressName01'),
-            'streetAddressName02' => $this->getConfigValue('streetAddressName02'),
-            'streetAddressStreet' => $this->getConfigValue('streetAddressStreet'),
-            'streetAddressHouseNumber' => $this->getConfigValue('streetAddressHouseNumber'),
-            'streetAddressCity' => $this->getConfigValue('streetAddressCity'),
-            'streetAddressZIP' => $this->getConfigValue('streetAddressZIP'),
-            'streetAddressCountry' => $this->getConfigValue('streetAddressCountry'),
-
-            'phoneNumber' => $this->getConfigValue('phoneNumber'),
-
-            'bankAccountOwner' => $this->getConfigValue('bankAccountOwner'),
-            'bankAccountIBAN' => $this->getConfigValue('bankAccountIBAN'),
-            'bankAccountBLZ' => $this->getConfigValue('bankAccountBLZ'),
-            'bankAccountBIC' => $this->getConfigValue('bankAccountBIC'),
-            'bankAccountCreditorIdentifier' => $this->getConfigValue('bankAccountCreditorIdentifier'),
-
-            'memberTable' => $this->getConfigValue('memberTable', $this->l->t('ClubMembers')),
-            'memberTableId' => $this->getConfigValue('memberTableId', -1),
-            'executiveBoardTable' => $executiveBoardTable,
-            'executiveBoardTableId' => $execitiveBoardTableId,
-            'executiveBoardMembers' => $executiveBoardMember,
-            'userGroupMembers' => array_map(function($user) { return $user->getUID(); }, $this->group()->getUsers()),
-            'userGroups' => array_map(function($group) { return $group->getGID(); }, $this->groupManager()->search('')),
-            'orchestra' => $this->getConfigValue('orchestra'),
-
-            'dbserver' => $this->getConfigValue('dbserver'),
-            'dbname' => $this->getConfigValue('dbname'),
-            'dbuser' => $this->getConfigValue('dbuser'),
-            'dbpassword' => $this->getConfigValue('dbpassword'),
-            'encryptionkey' => $this->getConfigValue('encryptionkey'),
-
-            'shareowner', $this->getConfigValue('shareowner', ''),
-            'concertscalendar', $this->getConfigValue('concertscalendar', $this->l->t('concerts')),
-            'rehearsalscalendar', $this->getConfigValue('rehearsalscalendar', $this->l->t('rehearsals')),
-            'othercalendar', $this->getConfigValue('othercalendar', $this->l->t('other')),
-            'managementcalendar', $this->getConfigValue('managementcalendar', $this->l->t('management')),
-            'financecalendar', $this->getConfigValue('financecalendar', $this->l->t('finance')),
-            'eventduration', $this->getConfigValue('eventduration', '180'),
-
-            'sharedaddressbook', $this->getConfigValue('sharedaddressbook', $this->l->t('contacts')),
-
-            'sharedfolder', $this->getConfigValue('sharedfolder',''),
-            'projectsfolder', $this->getConfigValue('projectsfolder',''),
-            'projectsbalancefolder', $this->getConfigValue('projectsbalancefolder',''),
-          ]);
-
-        // musician ids of the officials
-        foreach (['president', 'secretary', 'treasurer'] as $prefix) {
-          foreach (['Id', 'UserId', 'GroupId'] as $postfix) {
-            $official = $prefix.$postfix;
-            $templateParameters[$official] = $this->getConfigValue($official, -1);
-          }
-        }
-
-        foreach (['smtp', 'imap'] as $proto) {
-          foreach (['server', 'port', 'secure'] as $key) {
-            $templateParameters[$proto.$key] =  $this->getConfigValue($proto.$key);
-          }
-        }
-        foreach (['user', 'password', 'fromname', 'fromaddress', 'testaddress', 'testmode'] as $key) {
-          $templateParameters['email'.$key] = $this->getConfigValue('email'.$key);
-        }
-
-        foreach (['Preview',
-                  'Archive',
-                  'Rehearsals',
-                  'Trashbin',
-                  'Template',
-                  'ConcertModule',
-                  'RehearsalsModule'] as $key) {
-          $templateParameters['redaxo'.$key] = $this->getConfigValue('redaxo'.$key);
-        }
-
-        foreach (['phpmyadmin',
-                  'phpmyadminoc',
-                  'sourcecode',
-                  'sourcedocs',
-                  'ownclouddev'] as $link) {
-          $templateParamerers[$link] = $this->getConfigValue($link);
+      // musician ids of the officials
+      foreach (['president', 'secretary', 'treasurer'] as $prefix) {
+        foreach (['Id', 'UserId', 'GroupId'] as $postfix) {
+          $official = $prefix.$postfix;
+          $templateParameters[$official] = $this->getConfigValue($official, -1);
         }
       }
 
-      return new TemplateResponse(
-        $this->appName(),
-        self::TEMPLATE,
-        $templateParameters,
-        'blank',
-      );
+      foreach (['smtp', 'imap'] as $proto) {
+        foreach (['server', 'port', 'secure'] as $key) {
+          $templateParameters[$proto.$key] =  $this->getConfigValue($proto.$key);
+        }
+      }
+      foreach (['user', 'password', 'fromname', 'fromaddress', 'testaddress', 'testmode'] as $key) {
+        $templateParameters['email'.$key] = $this->getConfigValue('email'.$key);
+      }
+
+      foreach (['Preview',
+                'Archive',
+                'Rehearsals',
+                'Trashbin',
+                'Template',
+                'ConcertModule',
+                'RehearsalsModule'] as $key) {
+        $templateParameters['redaxo'.$key] = $this->getConfigValue('redaxo'.$key);
+      }
+
+      foreach (['phpmyadmin',
+                'phpmyadminoc',
+                'sourcecode',
+                'sourcedocs',
+                'ownclouddev'] as $link) {
+        $templateParamerers[$link] = $this->getConfigValue($link);
+      }
     }
+
+    return new TemplateResponse(
+      $this->appName(),
+      self::TEMPLATE,
+      $templateParameters,
+      'blank',
+    );
   }
 
   /**
