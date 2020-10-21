c35ea3c15b94cf83e86bee2f37289b0f34a1be7f
Claus-Justus Heine <himself@claus-justus-heine.de>
2020-10-21 09:06:31 +0200

    Start with PME table views, provide an interface class

---
+++ lib/TableView/ITableView.php
@@ -0,0 +1,46 @@
+<?php
+/* Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2011-2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\TableView;
+
+interface ITableView
+{
+    /** Short title for heading. */
+    public function shortTitle();
+
+    /** Header text informations. */
+    public function headerText();
+
+    /** Show the underlying table. */
+    public function render();
+
+    /** Set table-navigation enable/disable. */
+    public function navigation();
+
+    /** Run underlying table-manager (phpMyEdit for now). */
+    public function execute();
+}
+
+// Local Variables: ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: nil ***
+// End: ***
