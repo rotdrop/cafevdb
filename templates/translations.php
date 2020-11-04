e4eb4651b1d7c8b925969fe29819a72c96cbad5f
Claus-Justus Heine <Claus-Justus.Heine@IANS.Uni-Stuttgart.DE>
2020-11-04 23:47:30 +0100

    Online translation editing.

---
+++ templates/translations.php
@@ -0,0 +1,74 @@
+<?php
+/* Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB;
+
+use \OCA\CAFEVDB\PageRenderer\Util\Navigation;
+
+$phraseOptions = [];
+foreach ($translations as $keyId => $data) {
+    $translatedLocales = [];
+    foreach ($data['translations'] as $locale => $translation) {
+	$translatedLocales[] = $locale;
+    }
+    $translatedLocales = implode(',', $translatedLocales);
+    $phraseOptions[] = [
+	'value' => $keyId,
+	'name'  => $data['key'],
+	'title' => $translatedLocales,
+        'data'  => [
+            'translations' => $data['translations'],
+        ],
+    ];
+}
+?>
+<div id="tabs-<?php echo $_['tabNr']; ?>" class="personalblock admin devel">
+  <form id="translations">
+    <select
+        id="<?php echo $appName; ?>-translation-phrases"
+        data-placeholder="<?php echo $l->t("Select a phrase to translate"); ?>"
+	class="translation-phrases"
+        name="translation-phrases"
+	title="<?php $toolTips['translation-phrases']; ?>" >
+      <option data-translations="dummy"></option>
+      <?php echo Navigation::selectOptions($phraseOptions); ?>
+    </select>
+    <select
+        id="<?php echo $appName; ?>"-translation-locales"
+        data-placeholder="<?php echo $l->t("Select a target locate (language)"); ?>"
+        class="translation-locales"
+        name="translation-locales"
+        title="<?php $toolTips['translation-locales']; ?>" >
+      <option></option>
+      <?php echo Navigation::simpleSelectOptions($languages, $language); ?>
+    </select>
+    <input type="checkbox" id="<?php echo $appName ?>-hide-translated" name="hide-translated" class="checkbox"/>
+    <label for="<?php echo $appName ?>-hide-translated" title="<?php echo $toolTips['hide-translated']; ?>" >
+      <?php echo $l->t('hide translated');?>
+    </label>
+  </form>
+  <div id="<?php echo $appName; ?>-translation-key" class="translation-key">
+  </div>
+  <textarea id="<?php echo $appName; ?>-translation-translation" class="translation-translation">
+  </textarea>
+  <div class="translation msg"></div>
+</div>
