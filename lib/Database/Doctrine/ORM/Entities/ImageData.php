ede8a1f882134fec8ca90e8a5b92b66859847648
Claus-Justus Heine <Claus-Justus.Heine@IANS.Uni-Stuttgart.DE>
2020-11-05 14:20:05 +0100

    indentation

--- lib/Database/Doctrine/ORM/Entities/ImageData.php
+++ lib/Database/Doctrine/ORM/Entities/ImageData.php
@@ -13,177 +13,177 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class ImageData
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
+  /**
+   * @var int
+   *
+   * @ORM\Column(name="id", type="integer", nullable=false)
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="IDENTITY")
+   */
+  private $id;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="item_id", type="integer", nullable=false)
-     */
-    private $itemId;
+  /**
+   * @var int
+   *
+   * @ORM\Column(name="item_id", type="integer", nullable=false)
+   */
+  private $itemId;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="item_table", type="string", length=128, nullable=false)
-     */
-    private $itemTable;
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="item_table", type="string", length=128, nullable=false)
+   */
+  private $itemTable;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="mime_type", type="string", length=128, nullable=true)
-     */
-    private $mimeType;
+  /**
+   * @var string|null
+   *
+   * @ORM\Column(name="mime_type", type="string", length=128, nullable=true)
+   */
+  private $mimeType;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="MD5", type="string", length=32, nullable=true, options={"fixed"=true})
-     */
-    private $md5;
+  /**
+   * @var string|null
+   *
+   * @ORM\Column(name="MD5", type="string", length=32, nullable=true, options={"fixed"=true})
+   */
+  private $md5;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="Data", type="text", length=0, nullable=true)
-     */
-    private $data;
+  /**
+   * @var string|null
+   *
+   * @ORM\Column(name="Data", type="text", length=0, nullable=true)
+   */
+  private $data;
 
-    /**
-     * Get id.
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+  /**
+   * Get id.
+   *
+   * @return int
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
 
-    /**
-     * Set itemid.
-     *
-     * @param int $itemid
-     *
-     * @return ImageData
-     */
-    public function setItemId($itemid)
-    {
-        $this->itemid = $itemId;
+  /**
+   * Set itemid.
+   *
+   * @param int $itemid
+   *
+   * @return ImageData
+   */
+  public function setItemId($itemid)
+  {
+    $this->itemid = $itemId;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * Get itemId.
-     *
-     * @return int
-     */
-    public function getItemId()
-    {
-        return $this->itemId;
-    }
+  /**
+   * Get itemId.
+   *
+   * @return int
+   */
+  public function getItemId()
+  {
+    return $this->itemId;
+  }
 
-    /**
-     * Set itemtable.
-     *
-     * @param string $itemTable
-     *
-     * @return ImageData
-     */
-    public function setItemtable($itemTable)
-    {
-        $this->itemtable = $itemTable;
+  /**
+   * Set itemtable.
+   *
+   * @param string $itemTable
+   *
+   * @return ImageData
+   */
+  public function setItemtable($itemTable)
+  {
+    $this->itemtable = $itemTable;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * Get itemtable.
-     *
-     * @return string
-     */
-    public function getItemTable()
-    {
-        return $this->itemTable;
-    }
+  /**
+   * Get itemtable.
+   *
+   * @return string
+   */
+  public function getItemTable()
+  {
+    return $this->itemTable;
+  }
 
-    /**
-     * Set mimetype.
-     *
-     * @param string|null $mimetype
-     *
-     * @return ImageData
-     */
-    public function setMimetype($mimeType = null)
-    {
-        $this->mimetype = $mimeType;
+  /**
+   * Set mimetype.
+   *
+   * @param string|null $mimetype
+   *
+   * @return ImageData
+   */
+  public function setMimetype($mimeType = null)
+  {
+    $this->mimetype = $mimeType;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * Get mimetype.
-     *
-     * @return string|null
-     */
-    public function getMimetype()
-    {
-        return $this->mimeType;
-    }
+  /**
+   * Get mimetype.
+   *
+   * @return string|null
+   */
+  public function getMimetype()
+  {
+    return $this->mimeType;
+  }
 
-    /**
-     * Set md5.
-     *
-     * @param string|null $md5
-     *
-     * @return ImageData
-     */
-    public function setMd5($md5 = null)
-    {
-        $this->md5 = $md5;
+  /**
+   * Set md5.
+   *
+   * @param string|null $md5
+   *
+   * @return ImageData
+   */
+  public function setMd5($md5 = null)
+  {
+    $this->md5 = $md5;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * Get md5.
-     *
-     * @return string|null
-     */
-    public function getMd5()
-    {
-        return $this->md5;
-    }
+  /**
+   * Get md5.
+   *
+   * @return string|null
+   */
+  public function getMd5()
+  {
+    return $this->md5;
+  }
 
-    /**
-     * Set data.
-     *
-     * @param string|null $data
-     *
-     * @return ImageData
-     */
-    public function setData($data = null)
-    {
-        $this->data = $data;
+  /**
+   * Set data.
+   *
+   * @param string|null $data
+   *
+   * @return ImageData
+   */
+  public function setData($data = null)
+  {
+    $this->data = $data;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * Get data.
-     *
-     * @return string|null
-     */
-    public function getData()
-    {
-        return $this->data;
-    }
+  /**
+   * Get data.
+   *
+   * @return string|null
+   */
+  public function getData()
+  {
+    return $this->data;
+  }
 }
