dd9c3128c609fe1309a1f18a5749f90b2a661c16
Claus-Justus Heine <himself@claus-justus-heine.de>
2020-11-23 11:30:29 +0100

    Start working on project meta.

--- lib/Service/ProjectService.php
+++ lib/Service/ProjectService.php
@@ -23,11 +23,13 @@
 namespace OCA\CAFEVDB\Service;
 
 use OCA\CAFEVDB\Database\EntityManager;
-use OCA\CAFEVDB\Database\Doctrine\ORM\Entities\Projects;
+use OCA\CAFEVDB\Database\Doctrine\ORM\Entities;
+use OCA\CAFEVDB\Database\Doctrine\ORM\Repositories\ProjectsRepository;
+use OCA\CAFEVDB\Storage\UserStorage;
 
 /**
- * @todo sync with database repository
- * OCA\CAFEVDB\Database\Doctrine\ORM\Repositories\ProjectsRepository
+ * General support service, kind of inconsequent glue between
+ * Doctrine\ORM and CAFEVDB\PageRenderer.
  */
 class ProjectService
 {
@@ -39,13 +41,21 @@ class ProjectService
   /** @var EntityManager */
   protected $entityManager;
 
+  /** @var ProjectsRepository */
+  private $repository;
+
+  /** @var OCA\CAFEVDB\Storage\UserStorage */
+  private $userStorage;
+
   public function __construct(
-    ConfigService $configService,
-    EntityManager $entityManager
+    ConfigService $configService
+    , EntityManager $entityManager
+    , UserStorage $userStorage
   ) {
     $this->configService = $configService;
     $this->entityManager = $entityManager;
-    $this->setDatabaseRepository(Projects::class);
+    $this->userStorage = $userStorage;
+    $this->repository = $this->getDatabaseRepository(Entities\Project::class);
   }
 
   /**Generate an option table with all participants, suitable to be
@@ -73,7 +83,7 @@ class ProjectService
     // simply fetch all participants
     $query = "SELECT Name,Vorname,MusikerId FROM ".$table." WHERE 1";
     $stmt = $this->entityManager->getConnction()->query($query);
-    while($row = $stmt->fetch()) {
+    while ($row = $stmt->fetch()) {
       $key = $row['MusikerId'];
       $name = $row['Vorname'].' '.$row['Name'];
       $flags = ($key == $musicianId) ? Navigation::SELECTED : 0;
@@ -99,6 +109,251 @@ class ProjectService
   public function fetchAll() {
     return $this->findAll();
   }
+
+  /**
+   * Check for the existence of the project folders. Returns an array
+   * of folders (balance and general files).
+   *
+   * @param int $projectId Id of the project.
+   *
+   * @param string $projectName Name of the project.
+   *
+   * @param bool|string If a string create only this folder.
+   *
+   * @return array Array of created folders.
+   *
+   */
+  public function ensureProjectFolders($projectId, $projectName = false, $only = false)
+  {
+    $project = $this->repository->find($projectId);
+    if (!$projectName) {
+      $projectName = $project['Name'];
+    } else if ($projectName != $project['Name']) {
+      return false;
+    }
+
+    $sharedFolder   = $this->getConfigValue('sharedfolder');
+    $projectsFolder = $this->getConfigValue('projectsfolder');
+    $balanceFolder  = $this->getConfigValue('projectsbalancefolder');
+
+    $paths = [ 'project' => '/'.$sharedFolder.'/'.$projectsFolder,
+               'balance' => '/'.$sharedFolder.'/'.$balanceFolder.'/'.$projectsFolder ];
+    $returnPaths = [];
+    foreach ($paths as $key => $path) {
+      if ($only && $key != $only) {
+        continue;
+      }
+      try {
+        $this->userStorage->ensureFolder($path);
+        $path .= "/".$project['Jahr'];
+        $this->userStorage->ensureFolder($path);
+        $path .= "/".$project['Name'];
+        $this->userStorage->ensureFolder($path);
+        $returnPaths[$key] = $path;
+      } catch (\Throwable $t) {
+        $this->logException($t);
+      }
+    }
+    return $returnPaths;
+  }
+
+  /**
+   * Remove the folder for the given project.
+   *
+   * @param Project|array Project entity or plain query result.
+   *
+   * @return bool Status
+   */
+  public function removeProjectFolders($oldProject)
+  {
+    $sharedFolder   = $this->getConfigValue('sharedfolder');
+    $projectsFolder = $this->getConfigValue('projectsfolder');
+    $balanceFolder  = $this->getConfigValue('projectsbalancefolder');
+
+    $prefixPath = [
+      'project' => '/'.$sharedFolder.'/'.$projectsFolder.'/',
+      'balance' => '/'.$sharedFolder.'/'.$balanceFolder.'/'.$projectsFolder."/",
+    ];
+
+    $userFolder = $this->rootFolder->getUserFolder($this->userID());
+
+    $fileView = \OC\Files\Filesystem::getView();
+
+    foreach($prefixPath as $key => $prefix) {
+
+      $oldPath = $prefix.$oldProject['Jahr']."/".$oldProject['Name'];
+      $this->userStorage->delete($oldPath);
+    }
+
+    return true;
+  }
+
+  /**
+   * Remove the folders for the given projectgs
+   *
+   * @param Project|array $newProject Project entity or plain query result.
+   *
+   * @param Project|array $oldProject Project entity or plain query result.
+   *
+   * @return array Array with newly renamed or created folders.
+   */
+  public static function renameProjectFolder($newProject, $oldProject)
+  {
+    $sharedFolder   = $this->getConfigValue('sharedfolder');
+    $projectsFolder = $this->getConfigValue('projectsfolder');
+    $balanceFolder  = $this->getConfigValue('projectsbalancefolder');
+
+    $prefixPath = [
+      'project' => '/'.$sharedFolder.'/'.$projectsFolder.'/',
+      'balance' => '/'.$sharedFolder.'/'.$balanceFolder.'/'.$projectsFolder."/",
+    ];
+
+    $returnPaths = [];
+    foreach ($prefixPath as $key => $prefix) {
+
+      $oldPath = $prefix.$oldProject['Jahr']."/".$oldProject['Name'];
+      $newPrefixPath = $prefix.$newProject['Jahr'];
+
+      $newPath = $newPrefixPath.'/'.$newProject['Name'];
+
+      if ($fileView->is_dir($oldPath)) {
+
+        // If the year has changed it may be necessary to create a new
+        // directory.
+        $this->userStorage->ensureFolder($newPrefixPath);
+        $this->userStorage->rename($oldPath, $newPath);
+
+        $returnPaths[$key] = $newPath;
+      } else {
+        // Otherwise there is nothing to move; we simply create the new directory.
+        $returnPaths = array_merge($returnPaths,
+                                   $this->ensureProjectFolders($projectId, $projectName, $only = $key));
+      }
+    }
+
+    return $returnPaths;
+  }
+
+  public function projectWikiLink($pageName)
+  {
+    $orchestra = $this>getConfigValue('orchestra');
+
+    return $orchestra.":projects:".$pageName;
+  }
+
+  /** Generate an automated overview. Actually, the orchestra-title
+   * should be made configurable.
+   */
+  public function generateWikiOverview($handle = false)
+  {
+/*
+  ====== Projekte der Camerata Academica Freiburg e.V. ======
+
+  ==== 2011 ====
+  * [[Auvergne2011|Auvergne]]
+  * [[Weihnachten2011]]
+
+  ==== 2012 ====
+  * [[Listenpunkt]]
+  * [[Blah]]
+
+  ==== 2013 ====
+  * [[Listenpunkt]]
+  */
+    $orchestra = $this->getConfigValue('orchestra');
+    $orchestra = $this->getConfigValue('streetAddressName=1', $orchestra);
+
+    $projects = $this->findAll();
+
+    $page = "====== ".($this->l->t('Projects of %s', [$orchestra]))."======\n\n";
+
+    $year = -1;
+    foreach ($projects as $project) {
+      if ($project['Jahr'] != $year) {
+        $year = $row['Jahr'];
+        $page .= "\n==== ".$year."====\n";
+      }
+      $name = $row['Name'];
+
+      $matches = false;
+      if (preg_match('/^(.*\D)?(\d{4})$/', $name, $matches) == 1) {
+        $bareName = $matches[1];
+        //$projectYear = $matches[2];
+      } else {
+        $bareName = $name;
+      }
+
+      // A page is tagged with the project name; if this ever should
+      // be changed (which is possible), then change-trigger should
+      // create a new page as copy from the old one and change the
+      // text of the old one to contain a link to the new page.
+
+      $page .= "  * [[".self::projectWikiLink($name)."|".$bareName."]]\n";
+    }
+
+    $pageName = self::projectWikiLink('projects');
+
+    // $wikiLocation = \OCP\Config::GetAppValue("dokuwikiembed", 'wikilocation', '');
+    // $dwembed = new \DWEMBED\App($wikiLocation);
+    // $dwembed->putPage($pagename, $page,
+    //                   [ "sum" => "Automatic CAFEVDB synchronization",
+    //                     "minor" => true ]);
+
+  }
+
+//     /**Generate an almost empty project page. This spares people the
+//      * need to click on "new page".
+//      *
+//      * - We insert a proper title heading
+//      *
+//      * - We insert a sub-title "Contacts"
+//      *
+//      * - We insert a sub-title "Financial Arrangements"
+//      *
+//      * - We insert a sub-title "Location"
+//      */
+//     public static function generateProjectWikiPage($projectId, $projectName, $handle)
+//     {
+//       $orchestra = Config::$opts['orchestra']; // for the name-space
+
+//       $page = L::t('====== Project %s ======
+
+// ===== Forword =====
+
+// This wiki-page is useful to store selected project related
+// informations in comfortable and structured form. This can be useful
+// for "permant information" like details about supplementary fees,
+// contact informations and the like. In particular, this page could be
+// helpful to reduce unnecessary data-digging in our email box.
+
+// ===== Contacts =====
+// Please add any relevant email and mail-adresses here. Please use the wiki-syntax
+// * [[foobar@important.com|Mister Universe]]
+
+// ===== Financial Arrangements =====
+// Please add any special financial arrangements here. For example:
+// single-room fees, double-roome fees. Please consider using an
+// unordered list for this like so:
+//   * single room fee: 3000€
+//   * double room fee: 6000€
+//   * supplemenrary fee for Cello-players: 1500€
+
+// ===== Location =====
+// Whatever.',
+//                    array($projectName));
+
+//       $pagename = self::projectWikiLink($projectName);
+
+//       $wikiLocation = \OCP\Config::GetAppValue("dokuwikiembed", 'wikilocation', '');
+//       $dwembed = new \DWEMBED\App($wikiLocation);
+//       $dwembed->putPage($pagename, $page,
+//                         array("sum" => "Automatic CAFEVDB synchronization",
+//                               "minor" => true));
+
+//     }
+
+//   }; // class Projects
+
 }
 
 // Local Variables: ***
---
+++ lib/Storage/UserStorage.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2011-2014, 2016, 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Storage;
+
+use OCP\IL10N;
+use OCP\ILogger;
+use OCP\Files\IRootFolder;
+use OCP\Files\Folder;
+
+/**
+ * Some tweaks to for the user-folder stuff.
+ */
+class UserStorage
+{
+  use \OCA\CAFEVDB\Traits\LoggerTrait;
+
+  /** @var string */
+  protected $userId;
+
+  /** @var Folder */
+  protected $rootFolder;
+
+  /** @var Folder */
+  protected $userFolder;
+
+  public function __construct(
+    string $userId
+    , IRootFolder $rootFolder
+    , ILogger $logger
+    , IL10N $l10n
+  ) {
+    $this->userId = $userId;
+    $this->rootFolder = $rootFolder;
+    $this->logger = $logger;
+    $this->l = $l10n;
+    $this->userFolder = $this->rootFolder->getUserFolder($this->userId);
+  }
+
+  /**
+   * @return string The current user-id
+   */
+  public function userId():string
+  {
+    return $this->userId;
+  }
+
+
+  /**
+   * @return \OCP\Files\Folder The user-folder
+   */
+  public function get():Folder
+  {
+    return $this->userFolder;
+  }
+
+  /**
+   * @return \OCP\Files\Folder The root-folder
+   */
+  public function getRoot():Folder
+  {
+    return $this->rootFolder;
+  }
+
+  public function delete($path)
+  {
+    $this->userFolder->get($path)->delete();
+  }
+
+  public function rename($oldPath, $newPath)
+  {
+    $this->userFolder->get($oldPath)->move($newpath);
+  }
+
+  /**
+   * Make sure the given directory exists. If the given path existis
+   * and is not a directory, it will be moved out of the way or
+   * deleted.
+   *
+   * @param string $path The path the check and possibly create.
+   *
+   * @return \OCP\Files\Folder The folder object pointing to the given
+   * path.
+   *
+   * @throws \Exception
+   */
+  public function ensureFolder(string $path)
+  {
+    try {
+      $node = $this->userFolder->get($path);
+      if ($node->getType() != \OCP\FileInfo::TYPE_FOLDER) {
+        $node->delete();
+        throw new \OCP\Files\NotFoundException($this->l->t("Deleted non-directory %s", [$path]));
+      }
+    } catch (\OCP\Files\NotFoundException $e) {
+      $node = $userFolder->newFolder($path);
+    }
+    return $node;
+  }
+}
+
+// Local Variables: ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: nil ***
+// End: ***
