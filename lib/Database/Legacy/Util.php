6f05adbeeecb0db9b99e74f3bb2215f6fee4a912
Claus-Justus Heine <himself@claus-justus-heine.de>
2020-11-25 12:02:18 +0100

    cosmetics

--- lib/Database/Legacy/Util.php
+++ lib/Database/Legacy/Util.php
@@ -1,3 +1,27 @@
+<?php
+/* Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2011-2014, 2016, 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Database\Legacy;
+
 class Util
 {
   /**
@@ -49,10 +73,10 @@ class Util
       return false;
     }
 
-    $joinDflt = array('table' => false,
-                      'tablename' => false,
-                      'column' => true,
-                      'verbatim' => false);
+    $joinDflt = [ 'table' => false,
+                  'tablename' => false,
+                  'column' => true,
+                  'verbatim' => false ];
 
     $firstTable = array_merge($joinDflt, $firstTable);
     $table = $firstTable['table'];
@@ -66,7 +90,7 @@ class Util
     }
     $join .= $nl;
     $select = 'SELECT'.$nl;
-    foreach($joinStructure as $joinColumn => $joinedColumn) {
+    foreach ($joinStructure as $joinColumn => $joinedColumn) {
       // Set default options. The default options array MUST come
       // first, later arrays override (see manual for array_merge())
       $joinedColumn = array_merge($joinDflt, $joinedColumn);
