a437e24584c97ce7b1f344f74c4de4210bf49a89
Claus-Justus Heine <himself@claus-justus-heine.de>
2020-11-26 12:46:02 +0100

    cosmetics

--- js/before-ready.js
+++ js/before-ready.js
@@ -19,217 +19,215 @@
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-$(document).ready(function() {
+$(function() {
 
-    // ???? needed ????
-    $.widget("ui.dialog", $.ui.dialog, {
-        _allowInteraction: function(event) {
-            return !!$(event.target).closest(".mce-container").length || this._super( event );
-        }
-    });
-
-    if (false) {
-        // should somehow depend on debug mode.
-        $(document).on('ajaxError', function(event, xhr, settings, error) {
-            OC.dialogs.alert(t('cafevdb', 'Unhandled internal AJAX error:')+
-                             '<br/>'+
-                             t('cafevdb', 'Error')+': '+error+
-                             '<br/>'+
-                             t('cafevdb', 'URL')+': '+settings.url,
-                             t('cafevdb', 'Error'),
-                             undefined, true, true);
-            return false;
-        });
+  // ???? needed ????
+  $.widget("ui.dialog", $.ui.dialog, {
+    _allowInteraction: function(event) {
+      return !!$(event.target).closest(".mce-container").length || this._super( event );
     }
+  });
 
-    var content = $('#content');
-
-    content.on('cafevdb:content-update', function(event) {
-	$.fn.cafevTooltip.remove(); // remove any left-over items.
+  if (false) {
+    // should somehow depend on debug mode.
+    $(document).on('ajaxError', function(event, xhr, settings, error) {
+      OC.dialogs.alert(t('cafevdb', 'Unhandled internal AJAX error:')+
+                       '<br/>'+
+                       t('cafevdb', 'Error')+': '+error+
+                       '<br/>'+
+                       t('cafevdb', 'URL')+': '+settings.url,
+                       t('cafevdb', 'Error'),
+                       undefined, true, true);
+      return false;
     });
+  }
 
-    content.on('chosen:showing_dropdown', 'select', function(event, params)   {
-        var results = params.chosen.search_results;
-        var menuItems = results.find('li');
-	menuItems.cafevTooltip({placement:'right'});
-	if (!CAFEVDB.toolTipsEnabled) {
-	    menuItems.cafevTooltip('disable');
-	}
-	$.fn.cafevTooltip.remove(); // remove any left-over items.
-    });
+  var content = $('#content');
 
-    // Any pending form-submit which has not been caught otherwise is
-    // here intercepted and redirected to the page-loader in order to
-    // reduce load-time and to record usable history information.
-    content.on('submit', 'form', function(event) {
-        var form = $(this);
-        var action = form.attr('action');
-        if (action != '') {
-            // not for us, external target.
-            return true;
+  content.on('cafevdb:content-update', function(event) {
+    $.fn.cafevTooltip.remove(); // remove any left-over items.
+  });
+
+  content.on('chosen:showing_dropdown', 'select', function(event, params)   {
+    var results = params.chosen.search_results;
+    var menuItems = results.find('li');
+    menuItems.cafevTooltip({placement:'right'});
+    if (!CAFEVDB.toolTipsEnabled) {
+      menuItems.cafevTooltip('disable');
+    }
+    $.fn.cafevTooltip.remove(); // remove any left-over items.
+  });
+
+  // Any pending form-submit which has not been caught otherwise is
+  // here intercepted and redirected to the page-loader in order to
+  // reduce load-time and to record usable history information.
+  content.on('submit', 'form', function(event) {
+    var form = $(this);
+    var action = form.attr('action');
+    if (action != '') {
+      // not for us, external target.
+      return true;
+    }
+    var post = form.serialize();
+    //alert('post: '+post);
+    CAFEVDB.Page.loadPage(post);
+    return false;
+  });
+
+  // Any pending form-submit which has not been caught otherwise is
+  // here intercepted and redirected to the page-loader in order to
+  // reduce load-time and to record usable history information.
+  content.on('click', ':submit', function(event) {
+    var form = $(this.form);
+    var post = form.serialize();
+    var self = $(this);
+    if (self.attr('name')) {
+      var obj = {};
+      obj[self.attr('name')] = self.val();
+      post += '&' + $.param(obj);
+    }
+    //alert('post: '+post);
+    CAFEVDB.Page.loadPage(post);
+    return false;
+  });
+
+  // Intercept app-navigation events here and redirect to the page
+  // loader
+  content.on('click', 'ul#navigation-list li a', function(event) {
+    const target = $(event.target);
+    if (target.is('.nav-heading a')) {
+      // don't recurse on nav-heading which just should close the sidebar.
+      return true;
+    }
+    var post = $(this).data('post');
+    CAFEVDB.Page.loadPage(post);
+    //alert('post: '+post);
+    return false;
+  });
+
+
+  const musiciansCallback = {
+    callback: function(selector, parameters, resizeCB) {
+
+      if (parameters.reason == 'tabChange') {
+        resizeCB();
+        return;
+      }
+
+      var container = $(selector);
+      CAFEVDB.exportMenu(selector);
+
+      container.find('div.photo, #cafevdb_inline_image_wrapper').
+        off('click', 'img.zoomable').
+        on('click', 'img.zoomable', function(event) {
+          event.preventDefault();
+          CAFEVDB.Photo.popup(this);
+          return false;
+        });
+
+      CAFEVDB.Musicians.ready(container);
+
+      $(':button.musician-instrument-insurance').click(function(event) {
+        event.preventDefault();
+        var values = $(this).attr('name');
+
+        CAFEVDB.Page.loadPage($(this).attr('name'));
+
+        return false;
+      });
+
+      if (container.find('#contact_photo_upload').length > 0) {
+        var idField = container.find('input[name="PME_data_Id"]');
+        var recordId = -1;
+        if (idField.length > 0) {
+          recordId = idField.val();
         }
-        var post = form.serialize();
-        //alert('post: '+post);
-        CAFEVDB.Page.loadPage(post);
-        return false;
-    });
+        CAFEVDB.Photo.ready(recordId, 'MusicianPhoto', resizeCB);
+      } else {
+        container.find('div.photo, span.photo').imagesLoaded(resizeCB);
+      }
+    },
+    context: CAFEVDB,
+    parameters: []
+  };
 
-    // Any pending form-submit which has not been caught otherwise is
-    // here intercepted and redirected to the page-loader in order to
-    // reduce load-time and to record usable history information.
-    content.on('click', ':submit', function(event) {
-        var form = $(this.form);
-        var post = form.serialize();
-        var self = $(this);
-        if (self.attr('name')) {
-            var obj = {};
-            obj[self.attr('name')] = self.val();
-            post += '&' + $.param(obj);
-        }
-        //alert('post: '+post);
-        CAFEVDB.Page.loadPage(post);
-        return false;
-    });
+  PHPMYEDIT.addTableLoadCallback('all-musicians', musiciansCallback);
+  PHPMYEDIT.addTableLoadCallback('add-musicians', musiciansCallback);
 
-    // Intercept app-navigation events here and redirect to the page
-    // loader
-    content.on('click', 'ul#navigation-list li a', function(event) {
-	const target = $(event.target);
-	if (target.is('.nav-heading a')) {
-	    // don't recurse on nav-heading which just should close the sidebar.
-	    return true;
-	}
-        var post = $(this).data('post');
-        CAFEVDB.Page.loadPage(post);
-        //alert('post: '+post);
-        return false;
-    });
+  PHPMYEDIT.addTableLoadCallback('project-extra', {
+    callback: function(selector, parameters, resizeCB) {
+
+      if (parameters.reason != 'dialogOpen') {
+        resizeCB();
+        return;
+      }
+
+      CAFEVDB.ProjectExtra.ready(selector, resizeCB);
+    },
+    context: CAFEVDB,
+    parameters: []
+  });
 
 
-    const musiciansCallback = {
-        callback: function(selector, parameters, resizeCB) {
+  PHPMYEDIT.addTableLoadCallback('instruments', {
+    callback: function(selector, parameters, resizeCB) {
+      resizeCB();
+    },
+    context: CAFEVDB,
+    parameters: []
+  });
 
-            if (parameters.reason == 'tabChange') {
-                resizeCB();
-                return;
-            }
+  PHPMYEDIT.addTableLoadCallback('instrument-families', {
+    callback: function(selector, parameters, resizeCB) {
+      resizeCB();
+    },
+    context: CAFEVDB,
+    parameters: []
+  });
 
-            var container = $(selector);
-            CAFEVDB.exportMenu(selector);
+  PHPMYEDIT.addTableLoadCallback('project-payments', {
+    callback: function(selector, parameters, resizeCB) {
+      resizeCB();
+    },
+    context: CAFEVDB,
+    parameters: []
+  });
 
-            container.find('div.photo, #cafevdb_inline_image_wrapper').
-                off('click', 'img.zoomable').
-                on('click', 'img.zoomable', function(event) {
-                event.preventDefault();
-                CAFEVDB.Photo.popup(this);
-                return false;
-            });
+  PHPMYEDIT.addTableLoadCallback('debit-notes', {
+    callback: function(selector, parameters, resizeCB) {
 
-            CAFEVDB.Musicians.ready(container);
+      if (parameters.reason != 'dialogOpen') {
+        resizeCB();
+        return;
+      }
 
-            $(':button.musician-instrument-insurance').click(function(event) {
-                event.preventDefault();
-                var values = $(this).attr('name');
+      CAFEVDB.DebitNotes.ready(selector, resizeCB);
+    },
+    context: CAFEVDB,
+    parameters: []
+  });
 
-                CAFEVDB.Page.loadPage($(this).attr('name'));
+  CAFEVDB.addReadyCallback(function() {
+    CAFEVDB.exportMenu();
 
-                return false;
-            });
+    CAFEVDB.pmeTweaks();
 
-            if (container.find('#contact_photo_upload').length > 0) {
-                var idField = container.find('input[name="PME_data_Id"]');
-                var recordId = -1;
-                if (idField.length > 0) {
-                    recordId = idField.val();
-                }
-                CAFEVDB.Photo.ready(recordId, 'MusicianPhoto', resizeCB);
-            } else {
-                container.find('div.photo, span.photo').imagesLoaded(resizeCB);
-            }
-        },
-        context: CAFEVDB,
-        parameters: []
-    };
+    CAFEVDB.toolTipsInit();
 
-    PHPMYEDIT.addTableLoadCallback('all-musicians', musiciansCallback);
-    PHPMYEDIT.addTableLoadCallback('add-musicians', musiciansCallback);
+    // Prevent drag&drop outside allowed areas.
+    window.addEventListener("dragover", function(e){
+      e = e || event;
+      e.preventDefault();
+    }, false);
+    window.addEventListener("drop", function(e){
+      e = e || event;
+      e.preventDefault();
+    }, false);
 
-    PHPMYEDIT.addTableLoadCallback('project-extra', {
-        callback: function(selector, parameters, resizeCB) {
-
-            if (parameters.reason != 'dialogOpen') {
-                resizeCB();
-                return;
-            }
-
-            CAFEVDB.ProjectExtra.ready(selector, resizeCB);
-        },
-        context: CAFEVDB,
-        parameters: []
-    });
-
-
-    PHPMYEDIT.addTableLoadCallback('instruments', {
-        callback: function(selector, parameters, resizeCB) {
-            resizeCB();
-        },
-        context: CAFEVDB,
-        parameters: []
-    });
-
-    PHPMYEDIT.addTableLoadCallback('instrument-families', {
-        callback: function(selector, parameters, resizeCB) {
-            resizeCB();
-        },
-        context: CAFEVDB,
-        parameters: []
-    });
-
-    PHPMYEDIT.addTableLoadCallback('project-payments', {
-        callback: function(selector, parameters, resizeCB) {
-            resizeCB();
-        },
-        context: CAFEVDB,
-        parameters: []
-    });
-
-    PHPMYEDIT.addTableLoadCallback('debit-notes', {
-        callback: function(selector, parameters, resizeCB) {
-
-            if (parameters.reason != 'dialogOpen') {
-                resizeCB();
-                return;
-            }
-
-            CAFEVDB.DebitNotes.ready(selector, resizeCB);
-        },
-        context: CAFEVDB,
-        parameters: []
-    });
-
-    CAFEVDB.addReadyCallback(function() {
-        CAFEVDB.exportMenu();
-
-        CAFEVDB.pmeTweaks();
-
-        CAFEVDB.toolTipsInit();
-
-        // Prevent drag&drop outside allowed areas.
-        window.addEventListener("dragover", function(e){
-            e = e || event;
-            e.preventDefault();
-        }, false);
-        window.addEventListener("drop", function(e){
-            e = e || event;
-            e.preventDefault();
-        }, false);
-
-    });
+  });
 
 });
 
 // Local Variables: ***
-// js-indent-level: 4 ***
-// js3-indent-level: 4 ***
-// js3-label-indent-offset: -2 ***
+// js-indent-level: 2 ***
 // End: ***
