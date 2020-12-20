f590dbd90e31ab5ad5e42e741d1c1616e2690011
Claus-Justus Heine <himself@claus-justus-heine.de>
2020-12-20 23:48:02 +0100

    Start joining things.

--- lib/PageRenderer/ProjectParticipants.php
+++ lib/PageRenderer/ProjectParticipants.php
@@ -42,6 +42,8 @@ class ProjectParticipants extends PMETableViewBase
   const TEMPLATE = 'project-participants';
   const CSS_CLASS = self::TEMPLATE;
   const TABLE = 'ProjectParticipants';
+  const MUSICIANS_TABLE = 'Musicians';
+  const PROJECTS_TABLE = 'Projects';
 
   public function __construct(
     ConfigService $configService
@@ -86,6 +88,11 @@ class ProjectParticipants extends PMETableViewBase
       throw new \InvalidArgumentException('Project-id and/or -name must be given ('.$projectName.' / '.$projectId.').');
     }
 
+    $opts['filters']['AND'] = [
+      "PMEtable0.project_id = $projectId",
+      "PMEtable0.disabled <= ".intval($this->showDisabled),
+    ];
+
     // Number of records to display on the screen
     // Value of -1 lists all records in a table
     $opts['inc'] = -1;
@@ -110,79 +117,13 @@ class ProjectParticipants extends PMETableViewBase
     // Options you wish to give the users
     // A - add,  C - change, P - copy, V - view, D - delete,
     // F - filter, I - initial sort suppressed
-    $opts['options'] = 'ACPVDF';
+    $opts['options'] = 'CPVDFM';
 
     // Number of lines to display on multiple selection filters
     $opts['multiple'] = '6';
 
-    // Display special page elements
-    $opts['display'] =  Util::arrayMergeRecursive(
-      $opts['display'],
-      [
-        'form'  => true,
-        //'query' => true,
-        'sort'  => true,
-        'time'  => true,
-        'tabs'  => false
-    ]);
-
-    $musIdIdx = count($opts['fdd']);
-    $opts['fdd']['musician_id'] = array(
-      'tab'      => [ 'id' => 'musician' ],
-      'name'     => $this->l->t('Musician-Id'),
-      'input'    => 'R',
-      'select'   => 'T',
-      'options'  => 'LACPD',
-      'maxlen'   => 5,
-      'align'    => 'right',
-      'default'  => '0',
-      'sort'     => true,
-      );
-
-    $projIdIdx = count($opts['fdd']);
-    $opts['fdd']['project_id'] = array(
-      'tab'      => [ 'id' => 'musician' ],
-      'name'     => $this->l->t('Project-Id'),
-      'input'    => 'R',
-      'select'   => 'T',
-      'options'  => 'LACPD',
-      'maxlen'   => 5,
-      'align'    => 'right',
-      'default'  => '0',
-      'sort'     => true,
-      );
-
-    ///@@@@@@@@@@@@@@@@@@@@@
-
-//     // Name of field which is the unique key
-//     $opts['key'] = 'Id';
-
-//     // Type of key field (int/real/string/date etc.)
-//     $opts['key_type'] = 'int';
-
-//     // Sorting field(s)
-//     $opts['sort_field'] = array('sort_order','Voice','-SectionLeader','Name','Vorname');
-
-//     // GROUP BY clause, if needed.
-//     $opts['groupby_fields'] = array('Id', 'ProjectInstrumentId');
-
-//     $opts['filters'] = "PMEtable0.Disabled <= ".intval($this->showDisabled);
-
-//     // Options you wish to give the users
-//     // A - add,  C - change, P - copy, V - view, D - delete,
-//     // F - filter, I - initial sort suppressed
-//     // This is a view, undeletable.
-//     $opts['options'] = 'CPVDFM';
-
-//     // Number of lines to display on multiple selection filters
-//     $opts['multiple'] = '6';
-
-//     // Navigation style: B - buttons (default), T - text links, G - graphic links
-//     // Buttons position: U - up, D - down (default)
-//     //$opts['navigation'] = 'DB';
-
-//     $export = Navigation::tableExportButton();
-//     $opts['buttons'] = Navigation::prependTableButton($export, true);
+//  $export = Navigation::tableExportButton();
+//  $opts['buttons'] = Navigation::prependTableButton($export, true);
 
 //     // count number of finance fields
 //     $extraFinancial = 0;
@@ -197,78 +138,89 @@ class ProjectParticipants extends PMETableViewBase
 //       $financeTab = 'project';
 //     }
 
-//     // Display special page elements
-//     $opts['display'] = array_merge(
-//       $opts['display'],
-//       array(
-//         'form'  => true,
-//         //'query' => true,
-//         'sort'  => true,
-//         'time'  => true,
-//         'tabs' => self::tableTabs($userExtraFields, $useFinanceTab),
-//         'navigation' => 'VCD'
-//         ));
+    // Display special page elements
+    $opts['display'] =  Util::arrayMergeRecursive(
+      $opts['display'],
+      [
+        'form'  => true,
+        //'query' => true,
+        'sort'  => true,
+        'time'  => true,
+        'tabs'  => false,
+        //'tabs' => self::tableTabs($userExtraFields, $useFinanceTab),
+        'navigation' => 'VCD',
+    ]);
 
-//     // Set default prefixes for variables
-//     $opts['js']['prefix']               = 'PME_js_';
-//     $opts['dhtml']['prefix']            = 'PME_dhtml_';
-//     $opts['cgi']['prefix']['operation'] = 'PME_op_';
-//     $opts['cgi']['prefix']['sys']       = 'PME_sys_';
-//     $opts['cgi']['prefix']['data']      = 'PME_data_';
+    $projIdIdx = count($opts['fdd']);
+    $opts['fdd']['project_id'] = array(
+      'tab'      => [ 'id' => 'musician' ],
+      'name'     => $this->l->t('Project-Id'),
+      'input'    => 'R',
+      'select'   => 'T',
+      'options'  => 'LACPDV',
+      'maxlen'   => 5,
+      'align'    => 'right',
+      'default'  => '0',
+      'sort'     => true,
+      );
 
-//     //$opts['cgi']['append']['PME_sys_fl'] = 1;
+    $musIdIdx = count($opts['fdd']);
+    $opts['fdd']['musician_id'] = array(
+      'tab'      => [ 'id' => 'misc' ],
+      'name'     => $this->l->t('Musician-Id'),
+      'input'    => 'R',
+      'input|AP' => 'RH',
+      'input'    => 'R',
+      'select'   => 'T',
+      'options'  => 'LACPDV',
+      'maxlen'   => 5,
+      'align'    => 'right',
+      'default'  => '0',
+      'sort'     => true,
+    );
 
-//     /* Get the user's default language and use it if possible or you can
-//        specify particular one you want to use. Refer to official documentation
-//        for list of available languages. */
-//     //  $opts['language'] = $_SERVER['HTTP_ACCEPT_LANGUAGE'] . '-UTF8';
+    // initiate the join with the musicians table
+    $joinIndex[self::MUSICIANS_TABLE] = count($opts['fdd']);
+    $opts['fdd']['musicians_key'] = [
+      'tab' => 'all',
+      'name' => $this->l->t('Musician'),
+      'input' => 'V',
+      'sql' => 'musician_id',
+      'options' => '',
+      'sort' => true,
+      'values' => [
+        'table' => self::MUSICIANS_TABLE,
+        'column' => 'id',
+        'description' => [
+          'columns' => [ 'name', 'first_name' ],
+          'divs' => [ ', ' ],
+        ],
+        'join' => '$main_table.`musician_id` = $join_table.`id`'
+      ],
+    ];
 
-//     /* Table-level filter capability. If set, it is included in the WHERE clause
-//        of any generated SELECT statement in SQL query. This gives you ability to
-//        work only with subset of data from table.
 
-//        $opts['filters'] = "column1 like '%11%' AND column2<17";
-//        $opts['filters'] = "section_id = 9";
-//        $opts['filters'] = "PMEtable0.sessions_count > 200";
-//     */
+    // $musFirstNameIdx = count($opts['fdd']);
+    // $opts['fdd']['first_name'] = [
+    //   'name'     => $this->l->t('First Name'),
+    //   'select'   => 'T',
+    //   'maxlen'   => 384,
+    //   'sort'     => true,
+    //   'sql'     => '`PMEjoin'.$musFirstNameIdx.'`.`first_name`',
+    //   'tab'      => [ 'id' => 'tab-all' ], // display on all tabs, or just give -1
+    //   'values' => [
+    //     'table'  => self::MUSICIANS_TABLE,
+    //     'column' => 'id',
+    //     'description' => 'first_name',
+    //     'join' => '$join_table.id = $main_table.musician_id',
+    //   ],
+    // ];
 
-//     /* Field definitions
 
-//        Fields will be displayed left to right on the screen in the order in which they
-//        appear in generated list. Here are some most used field options documented.
+    // GROUP BY clause, if needed. Should come last
+    $opts['groupby_fields'] = [ 'PMEtable.projectId', PMEtable0.musicianId, /* 'ProjectInstrumentId' */ ];
 
-//        ['name'] is the title used for column headings, etc.;
-//        ['maxlen'] maximum length to display add/edit/search input boxes
-//        ['trimlen'] maximum length of string content to display in row listing
-//        ['width'] is an optional display width specification for the column
-//        e.g.  ['width'] = '100px';
-//        ['mask'] a string that is used by sprintf() to format field output
-//        ['sort'] true or false; means the users may sort the display on this column
-//        ['strip_tags'] true or false; whether to strip tags from content
-//        ['nowrap'] true or false; whether this field should get a NOWRAP
-//        ['select'] T - text, N - numeric, D - drop-down, M - multiple selection
-//        ['options'] optional parameter to control whether a field is displayed
-//        L - list, F - filter, A - add, C - change, P - copy, D - delete, V - view
-//        Another flags are:
-//        R - indicates that a field is read only
-//        W - indicates that a field is a password field
-//        H - indicates that a field is to be hidden and marked as hidden
-//        ['URL'] is used to make a field 'clickable' in the display
-//        e.g.: 'mailto:$value', 'http://$value' or '$page?stuff';
-//        ['URLtarget']  HTML target link specification (for example: _blank)
-//        ['textarea']['rows'] and/or ['textarea']['cols']
-//        specifies a textarea is to be used to give multi-line input
-//        e.g. ['textarea']['rows'] = 5; ['textarea']['cols'] = 10
-//        ['values'] restricts user input to the specified constants,
-//        e.g. ['values'] = array('A','B','C') or ['values'] = range(1,99)
-//        ['values']['table'] and ['values']['column'] restricts user input
-//        to the values found in the specified column of another table
-//        ['values']['description'] = 'desc_column'
-//        The optional ['values']['description'] field allows the value(s) displayed
-//        to the user to be different to those in the ['values']['column'] field.
-//        This is useful for giving more meaning to column values. Multiple
-//        descriptions fields are also possible. Check documentation for this.
-//     */
+    ///@@@@@@@@@@@@@@@@@@@@@
 
 //     $opts['fdd'] = array();
 
