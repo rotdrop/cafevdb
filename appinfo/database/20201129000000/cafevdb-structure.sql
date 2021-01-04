1964f5672a08a54c8752135c6b0df2ba0306c1b0
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-01-04 12:19:13 +0100

    Use lowercase for fieldname.

--- appinfo/database/20201129000000/cafevdb-structure.sql
+++ appinfo/database/20201129000000/cafevdb-structure.sql
@@ -25,16 +25,16 @@ DELIMITER $$
 CREATE OR REPLACE PROCEDURE generateNumbers(IN min INT)
 BEGIN
     CREATE TABLE IF NOT EXISTS numbers
-        ( N INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY )
+        ( n INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY )
         ENGINE memory
-        AS SELECT 1 N UNION ALL SELECT 2;
+        AS SELECT 1 n UNION ALL SELECT 2;
     SELECT COUNT(*) FROM numbers INTO @max;
     IF @max = 0 THEN
-        INSERT INTO numbers SELECT 1 N UNION ALL SELECT 2;
+        INSERT INTO numbers SELECT 1 n UNION ALL SELECT 2;
         SET @max = 2;
     END IF;
     WHILE @max < min DO
-        INSERT IGNORE INTO numbers SELECT (hi.N-1)*(SELECT COUNT(*) FROM numbers)+(lo.N-1)+1 AS N
+        INSERT IGNORE INTO numbers SELECT (hi.n-1)*(SELECT COUNT(*) FROM numbers)+(lo.n-1)+1 AS n
           FROM numbers lo, numbers hi;
         SELECT COUNT(*) FROM numbers INTO @max;
     END WHILE;
