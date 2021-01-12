ceaff85e4caa71c7056a74e7edfa7eaf9556be2a
Claus-Justus Heine <Claus-Justus.Heine@IANS.Uni-Stuttgart.DE>
2021-01-12 19:51:29 +0100

    Some general things for user input validation (e.g. money values).

---
+++ lib/Controller/ValidationController.php
@@ -0,0 +1,71 @@
+<?php
+/* Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Controller;
+
+use OCP\AppFramework\Controller;
+use OCP\IRequest;
+
+use OCA\CAFEVDB\Service\ConfigService;
+use OCA\CAFEVDB\Service\RequestParameterService;
+
+class ValidationController extends Controller {
+  use \OCA\CAFEVDB\Traits\ResponseTrait;
+  use \OCA\CAFEVDB\Traits\ConfigTrait;
+
+  /** @var \OCA\CAFEVDB\Service\ParametereService */
+  private $parameterService;
+
+  /** @var \OCA\CAFEVDB\Service\EventsService */
+  private $projectsService;
+
+  public function __construct(
+    $appName
+    , IRequest $request
+    , RequestParameterService $parameterService
+    , ConfigService $configService
+  ) {
+    parent::__construct($appName, $request);
+    $this->parameterService = $parameterService;
+    $this->configService = $configService;
+    $this->l = $this->l10N();
+  }
+
+  /**
+   * @NoAdminRequired
+   */
+  public function serviceSwitch($topic)
+  {
+    switch ($topic) {
+    case 'monetary_value':
+      break;
+    }
+    return self::grumble($this->l->t('Unknown Request'));
+  }
+
+
+}
+
+// Local Variables: ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: nil ***
+// End: ***
