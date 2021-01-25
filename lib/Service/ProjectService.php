5281afc825f1486012ed6f8e813c5b38cf8e55fd
Claus-Justus Heine <Claus-Justus.Heine@IANS.Uni-Stuttgart.DE>
2021-01-25 13:36:11 +0100

    Adjust for changed column names.

--- lib/Service/ProjectService.php
+++ lib/Service/ProjectService.php
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2011-2014, 2016, 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2011-2014, 2016, 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -809,7 +809,7 @@ Whatever.',
       return false;
     }
 
-    $musicianName = $musician['firstName'].' '.$musician['name'];
+    $musicianName = $musician['firstName'].' '.$musician['surName'];
 
     // check for already registered
     $exists = $project['participants']->exists(function($key, $participant) use ($musician) {
