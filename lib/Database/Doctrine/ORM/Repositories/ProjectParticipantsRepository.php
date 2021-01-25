3012ed16df0f313bdd7ba506ae1f7db0254152da
Claus-Justus Heine <Claus-Justus.Heine@IANS.Uni-Stuttgart.DE>
2021-01-25 21:28:39 +0100

    Initial revision.

---
+++ lib/Database/Doctrine/ORM/Repositories/ProjectParticipantsRepository.php
@@ -0,0 +1,38 @@
+<?php
+/* Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library se Doctrine\ORM\Tools\Setup;is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Database\Doctrine\ORM\Repositories;
+
+use OCA\CAFEVDB\Database\Doctrine\ORM\Entities;
+
+use Doctrine\ORM\Query;
+use Doctrine\ORM\EntityRepository;
+
+class ProjectParticipantsRepository extends EntityRepository
+{
+
+}
+
+// Local Variables: ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: nil ***
+// End: ***
