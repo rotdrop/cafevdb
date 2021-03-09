a88c7cc245bac8d3108a61d1feff472b84abb591
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-03-09 23:02:37 +0100

    doc

--- lib/Database/Doctrine/ORM/Entities/EmailAttachment.php
+++ lib/Database/Doctrine/ORM/Entities/EmailAttachment.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library se Doctrine\ORM\Tools\Setup;is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -75,7 +75,7 @@ class EmailAttachment implements \ArrayAccess
   /**
    * Get email-draft
    *
-   * @return int|EmailDraft
+   * @return null|EmailDraft
    */
   public function getDraft()
   {
