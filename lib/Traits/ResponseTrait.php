94c6253cf52b2ccf3728ee2587847f1c696b5261
Claus-Justus Heine <Claus-Justus.Heine@IANS.Uni-Stuttgart.DE>
2021-03-09 18:22:02 +0100

    Don't at a status-field to the data response.

--- lib/Traits/ResponseTrait.php
+++ lib/Traits/ResponseTrait.php
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -67,7 +67,6 @@ trait ResponseTrait
 
   static private function dataResponse($data, $status = Http::STATUS_OK)
   {
-    $data['status'] = $status;
     $response = new DataResponse($data, $status);
     $policy = $response->getContentSecurityPolicy($policy);
     $policy->addAllowedFrameAncestorDomain("'self'");
