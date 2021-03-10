37ac02c4ebbd52f8260d4ec6bb3a59bf4b737c91
Claus-Justus Heine <Claus-Justus.Heine@IANS.Uni-Stuttgart.DE>
2021-03-10 21:58:44 +0100

    Forgot a getter/setter for Projects entity.

--- lib/Database/Doctrine/ORM/Entities/Project.php
+++ lib/Database/Doctrine/ORM/Entities/Project.php
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library se Doctrine\ORM\Tools\Setup;is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -518,5 +518,29 @@ class Project implements \ArrayAccess
   public function getPayments():Collection
   {
     return $this->payments;
+  }
+
+  /**
+   * Set participantInstruments.
+   *
+   * @param ArrayCollection $participantInstruments
+   *
+   * @return Project
+   */
+  public function setParticipantInstruments(Collection $participantInstruments):Project
+  {
+    $this->participantInstruments = $participantInstruments;
+
+    return $this;
+  }
+
+  /**
+   * Get participantInstruments.
+   *
+   * @return ArrayCollection
+   */
+  public function getParticipantInstruments():Collection
+  {
+    return $this->participantInstruments;
   }
 }
