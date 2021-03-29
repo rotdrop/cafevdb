9af4da8fa3323b25a5be467e398f47460b747fec
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-03-30 01:49:32 +0200

    deletedAt -> deleted

--- lib/Database/Doctrine/ORM/Repositories/SepaDebitMandatesRepository.php
+++ lib/Database/Doctrine/ORM/Repositories/SepaDebitMandatesRepository.php
@@ -1,10 +1,11 @@
 <?php
-/* Orchestra member, musician and project management application.
+/**
+ * Orchestra member, musician and project management application.
  *
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library se Doctrine\ORM\Tools\Setup;is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -136,7 +137,7 @@ class SepaDebitMandatesRepository extends EntityRepository
     // @todo find about multiple incremental selects ...
     $selects = [
       'm.mandateReference',
-      'm.deletedAt',
+      'm.deleted',
       "GREATEST(COALESCE(MAX(d.dueDate), ''), COALESCE(MAX(m.lastUsedDate), '')) AS lastUsed",
       'm.mandateDate AS mandateIssued',
     ];
