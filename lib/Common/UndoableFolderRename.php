2b9b8cda8daf6970323cafff419f4bc5a03ea5ef
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-04-10 18:23:43 +0200

    Implement a generic run-queue with undo-stack.

---
+++ lib/Common/UndoableFolderRename.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Common;
+
+use OCA\CAFEVDB\Storage\UserStorage;
+
+/**
+ * Rename the given paths. The target path will be created if it does
+ * not exist. If the source path is empty the target folder will be
+ * created as empty folder.
+ */
+class UndoableFolderRename implements IUndoable
+{
+  /** @var UserStorage */
+  protected $userStorage;
+
+  /** @var string */
+  protected $oldName;
+
+  /** @var string */
+  protected $newName;
+
+  public function __construct(string $oldName, string $newName)
+  {
+    $this->oldName = self::normalizePath($oldName);
+    $this->newName = self::normalizePath($newName);
+    $this->userStorage = \OC::$server->get(UserStorage::class);
+  }
+
+  static private function normalizePath($path)
+  {
+    return rtrim(
+      preg_replace('|'.UserStorage::PATH_SEP.'+|', UserStorage::PATH_SEP, $path),
+      UserStorage::PATH_SEP);
+  }
+
+  protected function rename($from, $to)
+  {
+    $toComponents = explode(UserStorage::PATH_SEP, $to);
+    if (empty($toComponents)) {
+      throw new \Exception('Cannot rename to the root-node.');
+    }
+    $toPrefix = array_slice($toComponents, 0, count($toComponents)-1);
+    $this->userStorage->ensureFolderChain($toPrefix);
+
+    $fromDir = null;
+    if (!empty($from)) {
+      $fromDir = $this->userStorage->get($from);
+      if (empty($fromDir)) {
+        throw new \Exception('Cannot find old directory at location "'.$from.'".');
+      }
+    }
+
+    if (!empty($fromDir)) {
+      $this->userStorage->rename($from, $to);
+    } else {
+      // otherwise just create the new folder.
+      $this->userStorage->ensureFolder($to);
+    }
+  }
+
+
+  /** {@inheritdoc} */
+  public function do() {
+    $this->rename($this->oldName, $this->newName);
+  }
+
+  /** {@inheritdoc} */
+  public function undo() {
+    $this->rename($this->newName, $this->oldName);
+  }
+}
+
+// Local Variables: ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: nil ***
+// End: ***
---
+++ lib/Common/UndoableRunQueue.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Common;
+
+use OCP\ILogger;
+use OCP\IL10N;
+
+/**
+ * Implement a stack of actions with undo-actions.
+ */
+class UndoableRunQueue
+{
+  use \OCA\CAFEVDB\Traits\LoggerTrait;
+
+  /** @var array */
+  protected $actionQueue = [];
+
+  /** @var array */
+  protected $undoStack = null;
+
+  public function __construct(
+    ILogger $logger
+    , IL10N $l10n
+  ) {
+    $this->logger = $logger;
+    $this->l = $l10n;
+  }
+
+  /**
+   * Register the given action in the run-queue.
+   */
+  public function register(IUndoable $action)
+  {
+    $this->actionQueue[] = $action; // at end
+  }
+
+  /** Clears the action queue and the undo stack. */
+  public function clearActionQueue()
+  {
+    $this->actionQueue = [];
+    $this->undoStack = null;
+  }
+
+  /** Run all registered actions. */
+  public function executeActions()
+  {
+    try {
+      $this->undoStack = [];
+      while (!empty($this->actionQueue)) {
+        $action = array_shift($this->actionQueue); // from front
+        $action->do();
+        array_unshift($this->undoStack, $action); // at front
+      };
+    } catch (\Throwable $t) {
+      throw new \Exception('Exception during execution of run-queue; number of successful actions: '.count($this->undStack));
+    }
+  }
+
+  /**
+   * Run all undo-actions on the undo stack. The run-queue will
+   * continue even if exceptions occur during undo. At completion the
+   * undo-queue is empty.
+   */
+  public function executeUndo()
+  {
+    while (!empty($this->undoStack)) {
+      $action = array_shift($this->undoStack);
+      try {
+        if (is_callable([ $action, 'undo' ])) {
+          $action->undo();
+        }
+      } catch (\Throwable $t) {
+        $this->logException($t);
+      }
+    }
+  }
+}
+
+// Local Variables: ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: nil ***
