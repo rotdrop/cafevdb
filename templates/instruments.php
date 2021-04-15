8f27a713fc9409810674fcf8dd83ff9165422744
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-04-15 10:40:51 +0200

    typo

--- templates/instruments.php
+++ templates/instruments.php
@@ -1,10 +1,11 @@
 <?php
-/* Orchestra member, musician and project management application.
+/**
+ * Orchestra member, musician and project management application.
  *
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2011-2014, 2016, 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2011-2014, 2016, 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -36,7 +37,7 @@ if ($projectId >= 0) {
     $nav .= $pageNavigation->pageControlElement('project-payments', $project, $projectId);
     $nav .= $pageNavigation->pageControlElement('sepa-debit-mandates', $project, $projectId);
     $nav .= $pageNavigation->pageControlElement('sepa-debit-notes', $project, $projectId);
-    if ($projectId === $appConifg->getConfigValue('memberProjectid', false)) {
+    if ($projectId === $appConfig->getConfigValue('memberProjectid', false)) {
       $nav .= $pageNavigation->pageControlElement('insurances');
     }
   }
