e597c2b4f8d7d89abd228893850ec5b419378b6b
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-04-20 15:29:33 +0200

    Fix left-over "self" reference.

--- src/app/musicians.js
+++ src/app/musicians.js
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2011-2016, 2020 Claus-Justus Heine <himself@claus-justus-heine.de
+ * @copyright 2011-2016, 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -492,7 +492,7 @@ const ready = function(container) {
     .on('click', function(event) {
 
       const form = container.find('form.pme-form');
-      self.addMusicians(form);
+      addMusicians(form);
       return false;
     });
 };
