cf526910f1e79499435edaaf7f5442219d95e63a
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-04-30 02:13:59 +0200

    Add a simple PDF form-filler class based on php-phptk.

---
+++ lib/Documents/PDFFormFiller.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2011-2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Documents;
+
+use OCP\IL10N;
+use OCA\CAFEVDB\Service\ConfigService;
+
+use mikehaertl\pdftk\Pdf as PdfTk;
+
+class PDFFormFiller
+{
+  use \OCA\CAFEVDB\Traits\ConfigTrait;
+
+  /** @var PdfTk */
+  private $pdfTk;
+
+  public function __construct($pdfData)
+  {
+    // @todo check what $pdfData is ...
+    $this->pdfTk = new PdfTk('-');
+    $this->pdfTk
+      ->getCommand()
+      ->setStdIn($pdfData->getContent());
+  }
+
+  public function fill($fields)
+  {
+    $this->pdfTk
+      ->fillForm($fields)
+      ->needAppearances()
+      ->execute();
+  }
+
+  public function getContent()
+  {
+    return file_get_contents((string)$pdf->getTmpFile());
+  }
+}
