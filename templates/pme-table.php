189af98638fe23eb03b9e7eec2873da3a60ec0c1
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-05-10 00:00:47 +0200

    PME Table view, fix height.

--- templates/pme-table.php
+++ templates/pme-table.php
@@ -1,10 +1,11 @@
 <?php
-/* Orchestra member, musician and project management application.
+/**
+ * Orchestra member, musician and project management application.
  *
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2011-2016, 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2011-2016, 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -46,7 +47,8 @@ try {
   throw new \Exception("Renderer failed", $t->getCode(), $t);
 }
 
-echo '<div id="pme-table-container" class="pme-table-container '.$css.'" style="height:auto;">';
+//echo '<div id="pme-table-container" class="pme-table-container '.$css.'" style="height:auto;">';
+echo '<div id="pme-table-container" class="pme-table-container '.$css.'">';
 //$renderer->render();
 echo $pmeTable;
 echo '</div>';
