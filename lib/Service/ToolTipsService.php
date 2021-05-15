6bc90d7f4267dff4e399117c69840221b5f9b305
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-05-15 12:39:13 +0200

    Update translations

--- lib/Service/ToolTipsService.php
+++ lib/Service/ToolTipsService.php
@@ -969,9 +969,9 @@ tables. The actual number of rows per page can also changed later in
 the individual table views.'),
 
       'templates' => [
-        'projectDebitNoteMandateForm' => $this->l->t('A fillable PDF form for debit-mandates bound to special projects. The app is able to auto-fill form-fields with the names "projectName", "bankAccountOwner", "projectParticipant", "bankAccountIBAN", "bankAccountBIC", "bank". The fields in the PDF-form are identified by this names. Auto-filled mandates can be downloaed from the musician and project-participant views.'),
+        'projectDebitNoteMandateForm' => $this->l->t('A fillable PDF form for debit-mandates bound to special projects. The app is able to auto-fill form-fields with the names "projectName", "bankAccountOwner", "projectParticipant", "bankAccountIBAN", "bankAccountBIC", "bank". The fields in the PDF-form are identified by these names. Auto-filled mandates can be downloaed from the musician and project-participant views.'),
 
-        'generalDebitNoteMandateForm' => $this->l->t('A fillable PDF form for debit-mandates bound to special projects. The app is able to auto-fill form-fields with the names "bankAccountOwner", "projectParticipant", "bankAccountIBAN", "bankAccountBIC", "bank". The fields in the PDF-form are identified by this names. Auto-filled mandates can be downloaed from the musician and member\'s project view.'),
+        'generalDebitNoteMandateForm' => $this->l->t('A fillable PDF form for debit-mandates not bound to special projects. The app is able to auto-fill form-fields with the names "bankAccountOwner", "projectParticipant", "bankAccountIBAN", "bankAccountBIC", "bank". The fields in the PDF-form are identified by these names. Auto-filled mandates can be downloaed from the musician and member\'s project view.'),
       ],
 
       'test-cafevdb_dbpassword' => $this->l->t('Test data-base passphrase.'),
