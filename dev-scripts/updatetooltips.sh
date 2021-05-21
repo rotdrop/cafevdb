66a4a4616e74ef285fffe485540f9632b6e1e32c
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-05-21 13:08:24 +0200

    WIP work on tooltip updater.

--- dev-scripts/updatetooltips.sh
+++ dev-scripts/updatetooltips.sh
@@ -1,10 +1,25 @@
 #! /bin/bash
 DIRS="lib templates"
 FUNC=toolTipsService
-RE="s/.*"${FUNC}"\s*(['\"]\([^'\"]\\+\)['\"].*/\\1/gI"
-OUTPUT=lib/tooltips.txt
+EFUNC="(\\\$this->$FUNC|\\\$$FUNC\\s*)[([]"
+RE="s/.*${FUNC}\\s*[[]\\s*([^[]*)\\s*].*/\\1/gI"
+
+OUTPUT_TMP=$(mktemp /tmp/cafevdb-update-tooltips.XXXXXX)
+
+function cleanup() {
+    rm -f "$OUTPUT_TMP"
+}
+
+trap cleanup EXIT
+
+OUTPUT=$OUTPUT_TMP
 for dir in $DIRS; do
-    find $dir -name "*.php" -exec fgrep -ih ${FUNC} {} \;|\
-        sed -e $RE |\
+    find "$dir" -name "*.php" -exec egrep -h ${EFUNC} {} \;|\
+        sed -E -e "$RE" |\
+        fgrep -v '$' |\
         sort -u
-done > $OUTPUT
+done > "$OUTPUT"
+
+cat "$OUTPUT"
+echo "$EFUNC"
+echo "$RE"
