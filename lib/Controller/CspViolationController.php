247d9a239764de6592d32f3ce4cd55f748c157aa
Claus-Justus Heine <Claus-Justus.Heine@IANS.Uni-Stuttgart.DE>
2021-05-25 16:54:54 +0200

    cleanup

--- lib/Controller/CspViolationController.php
+++ lib/Controller/CspViolationController.php
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -28,9 +28,6 @@ use OCP\IRequest;
 use OCP\ILogger;
 use OCP\IL10N;
 use OCP\IConfig;
-
-use OCA\CAFEVDB\Service\ProgressStatusService;
-use OCP\IDBConnection;
 
 class CspViolationController extends Controller {
   use \OCA\CAFEVDB\Traits\ResponseTrait;
