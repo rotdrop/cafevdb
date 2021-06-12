5c43ef9dda7da95fe02860a2ad0a6e3426118ebe
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-06-12 14:57:51 +0200

    Include auto-loader for wrapped namespaces

--- config/cli-config.php
+++ config/cli-config.php
@@ -214,6 +214,7 @@ $encryptionService->initAppEncryptionKey();
  *****************************************************************************/
 
 require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/../vendor-wrapped/autoload.php";
 
 // otherwise Redaxo4Embedded's InstalledVersions is pulled in by autoload
 $installedVersions = __DIR__ . "/../vendor/composer/InstalledVersions.php";
