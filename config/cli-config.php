21d378d8dd10929aa624a8eb76a2374181634c8b
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-07-08 14:16:37 +0200

    Untabify and update logger for NC 22

--- config/cli-config.php
+++ config/cli-config.php
@@ -37,69 +37,69 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 define('OC_CONSOLE', 1);
 
 function exceptionHandler($exception) {
-	echo "An unhandled exception has been thrown:" . PHP_EOL;
-	echo $exception;
-	exit(1);
+    echo "An unhandled exception has been thrown:" . PHP_EOL;
+    echo $exception;
+    exit(1);
 }
 try {
-	require_once __DIR__ . $corePrefix . '/lib/base.php';
+    require_once __DIR__ . $corePrefix . '/lib/base.php';
 
-	// set to run indefinitely if needed
-	if (strpos(@ini_get('disable_functions'), 'set_time_limit') === false) {
-		@set_time_limit(0);
-	}
+    // set to run indefinitely if needed
+    if (strpos(@ini_get('disable_functions'), 'set_time_limit') === false) {
+        @set_time_limit(0);
+    }
 
-	if (!OC::$CLI) {
-		echo "This script can be run from the command line only" . PHP_EOL;
-		exit(1);
-	}
+    if (!OC::$CLI) {
+        echo "This script can be run from the command line only" . PHP_EOL;
+        exit(1);
+    }
 
-	set_exception_handler('exceptionHandler');
+    set_exception_handler('exceptionHandler');
 
-	if (!function_exists('posix_getuid')) {
-		echo "The posix extensions are required - see http://php.net/manual/en/book.posix.php" . PHP_EOL;
-		exit(1);
-	}
-	$user = posix_getpwuid(posix_getuid());
-	$configUser = posix_getpwuid(fileowner(OC::$configDir . 'config.php'));
-	if ($user['name'] !== $configUser['name']) {
-		echo "Console has to be executed with the user that owns the file config/config.php" . PHP_EOL;
-		echo "Current user: " . $user['name'] . PHP_EOL;
-		echo "Owner of config.php: " . $configUser['name'] . PHP_EOL;
-		echo "Try adding 'sudo -u " . $configUser['name'] . " ' to the beginning of the command (without the single quotes)" . PHP_EOL;
-		echo "If running with 'docker exec' try adding the option '-u " . $configUser['name'] . "' to the docker command (without the single quotes)" . PHP_EOL;
-		exit(1);
-	}
+    if (!function_exists('posix_getuid')) {
+        echo "The posix extensions are required - see http://php.net/manual/en/book.posix.php" . PHP_EOL;
+        exit(1);
+    }
+    $user = posix_getpwuid(posix_getuid());
+    $configUser = posix_getpwuid(fileowner(OC::$configDir . 'config.php'));
+    if ($user['name'] !== $configUser['name']) {
+        echo "Console has to be executed with the user that owns the file config/config.php" . PHP_EOL;
+        echo "Current user: " . $user['name'] . PHP_EOL;
+        echo "Owner of config.php: " . $configUser['name'] . PHP_EOL;
+        echo "Try adding 'sudo -u " . $configUser['name'] . " ' to the beginning of the command (without the single quotes)" . PHP_EOL;
+        echo "If running with 'docker exec' try adding the option '-u " . $configUser['name'] . "' to the docker command (without the single quotes)" . PHP_EOL;
+        exit(1);
+    }
 
-	$oldWorkingDir = getcwd();
-	if ($oldWorkingDir === false) {
-		echo "This script can be run from the CAFeV-DB root directory only." . PHP_EOL;
-		echo "Can't determine current working dir - the script will continue to work but be aware of the above fact." . PHP_EOL;
-	} elseif ($oldWorkingDir !== __DIR__ && !chdir(__DIR__)) {
-		echo "This script can be run from the CAFeV-DB root directory only." . PHP_EOL;
-		echo "Can't change to Nextcloud root directory." . PHP_EOL;
-		exit(1);
-	}
+    $oldWorkingDir = getcwd();
+    if ($oldWorkingDir === false) {
+        echo "This script can be run from the CAFeV-DB root directory only." . PHP_EOL;
+        echo "Can't determine current working dir - the script will continue to work but be aware of the above fact." . PHP_EOL;
+    } elseif ($oldWorkingDir !== __DIR__ && !chdir(__DIR__)) {
+        echo "This script can be run from the CAFeV-DB root directory only." . PHP_EOL;
+        echo "Can't change to Nextcloud root directory." . PHP_EOL;
+        exit(1);
+    }
 
-	if (!function_exists('pcntl_signal') && !in_array('--no-warnings', $argv)) {
-		echo "The process control (PCNTL) extensions are required in case you want to interrupt long running commands - see http://php.net/manual/en/book.pcntl.php" . PHP_EOL;
-	}
+    if (!function_exists('pcntl_signal') && !in_array('--no-warnings', $argv)) {
+        echo "The process control (PCNTL) extensions are required in case you want to interrupt long running commands - see http://php.net/manual/en/book.pcntl.php" . PHP_EOL;
+    }
 
-	$application = new Application(
-		\OC::$server->getConfig(),
-		\OC::$server->getEventDispatcher(),
-		\OC::$server->getRequest(),
-		\OC::$server->getLogger(),
-		\OC::$server->query(\OC\MemoryInfo::class)
-	);
+    $application = new Application(
+        \OC::$server->getConfig(),
+        \OC::$server->getEventDispatcher(),
+        \OC::$server->getRequest(),
+        \OC::$server->get(\Psr\Log\LoggerInterface::class),
+        \OC::$server->query(\OC\MemoryInfo::class)
+    );
     // $application->loadCommands(new ArgvInput(), new ConsoleOutput());
-	// $application->run();
+    // $application->run();
 } catch (Exception $ex) {
-	exceptionHandler($ex);
+    exceptionHandler($ex);
 } catch (Error $ex) {
-	exceptionHandler($ex);
+    exceptionHandler($ex);
 } catch (Throwable $ex) {
-	exceptionHandler($ex);
+    exceptionHandler($ex);
 }
 
 /**
