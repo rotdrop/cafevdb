1040ab1c7e73dc33225f29b5b6615f0f076d7127
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-07-22 23:35:05 +0200

    cosmetics

--- lib/Service/HistoryService.php
+++ lib/Service/HistoryService.php
@@ -1,12 +1,24 @@
 <?php
 /**
- * Nextcloud - cafevdb
+ * Orchestra member, musician and project management application.
  *
- * This file is licensed under the Affero General Public License version 3 or
- * later. See the COPYING file.
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
- * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright Claus-Justus Heine 2014-2021
+ * @author Claus-Justus Heine
+ * @copyright 2014-2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or1
+ * modify it under th52 terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace OCA\CAFEVDB\Service;
