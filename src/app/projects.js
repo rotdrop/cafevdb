1fabb8aed8270d3a7ec905d5bf40796609b48ce4
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-12-03 15:33:01 +0100

    Fun: trigger license extract with web-pack.

--- src/app/projects.js
+++ src/app/projects.js
@@ -5,6 +5,7 @@
  *
  * @author Claus-Justus Heine
  * @copyright 2011-2016, 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @license GNU AGPL version 3 or any later version
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -16,7 +17,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
+ * You should have received a copy of the GNU Affero General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
