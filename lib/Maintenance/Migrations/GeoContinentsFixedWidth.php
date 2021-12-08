7e3b745008a18e0f56181152c5b256aec2f3c2ef
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-12-09 00:56:08 +0100

    Adjust the data-type of the GeoContinents table.

---
+++ lib/Maintenance/Migrations/GeoContinentsFixedWidth.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2011-2016, 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Maintenance\Migrations;
+
+use OCP\ILogger;
+use OCP\IL10N;
+
+use OCA\CAFEVDB\Maintenance\IMigration;
+use OCA\CAFEVDB\Database\EntityManager;
+use OCA\CAFEVDB\Database\Doctrine\ORM\Entities;
+use OCA\CAFEVDB\Exceptions;
+
+/**
+ * Generate some needed procedures and functions. MySQL specific.
+ */
+class GeoContinentsFixedWidth implements IMigration
+{
+  use \OCA\CAFEVDB\Traits\LoggerTrait;
+  use \OCA\CAFEVDB\Traits\EntityManagerTrait;
+
+  public function description():string
+  {
+    return $this->l->t('Pin the data-type of the GeoContinents table to 2-char-fixed-width.');
+  }
+
+  private const SQL = [
+    'ALTER TABLE GeoContinents CHANGE code code CHAR(2) NOT NULL COLLATE `ascii_general_ci`, CHANGE target target CHAR(2) NOT NULL COLLATE `ascii_general_ci`;',
+  ];
+
+  public function __construct(
+    ILogger $logger
+    , IL10N $l10n
+    , EntityManager $entityManager
+  ) {
+    $this->logger = $logger;
+    $this->l = $l10n;
+    $this->entityManager = $entityManager;
+  }
+
+  public function execute():bool
+  {
+    $connection = $this->entityManager->getConnection();
+
+    // $connection->beginTransaction();
+    try {
+      foreach (self::SQL as $sql) {
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+      }
+      // if ($connection->getTransactionNestingLevel() > 0) {
+      //   $connection->commit();
+      // }
+    } catch (\Throwable $t) {
+      // if ($connection->getTransactionNestingLevel() > 0) {
+      //   try {
+      //     $connection->rollBack();
+      //   } catch (\Throwable $t2) {
+      //     $t = new Exceptions\DatabaseMigrationException($this->l->t('Rollback of Migration "%s" failed.', $this->description()), $t->getCode(), $t);
+      //   }
+      // }
+      throw new Exceptions\DatabaseMigrationException($this->l->t('Migration "%s" failed.', $this->description()), $t->getCode(), $t);
+    }
+    return true;
+  }
+};
+
+// Local Variables: ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: nil ***
+// End: ***
