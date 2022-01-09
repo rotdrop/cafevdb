3a99851e1366561456e947cd3b26972fe6783e79
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-09 13:01:24 +0100

    JS/HTML: emulate read-only for radio, selects and check-buttons.
    It is at least hard to understand that these input elements do not
    support readonly. They have state which is altered by the user.
    Work-around by setting to "disabled" and adding additional
    form-attributes which provide the form data on submit (disabled elements
    are not submitted).

---
+++ src/app/jquery-readonly.js
@@ -0,0 +1,105 @@
+/**
+ * Orchestra member, musicion and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2011-2016, 2020, 2021, 2022, 2022 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { $ } from './globals.js';
+import { refreshWidget as refreshSelectWidget } from './select-utils.js';
+
+$.fn.readonly = function(state) {
+  state = status || false;
+  this.each(function() {
+    const $this = $(this);
+    $this.prop('readonly', state);
+    if ($this.is('select')) {
+      // single-select can be handled like radio buttons
+      if (!$this.prop('multiple')) {
+        $this.find('option').each(function() {
+          const $option = $(this);
+          if (!state) {
+            const restoreDisabled = $option.data('readonlyRestoreDisabled');
+            if (restoreDisabled !== undefined) {
+              $option.prop('disabled', restoreDisabled);
+            }
+          } else {
+            $option.data('readonlyRestoreDisabled', $option.prop('disabled'));
+            $option.prop('disabled', !$option.prop('selected'));
+          }
+        });
+      } else {
+        const name = $this.attr('name') + '[]';
+        $this.find('option').each(function() {
+          const $option = $(this);
+          let placeholder = $option.data('readonlyPlaceholder');
+          if (!placeholder) {
+            placeholder = $('<input type="hidden" name="' + name + '"/>');
+            $this.after(placeholder);
+            $option.data('readonlyPlaceholder', placeholder);
+          }
+          placeholder.attr('value', $option.attr('value') || $option.text());
+          placeholder.prop('disabled', !state || !$option.prop('selected'));
+          if (!state) {
+            const restoreDisabled = $option.data('readonlyRestoreDisabled');
+            if (restoreDisabled !== undefined) {
+              $option.prop('disabled', restoreDisabled);
+            }
+          } else {
+            $option.data('readonlyRestoreDisabled', $option.prop('disabled'));
+            $option.prop('disabled', true);
+          }
+        });
+      }
+      refreshSelectWidget($this);
+    } else if ($this.is('radio')) {
+      let $container = $this.closest('fieldset');
+      if (!$container) {
+        $container = $this.closest('form');
+      }
+      if (!$container) {
+        $container = $('body');
+      }
+      const $radioGroup = $container.find('input:radio[name="' + $this.attr('name') + '"]');
+      $radioGroup.prop('readonly', state);
+      $radioGroup.each(function() {
+        const $radio = $(this);
+        if (!state) {
+          const restoreDisabled = $radio.data('readonlyRestoreDisabled');
+          if (restoreDisabled !== undefined) {
+            $radio.prop('disabled', restoreDisabled);
+          }
+        } else {
+          $radio.data('readonlyRestoreDisabled', $radio.prop('disabled'));
+          $radio.prop('disabled', !$radio.prop('checked'));
+        }
+      });
+    } else if ($this.is('checkbox')) {
+      let placeholder = $this.data('readonlyPlaceholder');
+      if (!placeholder) {
+        placeholder = $('<input type="hidden" name="' + $this.attr('name') + '"/>');
+        $this.after(placeholder);
+        $this.data('readonlyPlaceholder', placeholder);
+      }
+      placeholder.attr('value', $this.attr('value') || 'on');
+      placeholder.prop('disabled', !state || !$this.prop('checked'));
+      $this.prop('disabled', state);
+    }
+  });
+  return this;
+};
