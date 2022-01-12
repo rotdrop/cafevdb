582fe3fed9ae83ba13d719b563d6ac203c976e62
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-12 16:44:34 +0100

    Prefilled forms: strip intermediate office extension when creating the final filename.

--- lib/Service/Finance/FinanceService.php
+++ lib/Service/Finance/FinanceService.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2011-2016, 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2011-2016, 2020, 2021, 2022 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -268,7 +268,7 @@ class FinanceService
 
     $fileParts = [
       $this->timeStamp('Ymd'),
-      basename($formFile->getName()),
+      pathinfo($formFile->getName(), PATHINFO_FILENAME),
       str_replace('.', '-', $musician->getUserIdSlug()),
     ];
     $fileName = implode('-', $fileParts) . '.pdf';
