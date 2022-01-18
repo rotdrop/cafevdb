b7fff00459323bcb5bde5d09d78fbce4d2792e6a
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-18 12:23:40 +0100

    Fix row -> $row typo.

--- lib/PageRenderer/InstrumentInsurances.php
+++ lib/PageRenderer/InstrumentInsurances.php
@@ -279,7 +279,7 @@ class InstrumentInsurances extends PMETableViewBase
         //'filters' => parent::musicianInProjectSql($this->projectId),
       ],
       'display' => [
-        'prefix' => function($op, $pos, $k, row, $pme) {
+        'prefix' => function($op, $pos, $k, $row, $pme) {
           $instrumentHolder = $row[$this->queryIndexField('instrument_holder_id', $pme->fdd)];
           $billToParty = $row[$this->queryIndexField('bill_to_party_id', $pme->fdd)];
           $isClubMember = $row[$this->joinQueryField(self::MEMBERSHIP_TABLE, 'project_id', $pme->fdd)];
