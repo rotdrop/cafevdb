53eef29c43dac4a42e2cebbef4402c2f90d2e4ce
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-25 18:33:09 +0100

    ORM cli: force include of vendor-wrapped/composer/InstalledVersions.php

--- config/cli-config.php
+++ config/cli-config.php
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2020, 2021, 2022 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -221,9 +221,14 @@ require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../vendor-wrapped/autoload.php";
 
 // otherwise Redaxo4Embedded's InstalledVersions is pulled in by autoload
-$installedVersions = __DIR__ . "/../vendor/composer/InstalledVersions.php";
-if (file_exists($installedVersions)) {
-  require_once $installedVersions;
+$installedVersions = [
+  __DIR__ . "/../vendor-wrapped/composer/InstalledVersions.php",
+  __DIR__ . "/../vendor/composer/InstalledVersions.php",
+];
+foreach ($installedVersions as $file) {
+  if (file_exists($file)) {
+    require_once $file;
+  }
 }
 
 use OCA\CAFEVDB\Wrapped\Doctrine\ORM\Tools\Console\ConsoleRunner;
