c1e322913d259f83a57ea00050c9cc9450926e55
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-25 08:07:47 +0100

    ORM/Gedmo CreatedAt trait, cleanup PHP use.

--- lib/Database/Doctrine/ORM/Traits/CreatedAt.php
+++ lib/Database/Doctrine/ORM/Traits/CreatedAt.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2020, 2021, 2022 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library se Doctrine\ORM\Tools\Setup;is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -20,18 +20,8 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
-/**
- * Based on, with "At" removed from the names, i.e. updatedAt replaced
- * by updated etc.
- *
- * Timestampable Trait, usable with PHP >= 5.4
- *
- * @author Gediminas Morkevicius <gediminas.morkevicius@gmail.com>
- * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
 
 namespace OCA\CAFEVDB\Database\Doctrine\ORM\Traits;
-use OCA\CAFEVDB\Wrapped\Gedmo\Mapping\Annotation as Gedmo;
 
 trait CreatedAt
 {
