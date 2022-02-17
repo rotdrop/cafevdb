e3661936d20e9919a73e91acd5cb765f85240c8b
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-02-17 11:10:08 +0100

    Add initial-state to admin-settings in order to shut-off JS warnings.

--- lib/Settings/PersonalForm.php
+++ lib/Settings/PersonalForm.php
@@ -81,6 +81,9 @@ class PersonalForm {
   /** @var CloudUserConnectorService */
   private $cloudUserService;
 
+  /** @var IInitialStateService */
+  private $initialStateService;
+
   public function __construct(
     ConfigService $configService
     , AssetService $assetService
