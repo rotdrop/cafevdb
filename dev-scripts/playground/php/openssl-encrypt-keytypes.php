6c429586c0a20d8e18e2b44b456646337407ea99
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-02-24 09:17:05 +0100

    Playground: verify basic openssl encryption and signing.

--- dev-scripts/playground/php/openssl-encrypt-keytypes.php
+++ dev-scripts/playground/php/openssl-encrypt-keytypes.php
@@ -1,9 +1,11 @@
 <?php
 
 $config = [
-  'private_key_type' => OPENSSL_KEYTYPE_EC,
-  'priqvate_key_bits' => 512,
-  'curve_name' => 'prime256v1',
+  'private_key_type' => OPENSSL_KEYTYPE_RSA,
+  'private_key_bits' => 4096,
+  // 'private_key_type' => OPENSSL_KEYTYPE_EC,
+  // 'priqvate_key_bits' => 512,
+  // 'curve_name' => 'prime256v1',
 ];
 
 // Create the private and public key
@@ -17,6 +19,13 @@ $pubKey = openssl_pkey_get_details($res);
 $pubKey = $pubKey["key"];
 
 $data = 'plaintext data goes here';
+
+$signingAlgo = OPENSSL_ALGO_SHA512;
+openssl_sign($data, $signature, $privKey, $signingAlgo);
+
+echo 'SIG: ' . strlen($signature) . PHP_EOL;
+
+openssl_verify($data, $signature, $pubKey, $signingAlgo);
 
 // Encrypt the data to $encrypted using the public key
 openssl_public_encrypt($data, $encrypted, $pubKey);
