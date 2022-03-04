3aedf5ccdac6bb9b494347ef3c7c38d13c9e1089
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-03-04 22:10:39 +0100

    Fix AbstractMigration handler.

--- lib/Maintenance/Migrations/AbstractMigration.php
+++ lib/Maintenance/Migrations/AbstractMigration.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2011-2016, 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2011-2016, 2020, 2021, 2022 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -98,7 +98,6 @@ abstract class AbstractMigration implements IMigration
           if (is_callable($sql['bind']??null)) {
             call_user_func($sql['bind'], $statement);
           }
-          $statement = $connection->prepare($sql);
           $statement->execute();
         }
         if ($connection->getTransactionNestingLevel() > 0) {
