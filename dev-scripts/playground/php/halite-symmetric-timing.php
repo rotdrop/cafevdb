154b6c84c55e8b7dca948cd792c026ff842dea5d
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-03-18 14:21:51 +0100

    Test code for timing of crypto stuff.

---
+++ dev-scripts/playground/php/halite-symmetric-timing.php
@@ -0,0 +1,48 @@
+<?php
+
+include_once __DIR__ . '/console-setup.php';
+
+include_once __DIR__ . '/../../../vendor/autoload.php';
+
+// use OCP\Security\ICrypto;
+// $crypto = \OC::$server->query(ICrypto::class);
+
+use ParagonIE\Halite;
+use ParagonIE\HiddenString\HiddenString;
+
+$password = 'foo!bar';
+$salt = random_bytes(SODIUM_CRYPTO_PWHASH_SALTBYTES);
+$encryptionKey = Halite\KeyFactory::deriveEncryptionKey(new HiddenString($password), $salt);
+
+$data = file_get_contents("php://stdin");
+
+$inputStream = fopen('php://memory', 'w+');
+fwrite($inputStream, $data, strlen($data));
+rewind($inputStream);
+$haliteInput = new Halite\Stream\WeakReadOnlyFile($inputStream);
+
+$outputStream = fopen('php://memory', 'w+');
+$haliteOutput = new Halite\Stream\MutableFile($outputStream);
+
+echo 'DATA OF SIZE ' . strlen($data) . PHP_EOL;
+$timeStart = microtime(true);
+//$encrypted = Halite\Symmetric\Crypto::encrypt(new HiddenString($data), $encryptionKey);
+
+Halite\File::encrypt($haliteInput, $haliteOutput, $encryptionKey);
+
+rewind($outputStream);
+$haliteInput = new Halite\Stream\WeakReadOnlyFile($outputStream);
+ftruncate($inputStream, 0);
+$haliteOutput = new Halite\Stream\MutableFile($inputStream);
+
+$timeEnd = microtime(true);
+echo 'ENCRYPTION SECONDS ' . ($timeEnd - $timeStart) . PHP_EOL;
+
+Halite\File::decrypt($haliteInput, $haliteOutput, $encryptionKey);
+
+//$decrypted = Halite\Symmetric\Crypto::decrypt($encrypted, $encryptionKey);
+echo 'DECRYPTION SECONDS ' . (microtime(true) - $timeEnd) . PHP_EOL;
+
+rewind($inputStream);
+$decrypted = fread($inputStream, strlen($data));
+echo "CORRECT " . (int)($decrypted == $data) . PHP_EOL;
