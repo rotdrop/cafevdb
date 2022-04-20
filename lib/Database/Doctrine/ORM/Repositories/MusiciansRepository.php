9441b5a6e5eb9588d7990c5784f966e09eac9e6f
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-04-20 16:19:49 +0200

    MusiciansRepository::fetchIds() -- allow empty criteria.

--- lib/Database/Doctrine/ORM/Repositories/MusiciansRepository.php
+++ lib/Database/Doctrine/ORM/Repositories/MusiciansRepository.php
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2020, 2021, 2022 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
  * This library se Doctrine\ORM\Tools\Setup;is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
@@ -45,7 +45,7 @@ class MusiciansRepository extends EntityRepository
     );
   }
 
-  public function fetchIds(array $criteria)
+  public function fetchIds(array $criteria = [])
   {
     $queryParts = $this->prepareFindBy($criteria, [
       'id' => 'ASC',
