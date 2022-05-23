282db93b03e7bc1e2692df8b4743379d6f9a3ec6
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-05-24 00:30:26 +0200

    Fix bogus discriminator column in FileData table.

---
+++ lib/Maintenance/Migrations/FixFixFileDataLazyLoading.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2022 Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Maintenance\Migrations;
+
+class FixFixFileDataLazyLoading extends AbstractMigration
+{
+  protected static $sql = [
+    self::STRUCTURAL => [
+      "ALTER TABLE FileData ADD COLUMN IF NOT EXISTS type enum('generic','image','encrypted') NOT NULL COMMENT 'enum(generic,image,encrypted)(DC2Type:EnumFileType)'",
+      "ALTER TABLE FileData DROP transformation",
+    ],
+    self::TRANSACTIONAL => [
+      "UPDATE FileData fd INNER JOIN Files f ON f.id = fd.file_id
+SET fd.type = f.type",
+    ],
+  ];
+
+  public function description():string
+  {
+    return $this->l->t('Fix the fix for file-data lazy loading.');
+  }
+};
+
+// Local Variables: ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: nil ***
+// End: ***
