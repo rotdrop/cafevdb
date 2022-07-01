65dcbb2f7dc77ab7f89b6d0d6927ef6e3e32fa62
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-07-01 18:37:31 +0200

    AuthorizationService::isAdmin() -- fix after using it

--- lib/Service/AuthorizationService.php
+++ lib/Service/AuthorizationService.php
@@ -5,26 +5,28 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2020, 2021, 2022 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @license AGPL-3.0-or-later
  *
- * This library is free software; you can redistribute it and/or1
- * modify it under th52 terms of the GNU GENERAL PUBLIC LICENSE
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace OCA\CAFEVDB\Service;
 
 use OCP\IUserManager;
 use OCP\IGroupManager;
+use OCP\Group\ISubAdmin as IGroupSubAdminManager;
 use OCP\IConfig;
 use OCP\ILogger;
 use OCP\IL10N;
@@ -47,6 +49,9 @@ class AuthorizationService
   /** @var \OCP\IGroupManager */
   private $groupManager;
 
+  /** @var IGroupSubAdminManager */
+  private $groupSubAdminManager;
+
   /** @var \OCP\IConfig */
   private $config;
 
@@ -58,6 +63,7 @@ class AuthorizationService
     , IConfig $config
     , IUserManager $userManager
     , IGroupManager $groupManager
+    , IGroupSubAdminManager $groupSubAdminManager
     , ILogger $logger
     , IL10N $l10n
   ) {
@@ -65,6 +71,7 @@ class AuthorizationService
     $this->config = $config;
     $this->userManager = $userManager;
     $this->groupManager = $groupManager;
+    $this->groupSubAdminManager = $groupSubAdminManager;
     $this->logger = $logger;
     $this->l = $l10n;
     $this->userGroup = $this->config->getAppValue($this->appName, 'usergroup');
@@ -102,7 +109,7 @@ class AuthorizationService
     if (empty($user) || empty($group)) {
       return false;
     }
-    return $this->groupSubAdmin->isSubAdminofGroup($user, $group);
+    return $this->groupSubAdminManager->isSubAdminofGroup($user, $group);
   }
 
 }
