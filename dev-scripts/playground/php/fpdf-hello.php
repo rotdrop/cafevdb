307b839644b23080d20315666385726f44290fe9
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-07-08 11:04:55 +0200

    Play with PDF generation.

--- dev-scripts/playground/php/fpdf-hello.php
+++ dev-scripts/playground/php/fpdf-hello.php
@@ -1,11 +1,13 @@
 <?php
 
-include_once __DIR__ . '/console-setup.php';
-
 include_once __DIR__ . '/../../../vendor/autoload.php';
 
-$pdf=new FPDF('L', 'mm', [150, 20]);
-// $pdf->AddPage();
-// $pdf->SetFont('Arial','B',16);
-$pdf->Cell(0,20,'Hello World!');
-$pdf->Output();
+$string = 'Hello yy gg World!';
+$fontSize = 32;
+
+$pdf=new FPDF('L', 'pt', [$fontSize, 500]);
+//$pdf = new FPDF('P', 'pt', 'a4');
+$pdf->AddPage();
+$pdf->SetFont('Arial', 'B', $fontSize);
+$pdf->Text(0, $fontSize-8, $string);
+$pdf->Output('F', 'blah.pdf');
--- dev-scripts/playground/php/tcpdf-hello.php
+++ dev-scripts/playground/php/tcpdf-hello.php
@@ -1,11 +1,24 @@
 <?php
 
-include_once __DIR__ . '/console-setup.php';
-
 include_once __DIR__ . '/../../../vendor/autoload.php';
 
-$pdf = new TCPDF();                 // create TCPDF object with default constructor args
-$pdf->AddPage();                    // pretty self-explanatory
-$pdf->Write(1, 'Hello world');      // 1 is line height
+$string = 'eHello gg yy World';
+$fontSize = 32;
 
-$pdf->Output('hello_world.pdf');    // send the file inline to the browser (default).
+$pdf = new TCPDF('L', 'pt', [1*$fontSize, 500]);
+
+$pdf->setFont('dejavusans');
+$pdf->setFontSize($fontSize);
+$pdf->setMargins(0, 0);
+$pdf->setCellPaddings(0, 0);
+$pdf->setAutoPageBreak(false);
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+$pdf->SetAlpha(0);
+$stringWidth = $pdf->getStringWidth($string);
+$pdf->startPage('L', [1*$fontSize, $stringWidth]);
+$pdf->Text(0, $fontSize, $string, calign: 'D', valign: 'T', align: 'L');
+// $pdf->Text(0, $fontSize+8, $string);
+$output = $pdf->Output('blah.pdf', 'S');
+
+file_put_contents('blah.pdf', $output);
