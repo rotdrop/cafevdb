2de18ecbcfaca694a085bc7ec5c9fdbd410fdde2
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-07-08 17:20:51 +0200

    tcpdf, play with alpha and colors.

--- dev-scripts/playground/php/tcpdf-hello.php
+++ dev-scripts/playground/php/tcpdf-hello.php
@@ -2,23 +2,51 @@
 
 include_once __DIR__ . '/../../../vendor/autoload.php';
 
-$string = 'eHello gg yy World';
-$fontSize = 32;
+$text = 'eHello gg yy World';
+$fontSize = 16;
 
-$pdf = new TCPDF('L', 'pt', [1*$fontSize, 500]);
+$pageWidth = 595.3;
+$pageHeight = 841.9;
+$orientation = $pageHeight > $pageWidth ? 'P' : 'L';
 
-$pdf->setFont('dejavusans');
-$pdf->setFontSize($fontSize);
-$pdf->setMargins(0, 0);
-$pdf->setCellPaddings(0, 0);
+$pdf = new TCPDF();
+
+$pdf->setPageUnit('pt');
+$pdf->setFont('dejavusansmono');
+$margin = 0; // self::OVERLAY_FONTSIZE;
+$pdf->setMargins($margin, $margin, $margin, $margin);
 $pdf->setAutoPageBreak(false);
 $pdf->setPrintHeader(false);
 $pdf->setPrintFooter(false);
-$pdf->SetAlpha(0);
-$stringWidth = $pdf->getStringWidth($string);
-$pdf->startPage('L', [1*$fontSize, $stringWidth]);
-$pdf->Text(0, $fontSize, $string, calign: 'D', valign: 'T', align: 'L');
-// $pdf->Text(0, $fontSize+8, $string);
+
+$pdf->setFontSize($fontSize);
+$stringWidth = $pdf->GetStringWidth($text);
+$fontSize = 0.4 * $pageWidth / $stringWidth * $fontSize;
+$pdf->setFontSize($fontSize);
+$padding = 0.25 * $fontSize;
+$pdf->setCellPaddings($padding, $padding, $padding, $padding);
+
+$pdf->startPage($orientation, [ $pageWidth, $pageHeight ]);
+
+
+$cellWidth = 0.4 * $pageWidth + 2*$padding;
+
+
+$pdf->setXY(0.5 * $pageWidth, .75 * $fontSize);
+$pdf->Cell(0.5 * $pageWidth, 1.5*$fontSize, $text, calign: 'A', valign: 'T', align: 'L', fill: false);
+
+
+
+$pdf->setColor('fill', 200);
+$pdf->setColor('text', 255, 0, 0);
+
+$pdf->SetAlpha(1, 'Normal', 0.2);
+$pdf->Rect($pageWidth - $cellWidth, 0, $cellWidth, 1.5 * $fontSize, style: 'F', fill_color: [ 200 ]);
+$pdf->SetAlpha(1, 'Normal', 1);
+$pdf->setXY($pageWidth - $cellWidth, 0.25 * $fontSize);
+$pdf->Cell($cellWidth, 1.5*$fontSize, $text, calign: 'A', valign: 'T', align: 'R', fill: false);
+$pdf->endPage();
+
 $output = $pdf->Output('blah.pdf', 'S');
 
 file_put_contents('blah.pdf', $output);
