9fb8b7b7aec9488b36736847c7a9b3b558f74368
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-07-13 08:00:26 +0200

    New "Undoable" action in order to generate cloud-fs folder for participant fields.

---
+++ lib/Common/UndoableFolderCreate.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine
+ * @copyright 2022 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Common;
+
+use OCP\Files\Node as FileSystemNode;
+use OCP\Files\FileInfo;
+
+use OCA\CAFEVDB\Storage\UserStorage;
+
+/**
+ * Create the given path as folder, remove the directory when undo is requested.
+ */
+class UndoableFolderCreate implements IUndoable
+{
+  /** @var UserStorage */
+  protected $userStorage;
+
+  /** @var callable */
+  protected $name;
+
+  /** @var string */
+  protected $renamedName;
+
+  /** @var bool */
+  protected $gracefully;
+
+  /**
+   * Undoable folder rename, optionally ignoring non-existing source folder.
+   *
+   * @param string|Callable $folderName
+   *
+   * @param bool $gracefully Do not throw if the source-folder given
+   * as $oldName does not exist.
+   */
+  public function __construct($name, bool $gracefully = false)
+  {
+    $this->name = $name;
+    $this->gracefully = $gracefully;
+    $this->userStorage = \OC::$server->get(UserStorage::class);
+  }
+
+  static private function normalizePath($path)
+  {
+    return rtrim(
+      preg_replace('|'.UserStorage::PATH_SEP.'+|', UserStorage::PATH_SEP, $path),
+      UserStorage::PATH_SEP);
+  }
+
+  static private function renamedName($path, $time)
+  {
+    $pathInfo = pathinfo($path);
+    $renamed = $pathInfo['dirname']
+      . UserStorage::PATH_SEP
+      . $pathInfo['filename'] . '-renamed-' . $time . '.' . $pathInfo['extension'];
+    return $renamed;
+  }
+
+  /** {@inheritdoc} */
+  public function do() {
+    if (is_callable($this->name)) {
+      $this->name = call_user_func($this->name);
+    }
+    $this->name = self::normalizePath($this->name);
+
+    $this->renamedName = null;
+    // - $folderName does not exist
+    //   -> just create the new folder, delete on undo
+    // - $folderName exists and is a folder
+    //   -> just keep, do not delete on undo
+    // - $folderName exists and is a file
+    //   -> delete, undelete on undo, or just rename
+    $components = explode(UserStorage::PATH_SEP, $this->name);
+    if (empty($components)) {
+      throw new \Exception('Cannot create the root-node.');
+    }
+    $prefix = array_slice($components, 0, count($components)-1);
+    $this->userStorage->ensureFolderChain($prefix);
+
+    /** @var FileSystemNode $existing */
+    $existing = $this->userStorage->get($this->name);
+    if (!empty($existing) && $this->gracefully) {
+      // keep the folder or rename any existing file
+      if ($existing->getType() != FileInfo::TYPE_FOLDER) {
+        $this->renamedName = self::renamedName($this->name);
+        $this->userStorage->rename($this->name, $this->renamedName);
+        $existing = null;
+      }
+    }
+
+    if (empty($existing)) {
+      // just create the folder
+      $this->userStorage->ensureFolder($this->name);
+    }
+  }
+
+  /** {@inheritdoc} */
+  public function undo() {
+    $this->userStorage->delete($this->name);
+    if (!empty($this->renamedName)) {
+      $this->userStorage->rename($this->renamedName, $this->name);
+    }
+  }
+
+
+  /** {@inheritdoc} */
+  public function reset()
+  {
+    $this->renamedName = null;
+  }
+}
+
+// Local Variables: ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: nil ***
+// End: ***
