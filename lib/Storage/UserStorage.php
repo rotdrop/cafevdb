ad0c4db4dabc4506715464af7e653968770cc55e
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-09-20 18:19:47 +0200

    CS Storage/UserStorage.php

--- lib/Storage/UserStorage.php
+++ lib/Storage/UserStorage.php
@@ -4,25 +4,29 @@
  *
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
- * @author Claus-Justus Heine
+ * @author Claus-Justus Heine <himself@claus-justus-heine.de>
  * @copyright 2011-2014, 2016, 2020, 2021, 2022, Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @license AGPL-3.0-or-later
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace OCA\CAFEVDB\Storage;
 
+use \Throwable;
+use \RuntimeException;
+use \InvalidArgumentException;
 use ZipStream\ZipStream;
 use ZipStream\Option\Archive as ArchiveOptions;
 
@@ -38,9 +42,11 @@ use OCP\Files\Folder;
 use OCP\Files\FileInfo;
 use OCA\Files_Trashbin\Trash\ITrashManager;
 use OCA\Files_Trashbin\Trash\ITrashItem;
+use OCP\Files\NotFoundException as FileNotFoundException;
 
 use OCA\CAFEVDB\Constants;
 use OCA\CAFEVDB\Common\Util;
+use OCA\CAFEVDB\Exceptions\Exception;
 
 /**
  * Some tweaks to for the user-folder stuff.
@@ -64,12 +70,13 @@ class UserStorage
   /** @var Folder */
   protected $userFolder;
 
+  /** {@inheritdoc} */
   public function __construct(
-    IUserSession $userSession
-    , IAppContainer $appContainer
-    , IRootFolder $rootFolder
-    , ILogger $logger
-    , IL10N $l10n
+    IUserSession $userSession,
+    IAppContainer $appContainer,
+    IRootFolder $rootFolder,
+    ILogger $logger,
+    IL10N $l10n
   ) {
     $this->appContainer = $appContainer;
     $this->rootFolder = $rootFolder;
@@ -79,7 +86,9 @@ class UserStorage
   }
 
   /**
-   * @return IUser The current user
+   * @param IUser $user The current user.
+   *
+   * @return UserStorage
    */
   public function setUser(?IUser $user):UserStorage
   {
@@ -108,8 +117,14 @@ class UserStorage
     return $this->user->getUID();
   }
 
-  /** Return the path relative to user-folder */
-  public function getUserPath(Node $node)
+  /**
+   * Return the path relative to user-folder
+   *
+   * @param Node $node The cloud FS node.
+   *
+   * @return null|string
+   */
+  public function getUserPath(Node $node):?string
   {
     $path = $node->getPath();
     $userFolderPath = $this->userFolder->getPath();
@@ -122,9 +137,12 @@ class UserStorage
   /**
    * @param string|null $path Path to lookup.
    *
-   * @return \OCP\Files\Node The user-folder or the given sub-folder
+   * @param null|string $type Only consider nodes of this type, either
+   * FileInfo::TYPE_FOLDER or FileInfo::TYPE_FILE.
+   *
+   * @return null|Node The user-folder or the given sub-folder
    */
-  public function get(?string $path, $type = null):?Node
+  public function get(?string $path, ?string $type = null):?Node
   {
     try {
       $node = empty($path) ? $this->userFolder : $this->userFolder->get($path);
@@ -132,18 +150,23 @@ class UserStorage
         return null;
       }
       return $node;
-    } catch (\OCP\Files\NotFoundException $t) {
+    } catch (FileNotFoundException $t) {
       return null;
     }
   }
 
+  /**
+   * @param string|null $path Path to lookup.
+   *
+   * @return null|File The user-folder or the given sub-folder
+   */
   public function getFile(string $path):?File
   {
     return $this->get($path, FileInfo::TYPE_FILE);
   }
 
   /**
-   * @param string $path
+   * @param string $path Path to lookup.
    *
    * @return null|Folder
    */
@@ -154,8 +177,15 @@ class UserStorage
 
   /**
    * Recursively add all files in all sub-directories to the zip archive.
+   *
+   * @param Folder $folder The folder to archive.
+   *
+   * @param int $parentsToStrip How many path components to strip from the
+   * path-names in order to form the archive name.
+   *
+   * @param ZipStream $zipStream Archive creation backend.
    */
-  private function archiveFolderRecursively(Folder $folder, int $parentsToStrip, ZipStream $zipStream)
+  private function archiveFolderRecursively(Folder $folder, int $parentsToStrip, ZipStream $zipStream):void
   {
     $folderContents = $folder->getDirectoryListing();
     /** @var Node $node */
@@ -181,8 +211,12 @@ class UserStorage
    *
    * @param int $parentsToStrip The number of parent folders to strip
    * inside the archive. The default is to strip nothing.
+   *
+   * @param string $format Defaults to 'zip' and only 'zip' is supported ATM.
+   *
+   * @return null|string
    */
-  public function getFolderArchive($pathOrFolder, $parentsToStrip = 0, string $format = 'zip'):?string
+  public function getFolderArchive($pathOrFolder, int $parentsToStrip = 0, string $format = 'zip'):?string
   {
     /** @var \OCP\Files\File $node */
     if (!($pathOrFolder instanceof Folder)) {
@@ -214,7 +248,14 @@ class UserStorage
     return $data;
   }
 
-  static public function pathCat($first, $second = null):string
+  /**
+   * @param string|array $first Paths to cat. May be an array.
+   *
+   * @param null|string $second Optional second component.
+   *
+   * @return string The concatenated paths.
+   */
+  public static function pathCat($first, ?string $second = null):string
   {
     if (is_array($first)) {
       $first = implode(self::PATH_SEP, $first);
@@ -235,8 +276,14 @@ class UserStorage
    * Strip the given number of directories from $path. Return the
    * root-path self::PATH_SEP if $depth exceeds the number of
    * path-components present.
+   *
+   * @param string $path The path to work on.
+   *
+   * @param int $depth How many components to strip from the start.
+   *
+   * @return string
    */
-  static public function stripParents(string $path, int $depth):string
+  public static function stripParents(string $path, int $depth):string
   {
     return self::PATH_SEP . implode(self::PATH_SEP, array_slice(Util::explode(self::PATH_SEP, $path), $depth));
   }
@@ -253,9 +300,9 @@ class UserStorage
    * Gracefully try to delete $path. If $path does not exist, a notice
    * is written to the log but otherwise the error is ignored.
    *
-   * @param string $path
+   * @param string $path The path to the file to delete.
    */
-  public function delete($path)
+  public function delete(string $path):void
   {
     try {
       $this->userFolder->get($path)->delete();
@@ -277,15 +324,17 @@ class UserStorage
    * @param bool $overwriteExisting If true delete any existing
    * destination file before restoring.
    *
+   * @return bool
+   *
    * @bug This method uses internal APIs.
    */
-  public function restore($path, ?array $timeInterval = null, bool $overwriteExisting = false):bool
+  public function restore(string $path, ?array $timeInterval = null, bool $overwriteExisting = false):bool
   {
     /** @var ITrashManager $trashManager */
     $trashManager = $this->appContainer->get(ITrashManager::class);
 
     if (empty($trashManager)) {
-      throw new \RuntimeException($this->l->t('Unable to restore "%s": TrashManager cannot be loaded', $path));
+      throw new RuntimeException($this->l->t('Unable to restore "%s": TrashManager cannot be loaded', $path));
     }
 
     $path = trim($path, self::PATH_SEP);
@@ -327,28 +376,40 @@ class UserStorage
   /**
    * Rename $oldPath to $newPath which are interpreted as paths
    * relative to the user's folder.
+   *
+   * @param string $oldPath The path to move.
+   *
+   * @param string $newPath The path to move to.
+   *
+   * @throws Exception If renaming fails.
    */
-  public function rename(string $oldPath, string $newPath)
+  public function rename(string $oldPath, string $newPath):void
   {
     try {
       $newFullPath = $this->userFolder->getFullPath($newPath);
       $this->userFolder->get($oldPath)->move($newFullPath);
     } catch (\Throwable $t) {
-      throw new \Exception($this->l->t('Rename of "%s" to "%s" failed.', [ $oldPath, $newPath ]), $t->getCode(), $t);
+      throw new Exception($this->l->t('Rename of "%s" to "%s" failed.', [ $oldPath, $newPath ]), $t->getCode(), $t);
     }
   }
 
   /**
    * Rename $oldPath to $newPath which are interpreted as paths
    * relative to the user's folder.
+   *
+   * @param string $oldPath The source path.
+   *
+   * @param string $newPath The destination path.
+   *
+   * @throws Exception If something goes wrong.
    */
-  public function copy(string $oldPath, string $newPath)
+  public function copy(string $oldPath, string $newPath):void
   {
     try {
       $newPath = $this->userFolder->getFullPath($newPath);
       $this->userFolder->get($oldPath)->copy($newPath);
     } catch (\Throwable $t) {
-      throw new \Exception($this->l->t('Copy of "%s" to "%s" failed.', [ $oldPath, $newPath ]), $t->getCode(), $t);
+      throw new Exception($this->l->t('Copy of "%s" to "%s" failed.', [ $oldPath, $newPath ]), $t->getCode(), $t);
     }
   }
 
@@ -361,10 +422,10 @@ class UserStorage
    * path is relative to the user storage base directory and may
    * contain multiple components.
    *
-   * @return \OCP\Files\Folder The folder object pointing to the given
+   * @return Folder The folder object pointing to the given
    * path.
    *
-   * @throws \Exception
+   * @throws FileNotFoundException If something goes wrong.
    */
   public function ensureFolder(string $path)
   {
@@ -372,9 +433,9 @@ class UserStorage
       $node = $this->userFolder->get($path);
       if ($node->getType() != FileInfo::TYPE_FOLDER) {
         $node->delete();
-        throw new \OCP\Files\NotFoundException($this->l->t("Deleted non-directory %s", [$path]));
+        throw new FileNotFoundException($this->l->t("Deleted non-directory %s", [$path]));
       }
-    } catch (\OCP\Files\NotFoundException $e) {
+    } catch (FileNotFoundException $e) {
       $node = $this->userFolder->newFolder($path);
     }
     return $node;
@@ -388,14 +449,14 @@ class UserStorage
    * '/'.implode('/', $chain)
    * ```
    *
-   * @param array $chain Path components
+   * @param array $chain Path components.
    *
-   * @return \OCP\Files\Folder The folder object pointing to the
+   * @return Folder The folder object pointing to the
    * resulting path.
    *
-   * @throws \Exception
+   * @throws Exceptionn If something goes wrong.
    */
-  public function ensureFolderChain(array $chain)
+  public function ensureFolderChain(array $chain):Folder
   {
     $path = '';
     foreach ($chain as $component) {
@@ -420,39 +481,47 @@ class UserStorage
    * Put file content by path. Create $path if it does not exists,
    * otherwise replace its contents by the given data.
    *
+   * @param string $path The file path.
+   *
+   * @param string $content The file content.
+   *
    * @return File
    */
-  public function putContent($path, $content):File
+  public function putContent(string $path, string $content):File
   {
     try {
       try {
         $file = $this->userFolder->get($path);
         $file->putContent($content);
-      } catch(\OCP\Files\NotFoundException $e) {
+      } catch (\OCP\Files\NotFoundException $e) {
         $this->ensureFolder(dirname($path));
         $file = $this->userFolder->newFile($path, $content);
       }
     } catch (\Throwable $t) {
-      throw new \RuntimeException($this->l->t('Unable to set content of file "%s".', $path), $t->getCode(), $t);
+      throw new RuntimeException($this->l->t('Unable to set content of file "%s".', $path), $t->getCode(), $t);
     }
     return $file;
   }
 
   /**
    * Get file content by path.
+   *
+   * @param string $path The file path.
+   *
+   * @return The file content.
    */
-  public function getContent($path)
+  public function getContent(string $path):string
   {
     // check if file exists and read from it if possible
     try {
-      $file = $userFolder->get($path);
-      if($file instanceof \OCP\Files\File) {
+      $file = $this->userFolder->get($path);
+      if ($file instanceof File) {
         return $file->getContent();
       } else {
-        throw new \RuntimeException($this->l->t('Cannot read from folder "%s".', $path));
+        throw new RuntimeException($this->l->t('Cannot read from folder "%s".', $path));
       }
-    } catch(\Throwable $t) {
-      throw new \RuntimeException($this->l->t('Unable to get content of file "%s".', $path), $t->getCode() , $t);
+    } catch (Throwable $t) {
+      throw new RuntimeException($this->l->t('Unable to get content of file "%s".', $path), $t->getCode(), $t);
     }
   }
 
@@ -475,10 +544,10 @@ class UserStorage
   {
     if (is_string($pathOrNode)) {
       $node = $this->userFolder->get($pathOrNode);
-    } else if ($pathOrNode instanceof \OCP\Files\Node) {
+    } elseif ($pathOrNode instanceof \OCP\Files\Node) {
       $node = $pathOrNode;
     } else {
-      throw new \InvalidArgumentException($this->l->t('Argument must be a valid path or already a file-system node.'));
+      throw new InvalidArgumentException($this->l->t('Argument must be a valid path or already a file-system node.'));
     }
     $this->logDebug('NODE '.$node->getPath().' '.$node->getInternalPath());
 
@@ -491,7 +560,7 @@ class UserStorage
     if ($node->getType() == FileInfo::TYPE_FILE) {
       $webDAVRoot = \OCP\Util::linkToRemote('webdav/');
       return $webDAVRoot.$nodePath;
-    } else if ($node->getType() == FileInfo::TYPE_FOLDER) {
+    } elseif ($node->getType() == FileInfo::TYPE_FOLDER) {
       $parent = $node->getParent();
       $parentPath = substr(strchr($parent->getPath(), 'files/'), strlen('files/'));
       return \OCP\Util::linkToAbsolute('files', 'ajax/download.php', [
@@ -499,32 +568,33 @@ class UserStorage
         'files' => $node->getName(),
       ]);
     } else {
-      throw new \InvalidArgumentException($this->l->t('Unknown file type "%s" for download file "%s".', [ $node->getType(), $node->getName() ]));
+      throw new InvalidArgumentException($this->l->t('Unknown file type "%s" for download file "%s".', [ $node->getType(), $node->getName() ]));
     }
-
   }
 
   /**
    * Generate a link to the files-app pointing to the parent folder if
    * $pathOrNode is a file, or the node itself if it is a folder.
    *
-   * https://anaxagoras.home.claus-justus-heine.de/nextcloud-git/index.php/apps/files?dir=/camerata/projects/2020/Test2020/participants/claus-justus.heine
+   * Example URL generated is
    *
-   * @param string|\OCP\Files\Node $pathOrNode The file-system path or node.
+   * https://foo.bar.com/nextcloud/index.php/apps/files?dir=/camerata/projects/2020/Test2020/participants/claus-justus.heine
+   *
+   * @param string|Node $pathOrNode The file-system path or node.
    *
    * @param bool $subDir If the $pathOrNode refers to a folder then
    * open this folder. Otherwise open the parent.
    *
    * @return string|null URL to the files app.
    */
-  public function getFilesAppLink($pathOrNode, $subDir = false):?string
+  public function getFilesAppLink($pathOrNode, bool $subDir = false):?string
   {
     if (is_string($pathOrNode)) {
       $node = $this->userFolder->get($pathOrNode);
-    } else if ($pathOrNode instanceof \OCP\Files\Node) {
+    } elseif ($pathOrNode instanceof \OCP\Files\Node) {
       $node = $pathOrNode;
     } else {
-      throw new \InvalidArgumentException($this->l->t('Argument must be a valid path or already a file-system node.'));
+      throw new InvalidArgumentException($this->l->t('Argument must be a valid path or already a file-system node.'));
     }
     if ($subDir === false || $node->getType() != FileInfo::TYPE_FOLDER) {
       $node = $node->getParent();
@@ -541,18 +611,19 @@ class UserStorage
   /**
    * Create a data-uri from the given file.
    *
-   * @param string|\OCP\Files\File $pathOrNode.
+   * @param string|\OCP\Files\File $pathOrNode Either a path or a file-system node.
    *
+   * @return null|string
    */
   public function createDataUri($pathOrNode):?string
   {
     /** @var \OCP\Files\File $node */
     if (is_string($pathOrNode)) {
       $node = $this->userFolder->get($pathOrNode);
-    } else if ($pathOrNode instanceof \OCP\Files\Node) {
+    } elseif ($pathOrNode instanceof \OCP\Files\Node) {
       $node = $pathOrNode;
     } else {
-      throw new \InvalidArgumentException($this->l->t('Argument must be a valid path or already a file-system node.'));
+      throw new InvalidArgumentException($this->l->t('Argument must be a valid path or already a file-system node.'));
     }
     $dataUri = 'data:'.$node->getMimeType().';base64,' . base64_encode($node->getContent());
     return $dataUri;
