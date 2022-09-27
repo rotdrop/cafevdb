803b6d0ab74303e9dec9eb5afd6dccf92bbcfc62
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-09-27 14:29:52 +0200

    CS AddressBook/MusicianCardBackend.php

--- lib/AddressBook/MusicianCardBackend.php
+++ lib/AddressBook/MusicianCardBackend.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
  *
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
- * @author Claus-Justus Heine
- * @copyright 2021, 2022 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @author Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2021, 2022 Claus-Justus Heine
+ * @license AGPL-3.0-or-later
  *
  * This file based on ldap_contacts_backend, copyright 2020 Arthur Schiwon
  * <blizzz@arthur-schiwon.de>
  *
- * @license GNU AGPL version 3 or any later version
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -25,10 +25,11 @@ declare(strict_types=1);
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 namespace OCA\CAFEVDB\AddressBook;
+
+use \Sabre\DAV\Exception\NotFound as SabreNotFoundException;
 
 use OCA\CAFEVDB\Service\ConfigService;
 use OCA\CAFEVDB\Service\ContactsService;
@@ -36,6 +37,7 @@ use OCA\CAFEVDB\Database\EntityManager;
 use OCA\CAFEVDB\Database\Doctrine\ORM\Entities;
 use OCA\CAFEVDB\Database\Doctrine\ORM\Repositories;
 
+/** Generate VCards from the musicians database. */
 class MusicianCardBackend implements ICardBackend
 {
   use \OCA\CAFEVDB\Traits\ConfigTrait;
@@ -47,10 +49,11 @@ class MusicianCardBackend implements ICardBackend
   /** @var ContactsService */
   private $contactsService;
 
+  /** {@inheritdoc} */
   public function __construct(
-    ConfigService $configService
-    , ContactsService $contactsService
-    , EntityManager $entityManager
+    ConfigService $configService,
+    ContactsService $contactsService,
+    EntityManager $entityManager,
   ) {
     $this->configService = $configService;
     $this->l = $this->l10n();
@@ -61,11 +64,15 @@ class MusicianCardBackend implements ICardBackend
     }
   }
 
-  public function getURI(): string {
+  /** {@inheritdoc} */
+  public function getURI(): string
+  {
     return $this->appName().'-musicians';
   }
 
-  public function getDisplayName(): string {
+  /** {@inheritdoc} */
+  public function getDisplayName(): string
+  {
     $name = $this->getConfigValue('musiciansaddressbook');
     if (empty($name)) {
       $name = $this->l->t('%s Musicians', ucfirst($this->getConfigValue('orchestra', 'unknown')));
@@ -74,19 +81,23 @@ class MusicianCardBackend implements ICardBackend
   }
 
   /**
-   * @throws \Sabre\DAV\Exception\NotFound
+   * {@inheritdoc}
+   *
+   * @throws SabreNotFoundException
    */
   public function getCard($name): MusicianCard
   {
     $uuid = $this->getUuidFromUri($name);
     $musician = $this->musiciansRepository->findOneBy([ 'uuid' => $uuid ]);
     if (empty($musician)) {
-      throw new \Sabre\DAV\Exception\NotFound();
+      throw new SabreNotFoundException;
     }
     return $this->entryToCard($musician);
   }
 
-  /*
+  /**
+   * {@inheritdoc}
+   *
    * @todo To support $searchProperties we would need the mapping from
    * VCard properties to data-base properties.
    */
@@ -121,6 +132,7 @@ class MusicianCardBackend implements ICardBackend
     return $vCards;
   }
 
+  /** {@inheritdoc} */
   public function getCards(): array
   {
     // to appear in the contacts app, this must really return everything
@@ -133,16 +145,19 @@ class MusicianCardBackend implements ICardBackend
     return $vCards;
   }
 
+  /** {@inheritdoc} */
   public function getUriFromUuid($uuid)
   {
     return 'musician-'.$uuid.'.vcf';
   }
 
+  /** {@inheritdoc} */
   protected function getUuidFromUri($uri)
   {
     return substr($uri, strlen('musician-'), 36);
   }
 
+  /** {@inheritdoc} */
   protected function entryToCard(Entities\Musician $musician, ?int $lastModified = null): MusicianCard
   {
     $vCard = $this->contactsService->export($musician);
@@ -155,6 +170,7 @@ class MusicianCardBackend implements ICardBackend
     return new MusicianCard($uri, $lastModified, $vCard);
   }
 
+  /** {@inheritdoc} */
   public function getLastModified(?string $uri = null):int
   {
     $criteria = empty($uri) ? [] : [ 'uuid' => $this->getUuidFromUri($uri) ];
