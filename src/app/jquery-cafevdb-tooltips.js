c0ede77e0d0346abe10557938d9a1e8ff3603c6c
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-09-29 12:58:40 +0200

    JS tweak the too-costly tooltip generation s.t. it is handled by kind-of background jobs.
    This use setTimer(singleToolTipGenerator, 0) in order to give the
    event-loop a chance to run. Hopefully this will do the trick.
    
    The tooltip generation rate on my computer is only 1000/s, so the 20000
    popups needed for a large page take roughly 20 seconds.

--- src/app/jquery-cafevdb-tooltips.js
+++ src/app/jquery-cafevdb-tooltips.js
@@ -35,15 +35,20 @@ require('tooltips.css');
 
 console.log('jquery-cafevdb-tooltips');
 
-const whiteList = $.extend(
-  {},
-  $.fn.tooltip.Constructor.Default.whiteList,
-  {
-    table: [], thead: [], tbody: [], tr: [], td: [], th: [], dl: [], dt: [], dd: [],
-  }
-);
+const whiteList = {
+  ...$.fn.tooltip.Constructor.Default.whiteList,
+  table: [],
+  thead: [],
+  tbody: [],
+  tr: [],
+  td: [],
+  th: [],
+  dl: [],
+  dt: [],
+  dd: [],
+};
 
-const options = {
+const defaultOptions = {
   container: 'body',
   html: true,
   sanitize: true, // @todo just tweak whitelist
@@ -54,6 +59,120 @@ const options = {
   boundary: 'viewport',
   // delay: { show: 500, hide: 100000 },
 };
+
+let backGroundCount = 0;
+let maxBackGroundCount = 0;
+
+let backGroundDeferred = $.Deferred();
+let backGroundPromise = backGroundDeferred.promise();
+
+const rejectBackgroundPromise = function() {
+  backGroundDeferred.reject(maxBackGroundCount);
+  maxBackGroundCount = 0;
+  backGroundDeferred = $.Deferred();
+  backGroundPromise = backGroundDeferred.promise();
+};
+
+const unregisterBackgroundJob = function() {
+  if (--backGroundCount === 0) {
+    backGroundDeferred.resolve(maxBackGroundCount);
+    maxBackGroundCount = 0;
+    backGroundDeferred = $.Deferred();
+    backGroundPromise = backGroundDeferred.promise();
+  }
+};
+
+const lockElement = function($element) {
+  if ($element.data(appName + '-tooltip-lock') === true) {
+    return false;
+  }
+  $element.data(appName + '-tooltip-lock', true);
+  if (++backGroundCount > maxBackGroundCount) {
+    maxBackGroundCount = backGroundCount;
+  }
+  return true;
+};
+
+const unlockElement = function($element) {
+  unregisterBackgroundJob();
+  $element.data(appName + '-tooltip-lock', false);
+};
+
+function singleToolTipWorker(optionsForAll) {
+  const $this = $(this);
+  const selfOptions = $.extend(true, {}, optionsForAll);
+  const classAttr = $this.attr('class');
+  if (classAttr) {
+    if (classAttr.match(/tooltip-off/) !== null) {
+      $this.cafevTooltip('disable');
+      unlockElement($this);
+      return;
+    }
+    const tooltipClasses = classAttr.match(/tooltip-[a-z-]+/g);
+    if (tooltipClasses) {
+      for (let idx = 0; idx < tooltipClasses.length; ++idx) {
+        const tooltipClass = tooltipClasses[idx];
+        const placement = tooltipClass.match(/^tooltip-(bottom|top|right|left)$/);
+        if (placement && placement.length === 2 && placement[1].length > 0) {
+          selfOptions.placement = placement[1];
+          continue;
+        }
+        selfOptions.cssclass.push(tooltipClass);
+      }
+    }
+  }
+  $.fn.tooltip.call($this, 'dispose');
+  const appTitle = $this.data(appName + 'Title');
+  if (appTitle && !$this.attr('title')) {
+    $this.attr('title', appTitle);
+  } else {
+    const originalTitle = $this.data('original-title');
+    if (originalTitle && !$this.attr('title')) {
+      $this.attr('title', originalTitle);
+    }
+  }
+  $this.removeData(appName + 'Title');
+  $this.removeAttr('data-' + appName + '-title');
+  $this.removeAttr('data-original-title');
+  $this.removeData('original-title');
+  // const title = $this.attr('title');
+  // if ((title === undefined || title.trim() === '') && !$this.is(':invalid')) {
+  //   $this.removeAttr('title');
+  //   $this.cafevTooltip('destroy');
+  //   return;
+  // }
+  if (!selfOptions.title) {
+    $this.data(appName + 'Title', $this.attr('title'));
+    $this.attr('data-' + appName + '-title', $this.attr('title'));
+    $this.removeAttr('title');
+    selfOptions.title = function() {
+      const $this = $(this);
+      const originalTitle = $this.data(appName + 'Title');
+      if ($this.is(':invalid')) {
+        const invalidHint = t(appName, 'Please fill out this field!');
+        if (!selfOptions.html) {
+          return invalidHint + (originalTitle ? '\n' + originalTitle : '');
+        }
+        let titleHtml = `<div class="tooltip-field-required">${invalidHint}</div>`;
+        if (originalTitle) {
+          titleHtml += `<div class="tooltip-original-title">${originalTitle}</div>`;
+        }
+        return titleHtml;
+      }
+      return originalTitle;
+    };
+  }
+  if (!selfOptions.template) {
+    selfOptions.template = '<div class="tooltip '
+      + selfOptions.cssclass.join(' ')
+      + '" role="tooltip">'
+      + '<div class="tooltip-arrow"></div>'
+      + '<div class="tooltip-inner"></div>'
+      + '</div>';
+  }
+  $.fn.tooltip.call($this, selfOptions);
+  unlockElement($this);
+}
 
 /**
  * Extend the tooltips to honour some special class elements, and
@@ -66,100 +185,38 @@ const options = {
  *
  */
 $.fn.cafevTooltip = function(argument) {
+  const $this = this;
   if (typeof argument === 'undefined') {
     argument = {};
   }
   if (typeof argument === 'object' && argument != null) {
-    argument = $.extend(true, {}, options, argument);
-    if (typeof argument.placement === 'string') {
-      const words = argument.placement.split(' ');
+    const optionsForAll = $.extend(true, {}, defaultOptions, argument);
+    if (typeof optionsForAll.placement === 'string') {
+      const words = optionsForAll.placement.split(' ');
       if (words.length > 1) {
         for (const word of words) {
           if (word !== 'auto') {
-            argument.placement = word;
+            optionsForAll.placement = word;
             break;
           }
         }
       }
     }
-    if (argument.cssclass && typeof argument.cssclass === 'string') {
-      argument.cssclass = [argument.cssclass];
+    if (optionsForAll.cssclass && typeof optionsForAll.cssclass === 'string') {
+      optionsForAll.cssclass = [optionsForAll.cssclass];
     }
-    argument.cssclass.push('cafevdb');
-    // iterator over individual element in order to pick up the
-    // correct class-arguments.
-    this.each(function(index) {
-      const self = $(this);
-      const selfOptions = $.extend(true, {}, argument);
-      const classAttr = self.attr('class');
-      if (classAttr) {
-        if (classAttr.match(/tooltip-off/) !== null) {
-          self.cafevTooltip('disable');
-          return;
-        }
-        const tooltipClasses = classAttr.match(/tooltip-[a-z-]+/g);
-        if (tooltipClasses) {
-          for (let idx = 0; idx < tooltipClasses.length; ++idx) {
-            const tooltipClass = tooltipClasses[idx];
-            const placement = tooltipClass.match(/^tooltip-(bottom|top|right|left)$/);
-            if (placement && placement.length === 2 && placement[1].length > 0) {
-              selfOptions.placement = placement[1];
-              continue;
-            }
-            selfOptions.cssclass.push(tooltipClass);
-          }
-        }
+    optionsForAll.cssclass.push('cafevdb');
+    // Iterator over individual element in order to pick up the
+    // correct class-arguments. The setTimeout() hack is in order to
+    // fake background jobs and keep the UI somewhat response.
+    //
+    // @todo This has to be reworked, tooltips just take too much time.
+    $this.each(function(index) {
+      const $element = $(this);
+      if (!lockElement($element)) {
+        return;
       }
-      $.fn.tooltip.call(self, 'dispose');
-      const appTitle = self.data(appName + 'Title');
-      if (appTitle && !self.attr('title')) {
-        self.attr('title', appTitle);
-      } else {
-        const originalTitle = self.data('original-title');
-        if (originalTitle && !self.attr('title')) {
-          self.attr('title', originalTitle);
-        }
-      }
-      self.removeData(appName + 'Title');
-      self.removeAttr('data-' + appName + '-title');
-      self.removeAttr('data-original-title');
-      self.removeData('original-title');
-      // const title = self.attr('title');
-      // if ((title === undefined || title.trim() === '') && !self.is(':invalid')) {
-      //   self.removeAttr('title');
-      //   self.cafevTooltip('destroy');
-      //   return;
-      // }
-      if (!selfOptions.title) {
-        self.data(appName + 'Title', self.attr('title'));
-        self.attr('data-' + appName + '-title', self.attr('title'));
-        self.removeAttr('title');
-        selfOptions.title = function() {
-          const $this = $(this);
-          const originalTitle = $this.data(appName + 'Title');
-          if ($this.is(':invalid')) {
-            const invalidHint = t(appName, 'Please fill out this field!');
-            if (!selfOptions.html) {
-              return invalidHint + (originalTitle ? '\n' + originalTitle : '');
-            }
-            let titleHtml = `<div class="tooltip-field-required">${invalidHint}</div>`;
-            if (originalTitle) {
-              titleHtml += `<div class="tooltip-original-title">${originalTitle}</div>`;
-            }
-            return titleHtml;
-          }
-          return originalTitle;
-        };
-      }
-      if (!selfOptions.template) {
-        selfOptions.template = '<div class="tooltip '
-          + selfOptions.cssclass.join(' ')
-          + '" role="tooltip">'
-          + '<div class="tooltip-arrow"></div>'
-          + '<div class="tooltip-inner"></div>'
-          + '</div>';
-      }
-      $.fn.tooltip.call(self, selfOptions);
+      setTimeout(() => singleToolTipWorker.call($(this), optionsForAll));
     });
   } else {
     if (argument === 'destroy') {
@@ -167,7 +224,7 @@ $.fn.cafevTooltip = function(argument) {
     }
     $.fn.tooltip.apply(this, arguments);
   }
-  return this;
+  return $this;
 };
 
 $.fn.cafevTooltip.enable = function() {
@@ -190,6 +247,12 @@ $.fn.cafevTooltip.remove = function() {
 
 $.fn.cafevTooltip.hide = function() {
   $('[data-original-title]').cafevTooltip('hide');
+};
+
+export {
+  backGroundCount,
+  backGroundPromise,
+  rejectBackgroundPromise,
 };
 
 // Local Variables: ***
