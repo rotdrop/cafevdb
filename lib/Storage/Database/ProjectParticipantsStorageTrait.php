122af19225e4cb70843393a4c7b8d4c0b6ae102b
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-10-11 17:26:57 +0200

    CS lib/Storage/Database/ProjectParticipantsStorageTrait.php

--- lib/Storage/Database/ProjectParticipantsStorageTrait.php
+++ lib/Storage/Database/ProjectParticipantsStorageTrait.php
@@ -4,8 +4,8 @@
  *
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
- * @author Claus-Justus Heine
- * @copyright 2011-2014, 2016, 2020, 2021, 2022, Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @author Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2011-2014, 2016, 2020, 2021, 2022, Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -32,6 +32,10 @@ use OCA\CAFEVDB\Database\Doctrine\ORM\Entities;
 
 use OCA\CAFEVDB\Common\Util;
 
+/**
+ * Actually just provide some path names, translated with the app's configured
+ * locale.
+ */
 trait ProjectParticipantsStorageTrait
 {
   /**
@@ -138,6 +142,10 @@ trait ProjectParticipantsStorageTrait
 
   /**
    * PME-legacy.
+   *
+   * @param string $debitMandateReference
+   *
+   * @return string
    */
   protected function getLegacyDebitMandateFileName(string $debitMandateReference):string
   {
