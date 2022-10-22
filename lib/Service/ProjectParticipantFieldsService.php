443b3fabd0d4ce7f9596a46b6c7c69a27332254d
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-10-22 10:56:03 +0200

    ProjectParticipantFieldsService, use DatabaseStorageFile instead of EncryptedFile

--- lib/Service/ProjectParticipantFieldsService.php
+++ lib/Service/ProjectParticipantFieldsService.php
@@ -598,7 +598,7 @@ class ProjectParticipantFieldsService
         if (empty($value)) {
           return null;
         }
-        return $this->getDatabaseRepository(Entities\EncryptedFile::class)->find($value);
+        return $this->getDatabaseRepository(Entities\DatabaseStorageFile::class)->find($value);
       case DataType::TEXT:
       case DataType::HTML:
       default:
