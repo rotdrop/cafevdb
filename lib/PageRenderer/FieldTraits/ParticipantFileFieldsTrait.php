d57463d0ee8d49b3d642e45822d716db93ae8afd
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-01-08 21:15:31 +0100

    PageRenderer\FieldTraits: fix logExcetion() usage.

--- lib/PageRenderer/FieldTraits/ParticipantFileFieldsTrait.php
+++ lib/PageRenderer/FieldTraits/ParticipantFileFieldsTrait.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2011-2022 Claus-Justus Heine
+ * @copyright 2011-2023 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -334,7 +334,7 @@ trait ParticipantFileFieldsTrait
         $filesAppLink = $this->userStorage->getFilesAppLink($folderPath, true);
         break;
       } catch (/*\OCP\Files\NotFoundException*/ \Throwable $e) {
-        $this->logException($e, [ 'level' => \OCP\ILogger::DEBUG ]);
+        $this->logException($e, level: \OCP\ILogger::DEBUG);
         array_pop($pathChain);
       }
     }
