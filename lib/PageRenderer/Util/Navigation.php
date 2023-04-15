364f6f1ecea2a5cc9cb8d7316b61e3a9069d88b9
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-04-15 16:17:25 +0200

    Export menu, remove ODS option as it has been disabled and probably will remain disabled.

--- lib/PageRenderer/Util/Navigation.php
+++ lib/PageRenderer/Util/Navigation.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2011-2016, 2020, 2021, 2022 Claus-Justus Heine
+ * @copyright 2011-2016, 2020, 2021, 2022, 2023 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -245,9 +245,9 @@ class Navigation
           .'title="'.$this->toolTipsService['pme-export-choice'].'" '
           .'name="export" >
   <option value=""></option>
-  <option disabled '
+  <!-- <option disabled '
     .'title="'.$this->toolTipsService['pme-export-ods'].'" '
-    .'value="ODS">'.$this->l->t('LibreOffice Export').'</option>
+    .'value="ODS">'.$this->l->t('LibreOffice Export').'</option> -->
   <option '
     .'title="'.$this->toolTipsService['pme-export-excel'].'" '
     .'value="EXCEL">'.$this->l->t('Excel Export').'</option>
