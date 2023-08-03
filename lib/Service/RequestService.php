cab201cb5e9511ce2f85320d2e7129536176b0d4
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-08-03 15:58:51 +0200

    Fix calling convention for CTOR of RequestService.

--- lib/Service/RequestService.php
+++ lib/Service/RequestService.php
@@ -42,12 +42,10 @@ class RequestService extends ToolkitService
   public function __construct(
     IRequest $request,
     IURLGenerator $urlGenerator,
-    ISession $session,
     ILogger $logger,
     IL10N $l10n,
-    bool $closeSession = true,
   ) {
-    parent::__construct($request, $urlGenerator, $session, $logger, $l10n, $closeSession);
+    parent::__construct($request, $urlGenerator, $logger, $l10n);
   }
   // phpcs:enable Squiz.Commenting.FunctionComment.Missing
 }
