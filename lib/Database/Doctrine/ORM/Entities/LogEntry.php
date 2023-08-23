bbbafff6e1991649ce11a531dbf7e015f3edd656
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-08-23 15:15:45 +0200

    Entities\LogEntry, php doc.

--- lib/Database/Doctrine/ORM/Entities/LogEntry.php
+++ lib/Database/Doctrine/ORM/Entities/LogEntry.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020, 2022 Claus-Justus Heine
+ * @copyright 2020, 2022, 2023 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -99,7 +99,7 @@ class LogEntry extends Loggable\Entity\MappedSuperclass\AbstractLogEntry
   /**
    * Get remoteAddress.
    *
-   * @return Image|null
+   * @return null|string
    */
   public function getRemoteAddress()
   {
