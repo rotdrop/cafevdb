1072f509e91a9f49d83282a37ed563ed6251b92a
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-01-08 10:27:32 +0100

    NC removed some legacy app-navigation css rules, resurrect them.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2011-2016, 2020, 2021, 2022, 2023 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2011-2016, 2020, 2021, 2022, 2023, 2024 Claus-Justus Heine <himself@claus-justus-heine.de>
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -46,6 +46,7 @@ import './app/jquery-extensions.js';
 
 require('jquery-ui');
 
+require('app-navigation.scss');
 require('navsnapper.scss');
 require('variables-generator.scss');
 
