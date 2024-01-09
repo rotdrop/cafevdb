f37a67d3d503dbd0bdce9723c3fb634e86466092
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-01-09 14:55:15 +0100

    Replace legacy load-additional-scripts handling.

---
+++ lib/Listener/LoadAdditionalScriptsEventListener.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2024 Claus-Justus Heine
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Listener;
+
+use Throwable;
+
+use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent as HandledEvent;
+use OCP\EventDispatcher\Event;
+use OCP\EventDispatcher\IEventListener;
+use OCP\AppFramework\IAppContainer;
+use OCP\IRequest;
+use Psr\Log\LoggerInterface as ILogger;
+use Psr\Log\LogLevel;
+use OCP\IConfig;
+use OCP\AppFramework\Services\IInitialState;
+
+use OCA\CAFEVDB\Service\AssetService;
+
+/** Load additional scripts while running interactively. */
+class LoadAdditionalScriptsEventListener implements IEventListener
+{
+  use \OCA\CAFEVDB\Toolkit\Traits\LoggerTrait;
+  use \OCA\CAFEVDB\Toolkit\Traits\ApiRequestTrait;
+
+  const EVENT = HandledEvent::class;
+
+  /** @var IAppContainer */
+  private $appContainer;
+
+  // phpcs:disable Squiz.Commenting.FunctionComment.Missing
+  public function __construct(IAppContainer $appContainer)
+  {
+    $this->appContainer = $appContainer;
+    $this->logger = $this->appContainer->get(ILogger::class);
+  }
+  // phpcs:enable Squiz.Commenting.FunctionComment.Missing
+
+  /** {@inheritdoc} */
+  public function handle(Event $event):void
+  {
+    $this->logger = $this->appContainer->get(ILogger::class);
+    if (!($event instanceof HandledEvent)) {
+      return;
+    }
+    /** @var HandledEvent $event */
+
+    $this->logger = $this->appContainer->get(ILogger::class);
+
+    $request = $this->appContainer->get(IRequest::class);
+    if ($this->isNonInteractiveRequest($request, LogLevel::DEBUG)) {
+      return;
+    }
+
+    if (!$event->isLoggedIn()) {
+      // the scripts loaded here need authentication, so ...
+      return;
+    }
+
+    try {
+      /** @var string $appName */
+      $appName = $this->appContainer->get('appName');
+
+      /** @var AssetService $assetService */
+      $assetService = $this->appContainer->get(AssetService::class);
+
+      $scriptFile = $assetService->getJSAsset('background-jobs')['asset'];
+      \OCP\Util::addScript($appName, $scriptFile);
+      $this->logDebug('Added script file "' . $scriptFile . '" to template');
+    } catch (Throwable $t) {
+      $this->logException($t, 'Unable add the refresh java script while running interactively.');
+    }
+  }
+}
--- lib/Listener/Registration.php
+++ lib/Listener/Registration.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020, 2021, 2022, 2023 Claus-Justus Heine
+ * @copyright 2020, 2021, 2022, 2023, 2024 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -68,6 +68,7 @@ class Registration
     self::registerListener($context, SepaBulkTransactionSubmittedListener::class);
     self::registerListener($context, SepaBulkTransactionAnnouncedListener::class);
     self::registerListener($context, GroupMembershipListener::class);
+    self::registerListener($context, LoadAdditionalScriptsEventListener::class);
   }
 
   /**
