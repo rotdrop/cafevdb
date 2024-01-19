e1564bd039c40ba8c93edfb4b3d394a0b3b58f35
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-01-19 23:37:12 +0100

    files-hooks.ts: tweak the new-file menu.
    In some context the general NFM just makes no sense. Luckily, with the
    new NC framework it is quite easy to blend out itmes. Do this.

--- src/files-hooks.ts
+++ src/files-hooks.ts
@@ -24,16 +24,18 @@
 import Vue from 'vue';
 import { appName } from './app/app-info.js';
 import { getInitialState } from './services/initial-state-service.js';
+import { basename } from 'path';
 import { generateFilePath } from '@nextcloud/router';
-import { showError, showInfo } from '@nextcloud/dialogs';
-import { addNewFileMenuEntry, registerFileAction, FileAction, Node, FileType, Permission, View } from '@nextcloud/files';
+import { emit } from '@nextcloud/event-bus';
+import { showInfo, showSuccess } from '@nextcloud/dialogs';
+import { addNewFileMenuEntry, getNewFileMenuEntries, registerFileAction, FileAction, Node, Folder, FileType, Permission, View } from '@nextcloud/files';
+import type { Entry } from '@nextcloud/files';
 import { action as sidebarAction } from '../../files/src/actions/sidebarAction.ts';
 import { translate as t, translatePlural as n } from '@nextcloud/l10n';
+import axios from '@nextcloud/axios';
 import Tooltip from '@nextcloud/vue/dist/Directives/Tooltip.js'
 
 import logoSvg from '../img/cafevdb.svg?raw';
-
-console.info('LOGO SVG', logoSvg);
 
 declare global {
   interface Window {
@@ -71,8 +73,8 @@ const validTemplatePath = function(path: string) {
   return path.startsWith(initialState.sharing.files.folders.templates);
 };
 
-const getProjectNameFromProjectBalancesFolders = function(dirInfo) {
-  let dirName = dirInfo.path;
+const getProjectNameFromProjectBalancesFolders = function(folder: Folder) {
+  let dirName = folder.dirname;
   if (!dirName.startsWith(initialState.sharing.files.folders.projectBalances)) {
     return null;
   }
@@ -83,7 +85,7 @@ const getProjectNameFromProjectBalancesFolders = function(dirInfo) {
   return projectName;
 };
 
-const getProjectYearFromProjectName = function(projectName) {
+const getProjectYearFromProjectName = function(projectName: string|null) {
   if (!projectName) {
     return null;
   }
@@ -94,27 +96,27 @@ const getProjectYearFromProjectName = function(projectName) {
   return null;
 };
 
-const isProjectBalanceSupportingDocumentsTopFolder = function(dirInfo, projectName) {
-  projectName = projectName || getProjectNameFromProjectBalancesFolders(dirInfo);
+const isProjectBalanceSupportingDocumentsTopFolder = function(folder: Folder, projectName: string|null) {
+  projectName = projectName || getProjectNameFromProjectBalancesFolders(folder);
   if (!projectName) {
     return false;
   }
-  const dirName = dirInfo.path;
-  const baseName = dirInfo.name;
+  const dirName = folder.dirname;
+  const baseName = folder.basename;
   return dirName.startsWith(projectBalancesFolder)
     && baseName === supportingDocumentsFolder;
 };
 
-const isProjectBalanceSupportingDocumentsFolder = function(dirInfo, projectName, projectYear) {
-  projectName = projectName || getProjectNameFromProjectBalancesFolders(dirInfo);
+const isProjectBalanceSupportingDocumentsFolder = function(folder: Folder, projectName: string|null, projectYear: string|null) {
+  projectName = projectName || getProjectNameFromProjectBalancesFolders(folder);
   if (!projectName) {
     return false;
   }
   projectYear = projectYear || getProjectYearFromProjectName(projectName);
-  const dirName = dirInfo.path;
-  const baseName = dirInfo.name;
+  const dirName = folder.dirname;
+  const baseName = folder.basename;
   if (projectYear) {
-    return isProjectBalanceSupportingDocumentsTopFolder(dirInfo, projectName);
+    return isProjectBalanceSupportingDocumentsTopFolder(folder, projectName);
   } else {
     const result = dirName.startsWith(projectBalancesFolder + '/' + projectName + '/' + supportingDocumentsFolder)
       && baseName.match(/\d{4}$/);
@@ -165,176 +167,161 @@ registerFileAction(new FileAction({
     return null;
   },
   inline: () => true,
+  order: -1000000,
 }));
 
-addNewFileMenuEntry({
-  id: appName + '-project-supporting-document-folder',
-  displayName: t(appName, 'New Supporting Document'),
-  enabled() {
-  },
-});
+type createFolderResponse = {
+  fileid: number
+  source: string
+}
 
-addNewFileMenuEntry({
-  id: appName + '-project-supporting-document-year-folder',
-  displayName: t(appName, 'New Supporting Document '),
-  enabled() {
-  },
-});
+const createNewFolder = async (root: Folder, name: string): Promise<createFolderResponse> => {
+  const source = root.source + '/' + name
+  const encodedSource = root.encodedSource + '/' + encodeURIComponent(name)
 
+  const response = await axios({
+    method: 'MKCOL',
+    url: encodedSource,
+    headers: {
+      Overwrite: 'F',
+    },
+  })
+  return {
+    fileid: parseInt(response.headers['oc-fileid']),
+    source,
+  }
+}
+
+class SupportingDocumentEntry implements Entry
+{
+  private projectName: string|null = null;
+  private projectYear: string|null = null;
+  private isTopFolder: boolean = false;
+
+  public id: string;
+  public displayName: string;
+  public iconClass: string = 'icon-folder';
+  public order: number = 1000000;
+
+  public constructor(appName: string) {
+    this.id = appName + '-project-supporting-document-folder';
+    this.displayName = t(appName, 'New Supporting Document');
+  };
+  public enabled(folder: Folder) {
+    console.trace('ENABLED HELLO');
+    const projectName = getProjectNameFromProjectBalancesFolders(folder);
+    const projectYear = getProjectYearFromProjectName(projectName);
+
+    const isTopFolder = !!projectName && isProjectBalanceSupportingDocumentsTopFolder(folder, projectName);
+    const isDocumentsFolder = projectName && isProjectBalanceSupportingDocumentsFolder(folder, projectName, projectYear);
+
+    if (!isTopFolder && !isDocumentsFolder) {
+      this.projectName = null;
+      this.projectYear = null;
+      return false;
+    }
+
+    this.projectName = projectName;
+    this.projectYear = projectYear;
+    this.isTopFolder = isTopFolder;
+
+    if (!projectYear && isTopFolder) {
+      this.displayName = t(appName, 'New Year Folder');
+    } else {
+      this.displayName = t(appName, 'New Supporting Document');
+    }
+
+    return true;
+  };
+  public async handler(folder: Folder, content: Node[]) {
+    if (!this.projectYear && this.isTopFolder) {
+      await this.yearFolderHandler(folder, content);
+    } else {
+      await this.supportingDocumentHandler(folder, content);
+    }
+  };
+  private async yearFolderHandler(folder: Folder, content: Node[]) {
+    const year = '' + new Date().getFullYear();
+    let dirName = '' + year;
+    const yearFolders = content.filter((node: Node) => node.basename.match(/^\d{4}$/));
+    const existing = yearFolders.find((node: Node) => node.basename === dirName);
+    if (existing) {
+      const maxYear = yearFolders.reduce((accumulator: number, currentValue: Node) => Math.max(accumulator, +currentValue.basename), +yearFolders[0].basename);
+      dirName = '' + (maxYear + 1);
+    }
+    showInfo(t(appName, 'Year determined as {year}.', { year: dirName }));
+    const { fileid, source } = await createNewFolder(folder, dirName);
+
+    // Create the folder in the store
+    const newFolder = new Folder({
+      source,
+      id: fileid,
+      mtime: new Date(),
+      owner: null,
+      permissions: Permission.ALL,
+      root: folder?.root || 'this must not happen',
+    })
+
+    showSuccess(t('files', 'Created new folder "{name}"', { name: basename(source) }))
+    emit('files:node:created', newFolder)
+    emit('files:node:rename', newFolder)
+  };
+  private async supportingDocumentHandler(folder: Folder, content: Node[]) {
+    const folderPrefix = this.projectYear ? this.projectName : this.projectName + '-' + folder.basename;
+    const nameRegExp = new RegExp('^(?:' + folderPrefix + '-?)?' + '\\d{3}$');
+    const sequenceFolders = content.filter((node: Node) => node.basename.match(nameRegExp));
+    const sequences = sequenceFolders.map((node: Node) => +node.basename.substring(node.basename.length - 3));
+    sequences.sort((a, b) => a - b);
+    let sequence: number;
+    if (sequences[sequences.length - 1] !== sequences.length) {
+      // find first hole, inefficiently
+      let previous = 0;
+      for (const current of sequences) {
+        if (current - previous !== 1) {
+          break;
+        }
+      }
+      sequence = previous + 1;
+    } else {
+      sequence = sequences.length + 1;
+    }
+    const sequenceString = String(sequence).padStart(3, '0');
+    showInfo(t(appName, 'Document sequence determined as {sequence}.', { sequence: sequenceString }));
+    const dirName = folderPrefix + '-' + sequenceString;
+    const { fileid, source } = await createNewFolder(folder, dirName);
+
+    // Create the folder in the store
+    const newFolder = new Folder({
+      source,
+      id: fileid,
+      mtime: new Date(),
+      owner: null,
+      permissions: Permission.ALL,
+      root: folder?.root || 'this must not happen',
+    })
+
+    showSuccess(t('files', 'Created new folder "{name}"', { name: basename(source) }))
+    emit('files:node:created', newFolder)
+    emit('files:node:rename', newFolder)
+  };
+};
+
+const supportingDocumentsEntry = new SupportingDocumentEntry(appName);
+
+addNewFileMenuEntry(supportingDocumentsEntry);
+
+/*
+ * In special locations generic "new file" action should be very restricted.
+ */
 window.addEventListener('DOMContentLoaded', () => {
-
-  // menu file-actions can only depend on the literal local file-name,
-  // the type and the mime-type.
-  //
-  // inline file-actions are probably a relict from earlier days and
-  // can conditionally be enabled via the "shouldRender()" hook. The
-  // provided context give access to several interesting data like the
-  // directory and owner etc.
-
-  if (OC.Plugins) {
-    OC.Plugins.register('OCA.Files.NewFileMenu', {
-      menuData: {
-        id: 'project-supporting-document',
-        displayName: t(appName, 'New Supporting Document'),
-        templateName: t(appName, 'PROJECTNAME-XXX'),
-        folderPrefix: 'PROJECTNAME',
-        projectYear: 'PROJECTYEAR',
-        isTopFolder: true,
-        fileList: null,
-        iconClass: 'icon-folder',
-        fileType: 'httpd/unix-directory',
-        async supportingDocumentHandler(name) {
-          const nameRegExp = new RegExp('^(?:' + this.folderPrefix + '-?)?' + '(\\d{3}|XXX)$');
-          const sequenceMatch = name.match(nameRegExp);
-          if (!sequenceMatch) {
-            showError(t(appName, 'The name of the new document in support must match the format "{projectName}-XXX" where "XXX" is a placeholder for 3 decimal digits or a literal "XXX" in which case the next available sequence number is chosen automatically.', this));
-          }
-          let sequence = sequenceMatch[1];
-          if (sequence === 'XXX') {
-            const sequences = [];
-            for (const file of this.fileList.files) {
-              if (file.mimetype !== 'httpd/unix-directory') {
-                continue;
-              }
-              if (!file.name.match(nameRegExp)) {
-                continue;
-              }
-              sequences.push(+file.name.substr(file.name.length - 3));
-            }
-            sequences.sort((a, b) => a - b);
-            let previous = 0;
-            for (const current of sequences) {
-              if (current - previous !== 1) {
-                break;
-              }
-              previous = current;
-            }
-            sequence = previous + 1;
-          }
-          sequence = String(sequence).padStart(3, '0');
-          showInfo(t(appName, 'Document sequence determined as {sequence}.', { sequence }));
-          const dirName = this.folderPrefix + '-' + sequence;
-          await this.fileList.createDirectory(dirName);
-        },
-        async yearFolderHandler(name) {
-          const nameRegExp = /^(\d{4}|YYYY)$/;
-          const yearMatch = name.match(nameRegExp);
-          if (!yearMatch) {
-            showError(t(appName, 'The name of the new folder must match the format "YYYY" where "YYYY" is a placeholder for 4 decimal digits or a literal "YYYY" in which case a folder for the current year is created if it does not exist already.'));
-          }
-          let year = yearMatch[1];
-          if (year === 'YYYY') {
-            year = new Date().getFullYear();
-          }
-          showInfo(t(appName, 'Year determined as {year}.', { year }));
-          const dirName = '' + year;
-          const existing = this.fileList.files.find(file => file.name === dirName);
-          if (existing) {
-            showError(t(appName, 'There is already a directory or file with the name "{dirName}".', { dirName }));
-            return false;
-          }
-          await this.fileList.createDirectory(dirName);
-        },
-        async actionHandler(name) {
-          if (!this.projectYear && this.isTopFolder) {
-            await this.yearFolderHandler(name);
-          } else {
-            await this.supportingDocumentHandler(name);
-          }
-        },
-        checkFilename() {
-          // this seems to be unused
-          console.trace('CHECK FILENAME', arguments);
-        },
-      },
-      savedMenuItems: null,
-      installMenuNew(menu) {
-        const fileList = menu.fileList;
-        const dirInfo = fileList.dirInfo;
-        const projectName = getProjectNameFromProjectBalancesFolders(dirInfo);
-        const projectYear = getProjectYearFromProjectName(projectName);
-
-        const isTopFolder = projectName && isProjectBalanceSupportingDocumentsTopFolder(dirInfo, projectName);
-        const isDocumentsFolder = projectName && isProjectBalanceSupportingDocumentsFolder(dirInfo, projectName, projectYear);
-
-        if (!isTopFolder && !isDocumentsFolder) {
-          if (this.savedMenuItems) {
-            // the WOPI requests sent to the richdocuments do not
-            // contain enough authentication to access our data-base.
-            const menuItems = dirInfo.mountType === 'cafevdb-database'
-              ? this.savedMenuItems.filter((item) => /* !item.id.match('richdocuments') && */ (!projectYear || item.id !== 'folder'))
-              : this.savedMenuItems;
-            menu._menuItems = menuItems;
-          }
-          return;
-        }
-
-        console.info('DIRINFO', dirInfo);
-
-        this.menuData.folderPrefix = projectName;
-        this.menuData.projectYear = projectYear;
-        this.menuData.isTopFolder = isTopFolder;
-
-        if (!projectYear && isTopFolder) {
-          this.menuData.templateName = 'YYYY';
-          this.menuData.displayName = t(appName, 'New Year Folder');
-        } else {
-          if (!projectYear) {
-            this.menuData.folderPrefix = projectName + '-' + dirInfo.name;
-          }
-          this.menuData.templateName = this.menuData.folderPrefix + '-XXX';
-          this.menuData.displayName = t(appName, 'New Supporting Document');
-        }
-        this.menuData.actionHandler = this.menuData.actionHandler.bind(this.menuData);
-        this.menuData.fileList = menu.fileList;
-
-        const menuItems = this.savedMenuItems
-          ? this.savedMenuItems.filter((item) => /* !item.id.match('richdocuments') && */ item.id !== 'folder')
-          : [];
-        menu._menuItems = menuItems;
-
-        menu.addMenuEntry(this.menuData);
-      },
-      attach(menu) {
-        if (!this.savedMenuItems) {
-
-          this.savedMenuItems = menu._menuItems;
-
-          const menuRender = menu.render;
-          menu.render = function() {
-            menuRender.apply(this);
-            if (isProjectBalanceSupportingDocumentsFolder(this.fileList.dirInfo)) {
-              // this.$el.find('ul li:first').remove();
-            }
-          }.bind(menu);
-
-          menu.fileList.$el.on('afterChangeDirectory', (params) => {
-            this.installMenuNew(menu);
-          });
-        }
-
-        this.installMenuNew(menu);
-      },
-    });
+  const newFileMenuEntries = getNewFileMenuEntries();
+  console.info('NEW FILE MENU ENTRIES', newFileMenuEntries);
+  for (const entry of newFileMenuEntries) {
+    if (entry !== supportingDocumentsEntry && entry.id !== 'rich-workspace-init') {
+      const enabledMethod = entry.enabled;
+      entry.enabled = (folder: Folder) => {
+        return !supportingDocumentsEntry.enabled(folder) && (enabledMethod ? enabledMethod(folder) : true);
+      };
+    }
   }
 });
