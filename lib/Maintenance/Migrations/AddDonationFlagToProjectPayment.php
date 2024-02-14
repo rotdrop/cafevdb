ea3358541e025bcf59e5c65830739403218122c8
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-02-14 21:32:23 +0100

    Add "is_donation" field to project payments and expose it in the frontend.

---
+++ lib/Maintenance/Migrations/AddDonationFlagToProjectPayment.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Orchestra member, musicion and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2024 Claus-Justus Heine
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Maintenance\Migrations;
+
+use OCP\ILogger;
+use OCP\IL10N;
+
+use OCA\CAFEVDB\Database\EntityManager;
+
+/**
+ * Remember the id of a mailing list.
+ */
+class AddDonationFlagToProjectPayment extends AbstractMigration
+{
+  protected static $sql = [
+    self::STRUCTURAL => [
+      "ALTER TABLE ProjectPayments ADD COLUMN IF NOT EXISTS is_donation TINYINT(1) DEFAULT '0' NOT NULL",
+    ],
+  ];
+
+  /** {@inheritdoc} */
+  public function description():string
+  {
+    return $this->l->t('Add a flag to the project payments indicating whether the payment was a donation.');
+  }
+}
