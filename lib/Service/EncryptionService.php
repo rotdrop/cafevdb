44b6b5cdd140b9ec55127e324229c58bf0f3b58f
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-02-15 18:09:37 +0100

    EncryptionService, denoise.

--- lib/Service/EncryptionService.php
+++ lib/Service/EncryptionService.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020, 2021, 2022, 2023 Claus-Justus Heine
+ * @copyright 2020, 2021, 2022, 2023, 2024 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -193,7 +193,7 @@ class EncryptionService
    */
   public function bind(string $userId, string $password):void
   {
-    $this->logDebug('BINDING TO ' . $userId . ' PW LEN ' . strlen($password));
+    // $this->logDebug('BINDING TO ' . $userId . ' PW LEN ' . strlen($password));
     $this->userId = $userId;
     $this->userPassword = $password;
     $this->initUserKeyPair();
