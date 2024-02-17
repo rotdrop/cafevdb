90b32b6630b26d5730b77ed4200e51f1c4a7263c
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-02-17 16:16:16 +0100

    PageRenderer\SepaBankAccount typo in comment.

--- lib/PageRenderer/SepaBankAccounts.php
+++ lib/PageRenderer/SepaBankAccounts.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2011-2022 Claus-Justus Heine
+ * @copyright 2011-2022, 2024 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -298,7 +298,7 @@ class SepaBankAccounts extends PMETableViewBase
     if ($projectMode) {
       $opts['options'] .= 'M';
 
-      // controls display an location of edit/misc buttons
+      // controls display of an location of edit/misc buttons
       $opts['navigation'] = self::PME_NAVIGATION_MULTI;
       $opts['misc']['css']['major'] = 'misc';
       $opts['misc']['css']['minor'] = 'debit-note tooltip-right';
