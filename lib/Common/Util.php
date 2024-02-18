604225d41e52d61afad352c4a3700a6bfb5bdbd9
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-02-18 21:55:44 +0100

    Common\Util::normalizeSpaces() -> replace soft hyphen by nothing.

--- lib/Common/Util.php
+++ lib/Common/Util.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2011-2016, 2020, 2021, 2022, 2023 Claus-Justus Heine
+ * @copyright 2011-2016, 2020, 2021, 2022, 2023, 2024 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -149,6 +149,7 @@ class Util
     if ($name === null) {
       return null;
     }
+    $name = str_replace("\xc2\xad", '', $name); // soft hyphen
     $name = str_replace("\xc2\xa0", "\x20", $name);
     $name = trim($name);
     $name = str_replace("\r\n", "\n", $name);
