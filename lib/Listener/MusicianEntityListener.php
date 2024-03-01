63873dfb8cfc25706afe9d9999aac9a5430d27d4
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-03-01 12:47:38 +0100

    MusicianEntityListener, cosmetics.

--- lib/Listener/MusicianEntityListener.php
+++ lib/Listener/MusicianEntityListener.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2022, 2023 Claus-Justus Heine
+ * @copyright 2022-2024 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -58,9 +58,6 @@ class MusicianEntityListener
 {
   use \OCA\CAFEVDB\Toolkit\Traits\LoggerTrait;
   use \OCA\CAFEVDB\Traits\EntityManagerTrait;
-
-  /** @var IL10N */
-  protected IL10N $l;
 
   /** @var IAppContainer */
   protected $appContainer;
