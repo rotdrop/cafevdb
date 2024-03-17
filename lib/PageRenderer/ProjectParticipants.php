1a04c27e6a703c48cfd4ea3353e5e0bd7825d9b5
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-03-17 18:32:17 +0100

    Render general remarks read-only in project-participant view.
    It happened too often that these were erroneously misused as project
    remarks.

--- lib/PageRenderer/ProjectParticipants.php
+++ lib/PageRenderer/ProjectParticipants.php
@@ -1231,7 +1231,7 @@ class ProjectParticipants extends PMETableViewBase
       $opts['fdd'], self::MUSICIANS_TABLE, 'remarks',
       [
         'tab'      => ['id' => 'musician'],
-        'name'     => $this->l->t('Remarks'),
+        'name'     => $this->l->t('General Remarks'),
         'maxlen'   => 65535,
         'css'      => ['postfix' => [ 'remarks', 'tooltip-top', 'squeeze-subsequent-lines', ], ],
         'textarea' => [
@@ -1245,6 +1245,11 @@ class ProjectParticipants extends PMETableViewBase
           'postfix' => '</div></div>',
         ],
         'escape' => false,
+        'display' => [
+          'attributes' => [ 'readonly' => true, 'disabled' => true, ],
+          'popup' => 'data',
+        ],
+        'tooltip' => $this->l->t('General, not project-specific remarks. The field cannot be changed from here.'),
       ]);
 
     $this->makeJoinTableField(
