293c91914f038a395196de3c75d27df699cc96d5
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-04-18 12:13:46 +0200

    FilesHooksListeners, fix fetching of wrong CSS asset.

--- lib/Listener/FilesHooksListener.php
+++ lib/Listener/FilesHooksListener.php
@@ -68,11 +68,11 @@ class FilesHooksListener implements IEventListener
   const ASSET_BASENAME = [
     LoadAdditionalScriptsEvent::class => [
       Constants::JS => 'files-hooks',
-      Constants::CSS => null,
+      Constants::CSS => 'files-hooks',
     ],
     LoadSidebar::class => [
       Constants::JS => 'files-sidebar-hooks',
-      Constants::CSS => 'files-sidebar-hooks',
+      Constants::CSS => null,
     ],
   ];
 
