1f42fe33144c6d942783dc10fe113b175950b14e
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-05-31 10:12:56 +0200

    EmailForm\Composer, fix sepaBulkdTransaction -> bulkTransaction member name.

--- lib/EmailForm/Composer.php
+++ lib/EmailForm/Composer.php
@@ -3198,8 +3198,8 @@ Störung.';
     }
 
     if (!empty($this->bulkTransaction)) {
-      $sentEmail->setSepaBulkTransaction($this->sepaBulkTransaction);
-      $this->sepaBulkTransaction->addPreNotificationEmail($sentEmail);
+      $sentEmail->setSepaBulkTransaction($this->bulkTransaction);
+      $this->bulkTransaction->addPreNotificationEmail($sentEmail);
     }
 
     return $sentEmail;
