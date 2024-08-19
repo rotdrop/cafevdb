8f348734edf4cec5cc527f2638fa696d899e7a83
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-08-19 17:21:44 +0200

    Adapt babel config to NC30.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
     '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-class-properties',
+    '@babel/plugin-transform-private-methods',
   ],
   presets: [
     // https://babeljs.io/docs/en/babel-preset-typescript
