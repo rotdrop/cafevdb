ac76009e5ce046c8511cacd2faeaad9632a1f76d
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-01-15 10:37:03 +0100

    Vue/stores, trivial fixes in cloud-user-groups

--- src/stores/cloud-users-groups.js
+++ src/stores/cloud-users-groups.js
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2024 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2024, 2025 Claus-Justus Heine <himself@claus-justus-heine.de>
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -39,11 +39,14 @@ export const useCloudUsersGroupsStore = defineStore(storeId, {
     };
   },
   actions: {
+    debug(...args) {
+      console.debug(storeId, ...args);
+    },
     info(...args) {
       console.info(storeId, ...args);
     },
     error(...args) {
-      console.info(storeId, ...args);
+      console.error(storeId, ...args);
     },
     trace(...args) {
       console.trace(storeId, ...args);
