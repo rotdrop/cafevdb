63367bf677d957bbc4756ec337529ce591457196
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-01-17 15:11:44 +0100

    AdminSettings.vue, type intput -> input

--- src/components/AdminSettings.vue
+++ src/components/AdminSettings.vue
@@ -4,7 +4,7 @@
  - CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  -
  - @author Claus-Justus Heine
- - @copyright 2011-2016, 2020, 2021, 2022, 2023, 2024 Claus-Justus Heine <himself@claus-justus-heine.de>
+ - @copyright 2011-2016, 2020, 2021, 2022, 2023, 2024, 2025 Claus-Justus Heine <himself@claus-justus-heine.de>
  - @license AGPL-3.0-or-later
  -
  - This program is free software: you can redistribute it and/or modify
@@ -32,7 +32,7 @@
       </div>
       <div>
         <SelectWithSubmitButton v-model="settings.userAndGroupBackend"
-                                intput-id="user-and-group-backend-select"
+                                input-id="user-and-group-backend-select"
                                 :input-label="t(appName, 'User and group backend')"
                                 :hint="hints['settings:admin:user-and-group-backend']"
                                 :required="true"
