66c0d093d8a7b5d5256c11e34069d21f09f024dc
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-01-18 12:40:32 +0100

    Vue mixins/globalstate.js, don't import globals.js, but globalstate.js directly in order to reduce side effects.

--- src/mixins/global-state.js
+++ src/mixins/global-state.js
@@ -21,7 +21,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import globalState from '../app/globalstate.js';
+import { globalState } from '../app/pme-state.js';
 
 export default {
   data() {
