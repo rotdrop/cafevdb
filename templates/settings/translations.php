0aa4b16ebe09b085aae36da120ba68769a28dd43
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-01-18 12:40:01 +0100

    Legacy settings templates: fix initialization of translations.

--- templates/settings/translations.php
+++ templates/settings/translations.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020, 2023 Claus-Justus Heine
+ * @copyright 2020, 2023, 2025 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -52,7 +52,7 @@ foreach ($translations as $keyId => $data) {
         class="translation-phrases"
         name="translation-phrases"
         title="<?php $toolTips['translation-phrases']; ?>" >
-      <option data-translations="dummy"></option>
+      <option data-translations='{}'></option>
       <?php echo Navigation::selectOptions($phraseOptions); ?>
     </select>
     <select
