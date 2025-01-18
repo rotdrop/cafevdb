c7c82ad9b26b2ebebd2453549d4d01e7c61c3075
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-01-18 13:48:47 +0100

    TS: event-bus.ts, cleanup.

--- src/event-bus.ts
+++ src/event-bus.ts
@@ -23,24 +23,27 @@
 
 import { appName } from './config.js';
 
-// app events
-export const APP_SETTINGS_POPUP = appName + ':app-settings-popup';
-export const TOGGLE_TOOLTIPS = appName + ':toggle-tooltips';
-export const PROJECT_POPUP = appName + ':project-popup';
-export const PROJECT_INSTRUMENTATION_NUMBERS_POPUP = appName + ':project-instrumentation-numbers-popup';
-export const PROJECT_PARTICIPANT_FIELDS_POPUP = appName + ':project-participant-fields-popup';
-export const PROJECT_EVENTS_POPUP = appName + ':project-events-popup';
-export const PUSH_BUSY_STATE = appName + ':push-busy-state';
-export const POP_BUSY_STATE = appName + ':pop-busy-state';
-export const GLOBAL_STATE = appName + ':global-state';
-export const PME_STATE = appName + ':pme-state';
+export const appEventName = (tag: string) => appName + ':' + tag;
 
-export const SET_FINANCE_MODE = appName + ':finance-mode';
-export const SET_EXPERT_MODE = appName + ':expert-mode';
-export const SET_SHOW_DISABLED = appName + ':show-disabled';
-export const SET_DEBUG_MODES = appName + ':debug-modes';
-export const SET_TOOLTIPS_MODE = appName + ':tooltips-mode';
-export const SET_PAGE_ROWS = appName + ':page-rows';
+// app events
+export const APP_SETTINGS_POPUP = appEventName(':app-settings-popup');
+export const TOGGLE_TOOLTIPS = appEventName(':toggle-tooltips');
+export const PROJECT_POPUP = appEventName(':project-popup');
+export const PROJECT_INSTRUMENTATION_NUMBERS_POPUP = appEventName(':project-instrumentation-numbers-popup');
+export const PROJECT_PARTICIPANT_FIELDS_POPUP = appEventName(':project-participant-fields-popup');
+export const PROJECT_EVENTS_POPUP = appEventName(':project-events-popup');
+export const PUSH_BUSY_STATE = appEventName(':push-busy-state');
+export const POP_BUSY_STATE = appEventName(':pop-busy-state');
+export const GLOBAL_STATE = appEventName(':global-state');
+export const PME_STATE = appEventName(':pme-state');
+
+export const SET_FINANCE_MODE = appEventName(':finance-mode');
+export const SET_EXPERT_MODE = appEventName(':expert-mode');
+export const SET_SHOW_DISABLED = appEventName(':show-disabled');
+export const SET_DEBUG_MODES = appEventName(':debug-modes');
+export const SET_TOOLTIPS_MODE = appEventName(':tooltips-mode');
+export const SET_PAGE_ROWS = appEventName(':page-rows');
+export const SET_DESELECT_INVISIBLE = appEventName(':page-rows');
 
 // global events
 export const TOGGLE_NAVIGATION = 'toggle-navigation';
