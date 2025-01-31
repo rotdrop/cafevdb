007fb48b3d970a3960c92065ab7c485e1c177d99
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-01-31 19:33:05 +0100

    legacy templates, fix computing the project name.

--- templates/project-participants.php
+++ templates/project-participants.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2011-2016, 2020, 2021, 2023, 2024 Claus-Justus Heine
+ * @copyright 2011-2016, 2020, 2021, 2023, 2024, 2025 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -30,7 +30,7 @@ use OCA\CAFEVDB\Service\ConfigService;
 
 $cssPfx = $renderer->cssPrefix();
 $cssClass = 'instrumentation'; // @todo generalize
-$project = $renderer->getProjectName();
+$projectName = $renderer->getProjectName();
 $projectId = $renderer->getProjectId();
 
 //$missing = Projects::missingInstrumentationTable($projectId);
