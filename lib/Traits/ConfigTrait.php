981720064cbd8208c6590485f1bb37a8e4383381
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-02-01 17:31:42 +0100

    ConfigTrait, cleanup logger() getter.

--- lib/Traits/ConfigTrait.php
+++ lib/Traits/ConfigTrait.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2011-2016, 2020, 2021, 2022, 2023, 2024 Claus-Justus Heine
+ * @copyright 2011-2016, 2020-2025 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -105,7 +105,7 @@ trait ConfigTrait
   /** @return LoggerInterface */
   public function logger():LoggerInterface
   {
-    return $this->configService ? $this->configService->logger() : $this->logger;
+    return $this->configService->logger();
   }
 
   /** @return IAppContainer */
