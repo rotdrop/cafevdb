a15fa2988afb1df91418ab120581459f1db13b41
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-02-05 14:01:10 +0100

    WIP BeforeTemplateRenderedListener: custom logo.
    This should probably be changed to just provide an end-point for a
    custom logo.

--- lib/Listener/BeforeTemplateRenderedListener.php
+++ lib/Listener/BeforeTemplateRenderedListener.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2011-2016, 2020, 2021, 2022, 2024 Claus-Justus Heine
+ * @copyright 2011-2016, 2020-2025 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -25,6 +25,9 @@
 
 namespace OCA\CAFEVDB\Listener;
 
+use Throwable;
+use Psr\Log\LoggerInterface;
+
 use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent as HandledEvent;
 use OCP\AppFramework\IAppContainer;
 use OCP\EventDispatcher\Event;
@@ -34,6 +37,9 @@ use OCP\IURLGenerator;
 use OCP\IUserSession;
 
 use OCA\CAFEVDB\Service\AuthorizationService;
+use OCA\CAFEVDB\Documents\TemplateService;
+use OCA\CAFEVDB\Service\ImagesService;
+use OCA\CAFEVDB\Service\ConfigService;
 
 /**
  * Install the navigation entries if the current user is logged and is
@@ -41,6 +47,8 @@ use OCA\CAFEVDB\Service\AuthorizationService;
  */
 class BeforeTemplateRenderedListener implements IEventListener
 {
+  use \OCA\CAFEVDB\Toolkit\Traits\LoggerTrait;
+
   const EVENT = HandledEvent::class;
 
   // phpcs:disable Squiz.Commenting.FunctionComment.Missing
@@ -56,6 +64,8 @@ class BeforeTemplateRenderedListener implements IEventListener
     if (!($event instanceof HandledEvent)) {
       return;
     }
+
+    $this->logger = $this->appContainer->get(LoggerInterface::class);
 
     /** @var IUserSession $userSession */
     $userSession = $this->appContainer->get(IUserSession::class);
@@ -80,13 +90,34 @@ class BeforeTemplateRenderedListener implements IEventListener
     /** @var string $appName */
     $appName = $this->appContainer->get('appName');
 
-    $navigationManager->add([
+    $appIcon = $urlGenerator->imagePath($appName, 'app.svg');
+
+    try {
+      /** @var TemplateService $templateService */
+      $templateService = $this->appContainer->get(TemplateService::class);
+      $customLogo = $templateService->getDocumentTemplate(ConfigService::DOCUMENT_TEMPLATE_LOGO);
+
+      if ($customLogo) {
+        /** @var ImagesService $imagesService */
+        $imagesService = $this->appContainer->get(ImagesService::class);
+        $customLogo = $imagesService->dataUriFromFile($customLogo, ImagesService::SVG_OPTIMIZE|ImagesService::CONVERT_TO_SVG);
+        $this->logInfo('LOGO ??? ' . $customLogo);
+        $customLogo = null;
+      }
+    } catch (Throwable $t) {
+      $this->logException($t);
+    }
+
+    $navEntry = [
       'id' => $appName,
       'name' => 'CAFeVDB',
       'href' => $urlGenerator->linkToRoute(implode('.', [ $appName, 'page', 'index' ])),
-      'icon' => $urlGenerator->imagePath($appName, 'app.svg'),
+      'icon' => $customLogo ?? $appIcon,
       'type' => 'link',
       'order' => 1,
-    ]);
+    ];
+    // $this->logInfo('NAV ' . print_r($navEntry, true));
+
+    $navigationManager->add($navEntry);
   }
 }
