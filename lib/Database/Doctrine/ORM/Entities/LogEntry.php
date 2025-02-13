c50d430545a262c54ab6c7a5cb9c65322c89bd12
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-02-13 02:17:08 +0100

    LogEntry: object_id must be read-write to support auto increment.
    It has been R/W, I suspect that the AttributeOverride semantics have
    changed ...

--- lib/Database/Doctrine/ORM/Entities/LogEntry.php
+++ lib/Database/Doctrine/ORM/Entities/LogEntry.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020, 2022-2024 Claus-Justus Heine
+ * @copyright 2020, 2022-2025 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -52,8 +52,9 @@ use OCA\CAFEVDB\Wrapped\Gedmo\Loggable;
     name: 'objectId',
     column: new ORM\Column(
      name: 'object_id',
-      type: 'string',
-      length: 573
+     type: 'string',
+     length: 573,
+     nullable: true,
     )
   ),
 ])]
