219dffdc90c93a8a29f07d8275c4085b1975c8a8
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-02-26 09:49:41 +0100

    DB ChangeLog entity, comment change

--- lib/Database/Doctrine/ORM/Entities/ChangeLog.php
+++ lib/Database/Doctrine/ORM/Entities/ChangeLog.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020-2022, 2024 Claus-Justus Heine
+ * @copyright 2020-2022, 2024, 2025 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -32,7 +32,7 @@ use OCA\CAFEVDB\Database\Doctrine\ORM as CAFEVDB;
 use OCA\CAFEVDB\Wrapped\Doctrine\ORM\Mapping as ORM;
 
 /**
- * Changelog
+ * Legacy Changelog.
  */
 #[ORM\Table(name: 'ChangeLog')]
 #[ORM\Entity]
