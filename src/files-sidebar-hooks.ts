50e412fcc5dfe1204ee945349f82ded0fdd7aec6
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-03-12 13:43:49 +0100

    TS; add missing ;

--- src/files-sidebar-hooks.ts
+++ src/files-sidebar-hooks.ts
@@ -27,7 +27,7 @@ import { generateFilePath } from '@nextcloud/router';
 import { getRequestToken } from '@nextcloud/auth';
 import { translate as t } from '@nextcloud/l10n';
 import logoSvg from '../img/cafevdb.svg?raw';
-import type { LegacyFileInfo } from '@nextcloud/files'
+import type { LegacyFileInfo } from '@nextcloud/files';
 
 // eslint-disable-next-line camelcase
 __webpack_nonce__ = btoa(getRequestToken() || '');
