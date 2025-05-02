632b36cdf599af7902d561565828c834808853d5
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-05-02 09:45:06 +0200

    history.ts, logging

--- src/stores/history.ts
+++ src/stores/history.ts
@@ -469,10 +469,16 @@ export default defineStore(storeId, () => {
     let promise: Promise<void>;
     let count = 0;
     do {
-      logger.debug(3, 'ATTEMPT TO AQUIRE MUTATION LOCK', ++count);
+      logger.debug(3, 'ATTEMPT TO AQUIRE MUTATION LOCK', {
+        count: ++count,
+        mutationLock,
+      });
       await (promise = mutationLock.promise);
     } while (promise !== mutationLock.promise);
-    logger.debug(3, 'AQUIRED MUTATION LOCK');
+    logger.debug(3, 'AQUIRED MUTATION LOCK', {
+      count,
+      mutationLock,
+    });
     return mutationLock = Promise.withResolvers<void>();
   };
 
