fd893336e71a81605d973e47aba2e211eace1d93
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-06-05 22:16:03 +0200

    dev-scripts playground trials

--- dev-scripts/playground/php/enums.php
+++ dev-scripts/playground/php/enums.php
@@ -1,17 +1,25 @@
 <?php
 
-
-enum Foo: string
-{
-  case FOO = 'bar';
-  case BAR = 'foo';
-}
-
 enum Blah
 {
   case FOO;
   case BAR;
 }
 
+enum Foo: string
+{
+  case FOO = 'bar';
+  case BAR = 'foo';
+
+  public static function caseValues():array
+  {
+    return array_map(fn(self $case) => $case->value, self::cases());
+  }
+}
+
 print_r(FOO::cases());
+print_r(FOO::caseValues());
 print_r(BLAH::cases());
+print_r((array)Foo::FOO);
+
+print json_encode(Foo::cases()) . PHP_EOL;
