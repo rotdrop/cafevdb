ef99f655308025c94c5ab7b33d7d9e47ce74442f
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-10-05 18:04:22 +0200

    PersonalSettingsController, fix missing EntityManager instance.

--- lib/Controller/PersonalSettingsController.php
+++ lib/Controller/PersonalSettingsController.php
@@ -1995,6 +1995,7 @@ class PersonalSettingsController extends Controller
         $format = $this->request->getParam('format');
 
         $project = $this->projectService->findById($this->getClubMembersProjectId());
+        $this->entityManager = $this->di(EntityManager::class);
         $flatProject = $this->flattenProject($project);
 
         /** @var InstrumentationService $instrumentationService */
