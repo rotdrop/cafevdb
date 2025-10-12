39e496ff93ce6abedd76d22d87b3e9a18f09d36c
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-10-12 15:41:34 +0200

    ORM\Traits\TranslatableTrait, remove the last use of anontation in favour of attributes.

--- lib/Database/Doctrine/ORM/Traits/TranslatableTrait.php
+++ lib/Database/Doctrine/ORM/Traits/TranslatableTrait.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020, 2021, 2022, 2023 Claus-Justus Heine
+ * @copyright 2020-2023, 2025 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -36,9 +36,8 @@ trait TranslatableTrait
    * Gedmo\Translatable (has to?) clean the changeset of the actual
    * entity. Unfortunately this also spoils the update listeners. We work
    * around by remembering any old value.
-   *
-   * @Gedmo\TranslationChangeSet
    */
+  #[Gedmo\TranslationChangeSet]
   private $translationChangeSet;
 
   /**
