150c081e604c9a9ec42967016e99af82d71a0153
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-10-22 10:00:17 +0200

    UndoableRunQueueException::getRunQueue(): return the correct member variable

--- lib/Exceptions/UndoableRunQueueException.php
+++ lib/Exceptions/UndoableRunQueueException.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020, 2021, 2022, 2024 Claus-Justus Heine
+ * @copyright 2020-2022, 2024, 2025 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -49,7 +49,7 @@ class UndoableRunQueueException extends RuntimeException
   /** @return UndoableRunQueue */
   public function getRunQueue():UndoableRunQueue
   {
-    return $this->runQueue;
+    return $this->queue;
   }
 
   /**
