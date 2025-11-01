bee8264eb7b77f7f5b49efff32bf98e7fd835335
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-11-01 23:57:50 +0100

    Move more dev-tools to vendor-bin.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-require_once './vendor/autoload.php';
+require_once './vendor-bin/cs-fixer/vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
 
 $config = new Config();
 $config
-	->getFinder()
-	->ignoreVCSIgnored(true)
-	->notPath('vendor')
-	->in(__DIR__);
+  ->getFinder()
+  ->ignoreVCSIgnored(true)
+  ->notPath('vendor')
+  ->in(__DIR__);
 return $config;
