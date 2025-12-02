a0fa4fdb5cacf4a9f9f2d237a62e69ea1b8d919c
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-12-02 22:33:51 +0100

    PHPUnit, EventsServiceTest, split out setup to a trait for reusal.

---
+++ tests/phpunit/Unit/Service/SetupEventsServiceTrait.php
@@ -0,0 +1,243 @@
+<?php
+/**
+ * Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2025 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Tests\Unit\Service;
+
+use UnexpectedValueException;
+
+use PHPUnit\Framework\Attributes;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+use OCP\AppFramework\IAppContainer;
+use OCP\Calendar\ICalendar;
+use OCP\Calendar\IManager as CalendarManager;
+use OCP\IDateTimeFormatter;
+use OCP\IL10N;
+
+use OCA\DAV\CalDAV\CalDavBackend;
+
+use OCA\CAFEVDB\Database\Doctrine\DBAL\Types\EnumVCalendarType as VCalendarType;
+use OCA\CAFEVDB\Database\Doctrine\ORM\Entities;
+use OCA\CAFEVDB\Database\EntityManager;
+use OCA\CAFEVDB\Legacy\Calendar\OC_Calendar_Object;
+use OCA\CAFEVDB\Service\CalDavService;
+use OCA\CAFEVDB\Service\ConfigService;
+use OCA\CAFEVDB\Service\EventsService;
+use OCA\CAFEVDB\Service\ProjectService;
+use OCA\CAFEVDB\Service\VCalendarService;
+use OCA\CAFEVDB\Settings\ConfigConstants;
+use OCA\CAFEVDB\Tests\MockProvider;
+use OCA\CAFEVDB\Tests\Unit\Database\Doctrine\ORM\Entities\EntityGeneratorTrait;
+use OCA\CAFEVDB\Wrapped\Doctrine\ORM\EntityRepository;
+
+/**
+ * Mock around s.t. the EventsService class can be instantiated and used.
+ *
+ * @todo Currently this covers only the generation of the ProjectEvent
+ * matrix. It should be extended to also cover the various events emitted by
+ * the dav app.
+ */
+trait SetupEventsServiceTrait
+{
+  use EntityGeneratorTrait {
+    EntityGeneratorTrait::setup as entitySetup;
+  }
+
+  private EventsService $eventsService;
+
+  /**
+   * @var array<string, array>
+   *
+   * Some faked DB rows for generating calendar objects.
+   */
+  private array $calendarObjects = [];
+
+  /**
+   * Flat array of fake database rows of calendar objects.
+   */
+  private array $calendarData = [];
+
+  private $defaultCalendars = [];
+
+  private IAppContainer $appContainer;
+
+  /**
+   * {@inheritdoc}
+   *
+   * @return void
+   */
+  public function setup(): void
+  {
+    $this->entitySetup(persist: false);
+
+    /** @var MockProvider $mockProvider */
+    $mockProvider = \OCP\Server::get(MockProvider::class);
+
+    $this->appContainer = $mockProvider->getAppContainer();
+
+    $l = $mockProvider->getL10N();
+
+    $cloudConfig = $mockProvider->getCloudConfig();
+
+    /** @var ConfigService $configService */
+    $configService = $mockProvider->getConfigService();
+    $calendarId = 1;
+    foreach (array_keys(ConfigConstants::CALENDARS) as $uri) {
+      $configService->setConfigValue($uri . ConfigConstants::CALENDAR_KEY_POSTFIX, $l->t('uri'));
+      $configService->setConfigValue($uri . ConfigConstants::CALENDAR_ID_KEY_POSTFIX, $calendarId);
+      $this->defaultCalendars[$uri] = $calendarId;
+      ++$calendarId;
+    }
+
+    $projectEventId = 1;
+    $this->calendarData = array_values(CalendarObjects::getData($this->project->getName(), $this->defaultCalendars, $l));
+    foreach ($this->calendarData as $index => $row) {
+      $this->calendarObjects[$row['calendarid']][$row['uri']] = $index;
+    }
+    self::addProjectEvents($this->project, $this->defaultCalendars);
+
+    /** @var CalDavBackend $calDavBackend */
+    $calDavBackend = $this->getMockBuilder(CalDavBackend::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $calDavBackend->method('getCalendarObject')
+      ->willReturnCallback(
+        function(
+          $calendarId,
+          $objectUri,
+          int $calendarType = CalDavBackend::CALENDAR_TYPE_CALENDAR,
+        ): ?array {
+          $rowIndex = $this->calendarObjects[$calendarId][$objectUri] ?? null;
+          if ($rowIndex !== null) {
+            return $this->rowToCalendarObject($this->calendarData[$rowIndex]);
+          }
+          echo 'NOT FOUND ' . $calendarId . ' ' . $objectUri . PHP_EOL;
+          print_r($this->calendarObjects);
+          return null;
+        },
+      );
+    $calDavBackend->method('getCalendarById')
+      ->willReturnCallback(
+        function(int $calendarId): ?array {
+          if ($calendarId < 1 || $calendareId > count($this->defaultCalendars)) {
+            return null;
+          }
+          $calendars = array_flip($this->defaultCalendars);
+          return [
+            'principaluri' => 'principals/users/calendar.owner',
+            'uri' => $calendars[$calendarId],
+          ];
+        },
+      );
+
+    /** @var CalendarManager $calendarManager */
+    $calendarManager = $this->getMockBuilder(CalendarManager::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $calendarManager->method('getCalendars')
+      ->willReturnCallback(
+        function() use($l) : array {
+          $calendars = [];
+          foreach ($this->defaultCalendars as $uri => $id) {
+            $calendar = $this->getMockBuilder(ICalendar::class)
+              ->disableOriginalConstructor()
+              ->getMock();
+            $calendar->method('getKey')->willReturn((string)$id);
+            $calendar->method('getdisplayName')->willReturn($l->t(ucfirst($uri) . ' (calendar.owner)'));
+            $calendars[] = $calendar;
+          }
+          return $calendars;
+        },
+      );
+
+    $calDavService = new CalDavService(
+      configService: $configService,
+      calendarManager: $calendarManager,
+      calDavBackend: $calDavBackend,
+    );
+
+    /** @var EntityManager $entityManager */
+    $entityManager = $this->getMockBuilder(EntityManager::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $entityManager->method('getRepository')->willReturnCallback(
+      function(string $className) {
+        switch ($className) {
+          case Entities\ProjectEvent::class:
+            $repository = $this->getMockBuilder(EntityRepository::class)
+              ->disableOriginalConstructor()
+              ->getMock();
+            $repository->method('findBy')->willReturnCallback(
+              function(array $criteria, ?array $orderBy = null, ?int $limit = null, ?int $offset = null): array {
+                if (!isset($criteria['project']) || ($criteria['type'] ?? VCalendarType::VEVENT) != VCalendarType::VEVENT) {
+                  throw new UnexpectedValueException('Can only fake search for VEVENT given a project or project-id.');
+                }
+                $projectOrId = $criteria['project'];
+                if ($projectOrId !== $this->project && $projectOrId != $this->project->getId()) {
+                  return [];
+                }
+                return $this->project->getCalendarEvents()->toArray();
+              },
+            );
+            return $repository;
+        }
+        return null;
+      },
+    );
+
+    /** @var ProjectService $projectService */
+    $projectService = $this->getMockBuilder(ProjectService::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $vCalendarService = new VCalendarService(
+      configService: $mockProvider->getConfigService(),
+      legacyCalendarObject: new OC_Calendar_Object(
+        userSession: $mockProvider->getUserSession(),
+        l: $mockProvider->getL10N(),
+      ),
+    );
+
+    $this->eventsService = new EventsService(
+      userSession: $mockProvider->getUserSession(),
+      configService: $mockProvider->getConfigService(),
+      entityManager: $entityManager,
+      projectService: $projectService,
+      calDavService: $calDavService,
+      vCalendarService: $vCalendarService,
+      dateTimeFormatter: \OCP\Server::get(IDateTimeFormatter::class),
+    );
+  }
+
+  /**
+   * Just one silly do-nothing test to avoid "missing tests" complaints.
+   *
+   * @return void
+   */
+  public function testSetup(): void
+  {
+    $this->expectNotToPerformAssertions();
+  }
+}
