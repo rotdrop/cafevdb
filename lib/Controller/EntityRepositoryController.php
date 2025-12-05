0f86202cfd293ca76f178141d4b2ea2b5952838d
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-12-05 10:42:05 +0100

    New yet upcoming and empty EntityRepositoryController.
    Will use the EntitySerializer in order to transfer database entities to
    the frontend in a unified way.

---
+++ lib/Controller/EntityRepositoryController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Orchestra member, musician and project management application.
+ *
+ * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
+ *
+ * @author Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2025 Claus-Justus Heine
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\CAFEVDB\Controller;
+
+use OCP\AppFramework\Http\Attribute as CoreAttributes;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\AppFramework\OCS;
+use OCP\AppFramework\OCSController;
+use OCP\IRequest;
+
+use OCA\CAFEVDB\Attributes;
+
+/** Export entities to the frontend. */
+class EntityRepositoryController extends OCSController
+{
+  // phpcs:disable Squiz.Commenting.FunctionComment.Missing
+  public function __construct(
+    ?string $appName,
+    IRequest $request,
+   ) {
+    parent::__construct($appName, $request);
+  }
+  // phpcs:enable
+}
