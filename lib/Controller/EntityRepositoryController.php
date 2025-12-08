f69e7bf6820e4de2f347ed351f9f907439458163
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-12-08 15:11:07 +0100

    new EntityRepositoryController for fetching database entities.

--- lib/Controller/EntityRepositoryController.php
+++ lib/Controller/EntityRepositoryController.php
@@ -25,22 +25,69 @@
 namespace OCA\CAFEVDB\Controller;
 
 use OCP\AppFramework\Http\Attribute as CoreAttributes;
-use OCP\AppFramework\Http\DataResponse;
+use OCP\AppFramework\Http\JSONResponse;
 use OCP\AppFramework\OCS;
 use OCP\AppFramework\OCSController;
 use OCP\IRequest;
+use Psr\Log\LoggerInterface;
 
 use OCA\CAFEVDB\Attributes;
+use OCA\CAFEVDB\Database\EntityManager;
+use OCA\CAFEVDB\Database\Doctrine\ORM\Util\EntitySerializer;
 
 /** Export entities to the frontend. */
 class EntityRepositoryController extends OCSController
 {
+  use \OCA\CAFEVDB\Toolkit\Traits\LoggerTrait;
+
   // phpcs:disable Squiz.Commenting.FunctionComment.Missing
   public function __construct(
     ?string $appName,
     IRequest $request,
-   ) {
+    private EntityManager $entityManager,
+    private EntitySerializer $entitySerializer,
+    protected LoggerInterface $logger,
+  ) {
     parent::__construct($appName, $request);
   }
   // phpcs:enable
+
+  /**
+   * Parameters are subemitted via query-string, except for the entity name.
+   *
+   * @param string $entityName
+   *
+   * @param string $findBy Base64 encode array of search criteria as
+   * understood by
+   * \OCA\CAFEVDB\Database\Doctrine\ORM\Repositories\EntityRepository::findBy).
+   *
+   * @param null|int $limit
+   *
+   * @param int $offset
+   *
+   * @param int $depth
+   *
+   * @return DataResponse
+   */
+  #[CoreAttributes\NoAdminRequired]
+  #[CoreAttributes\ApiRoute(
+    verb: 'GET',
+    url: '/v1/entities/{entityName}/{findBy}',
+    defaults: ['depths' => 2],
+  )]
+  public function getEntities(
+    string $entityName,
+    string $findBy,
+    ?int $limit = null,
+    int $offset = 0,
+    int $depth = 2,
+  ): JSONResponse {
+    $criteria = json_decode($findBy, associative: true);
+    $repository = $this->entityManager->getRepository($entityName);
+    $entities = $repository->findBy($criteria, limit: $limit, offset: $offset);
+    foreach ($entities as $entity) {
+      $this->entitySerializer->addEntity($entity, $depth);
+    }
+    return $this->entitySerializer->export();
+  }
 }
