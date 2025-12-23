2a3fc350b9b98152a93a373b8baef1b732845458
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-12-23 10:16:26 +0100

    FindLikeTrait, fix mispelled constant.

--- lib/Database/Doctrine/ORM/Traits/FindLikeTrait.php
+++ lib/Database/Doctrine/ORM/Traits/FindLikeTrait.php
@@ -346,7 +346,7 @@ trait FindLikeTrait
         && array_keys($criteria)[0] === self::OPTIONS_KEY
         && is_array($criteria[self::OPTIONS_KEY])) {
       $options = array_merge($options, $criteria[self::OPTIONS_KEY]);
-      unset($criteria[self::OPTIONS_KEYS]);
+      unset($criteria[self::OPTIONS_KEY]);
     }
 
     $orderBy = $orderBy?:[];
