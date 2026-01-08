43074323bcb5af305e75f448bc50901069a7f9d9
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-01-08 15:16:25 +0100

    DB ORM AutoIncrementTrait, gracefully return null before id is defined.

--- lib/Database/Doctrine/ORM/Traits/AutoIncrementTrait.php
+++ lib/Database/Doctrine/ORM/Traits/AutoIncrementTrait.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020-2022, 2024, 2025 Claus-Justus Heine
+ * @copyright 2020-2022, 2024-2026 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -60,8 +60,8 @@ trait AutoIncrementTrait
    *
    * @return null|int
    */
-  public function getId(): int
+  public function getId(): ?int
   {
-    return $this->id;
+    return $this->id ?? null;
   }
 }
