f7d2e77019b17a2b5c397169eff317eee4c3646e
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-01-08 09:09:48 +0100

    Handle some PHP deprecations which popped up during unit testing.

--- lib/Service/L10N/BiDirectionalL10N.php
+++ lib/Service/L10N/BiDirectionalL10N.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020-2022, 2024, 2025 Claus-Justus Heine
+ * @copyright 2020-2022, 2024-2026 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -164,7 +164,7 @@ class BiDirectionalL10N
     if ($file === false) {
       return null;
     }
-    $header = fgetcsv($file);
+    $header = fgetcsv($file, escape: '');
     if (empty($header)) {
       return null;
     }
@@ -174,7 +174,7 @@ class BiDirectionalL10N
 
     $targetLookup = [];
     $keyLookup = [];
-    for ($row = fgetcsv($file); !empty($row); $row = fgetcsv($file)) {
+    for ($row = fgetcsv($file, escape: ''); !empty($row); $row = fgetcsv($file, escape: '')) {
       if (empty($row[$targetColumn]) || empty($row[$keyColumn])) {
         continue;
       }
