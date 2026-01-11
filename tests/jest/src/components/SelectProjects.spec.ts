6cb8e216205ec8a2351fab6b8ed617524f5bf16a
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-01-11 22:58:10 +0100

    SelectProjects.spec.ts, adjust comment.

--- tests/jest/src/components/SelectProjects.spec.ts
+++ tests/jest/src/components/SelectProjects.spec.ts
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2025 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2025, 2026 Claus-Justus Heine <himself@claus-justus-heine.de>
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -107,7 +107,7 @@ localVue.directive('tooltip', Tooltip);
 // @ts-expect-error 2769
 localVue.use(createTestingPinia());
 
-describe('SettingsSelectUsers component', () => {
+describe('SettingsSelectProjects component', () => {
 
   const propsData = {
     // clear all options, only makes sense if multiple == true
