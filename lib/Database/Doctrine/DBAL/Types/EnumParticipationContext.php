a15f8400d50a3b528ba80fccc45a70f54ace42e9
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-01-11 13:19:58 +0100

    EnumParticipationContext/Status, remove the special translation tags.
    The TranslatableEnumTrait now includes the Enum class-name for all
    enums, so no need any more to modify the tag.

--- lib/Database/Doctrine/DBAL/Types/EnumParticipationContext.php
+++ lib/Database/Doctrine/DBAL/Types/EnumParticipationContext.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2011-2022, 2024, 2025 Claus-Justus Heine
+ * @copyright 2011-2022, 2024-2026 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -40,10 +40,4 @@ enum EnumParticipationContext: string
   case ASSOCIATES = 'associates';
   case PARTICIPANTS = 'participants';
   case UNRESTRICTED = 'unrestricted';
-
-  /** {@inheritdoc} */
-  public static function l10nTag(): string
-  {
-    return self::L10N_TAG . '_DISPLAY_CONTEXT: ';
-  }
 }
--- lib/Database/Doctrine/DBAL/Types/EnumParticipationStatus.php
+++ lib/Database/Doctrine/DBAL/Types/EnumParticipationStatus.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2011-2022, 2024, 2025 Claus-Justus Heine
+ * @copyright 2011-2022, 2024-2026 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -39,10 +39,4 @@ enum EnumParticipationStatus: string
   case REGULAR = 'regular';
   case SOLOIST = 'soloist';
   case TEMPORARY = 'temporary';
-
-  /** {@inheritdoc} */
-  public static function l10nTag(): string
-  {
-    return self::L10N_TAG . '_PARTICIPATION_STATUS: ';
-  }
 }
