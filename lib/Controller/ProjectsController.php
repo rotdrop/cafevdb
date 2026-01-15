4b992b4b23d6fbef3974c6de2d2ff82590c7d48e
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-01-16 00:46:59 +0100

    ProjectsController: fix yet another string vs enum issue.

--- lib/Controller/ProjectsController.php
+++ lib/Controller/ProjectsController.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2020, 2021, 2022, 2023, 2024, 2025 Claus-Justus Heine
+ * @copyright 2020, 2021, 2022, 2023, 2024, 2025, 2026 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -106,7 +106,7 @@ class ProjectsController extends Controller
         $projectId = $record['id']??null;
         $projectName = $projectValues['name'];
         $projectYear = $projectValues['year'];
-        $attachYear  = !empty($projectValues['type']) && $projectValues['type'] == ProjectType::TEMPORARY;
+        $attachYear  = !empty($projectValues['type']) && $projectValues['type'] == ProjectType::TEMPORARY->value;
 
         $infoMessage = "";
         switch ($control) {
