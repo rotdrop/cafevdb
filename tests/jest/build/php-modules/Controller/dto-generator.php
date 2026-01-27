c65ac514f5db2d16a09871e4ca2cb7dc5571d7ff
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-01-27 23:01:04 +0100

    Adjust jest test input after changing PHP DTOs

--- tests/jest/build/php-modules/Controller/dto-generator.php
+++ tests/jest/build/php-modules/Controller/dto-generator.php
@@ -6,7 +6,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2025 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2025, 2026 Claus-Justus Heine <himself@claus-justus-heine.de>
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -42,8 +42,8 @@ switch ($dtoName) {
   case array_pop(explode('\\', DTO\DownloadsShareResponse::class)):
     $dto = new DTO\DownloadsShareResponse(
       messages: ['MESSAGE'],
-      share: 'SHARE',
-      folder: 'FOLDER',
+      url: 'SHARE',
+      path: 'FOLDER',
       expires: DateTime::createFromFormat('Y-m-d h:i:s', '2025-11-04 01:02:03'),
     );
     break;
