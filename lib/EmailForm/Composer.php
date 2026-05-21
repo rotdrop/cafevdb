41459aaa38484656d1377cb21a6b42787395d611
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-05-21 16:55:41 +0200

    EmailForm\Composer, fix user agent for link validation.
    Reservix rejects "wrong" user agents with 403.

--- lib/EmailForm/Composer.php
+++ lib/EmailForm/Composer.php
@@ -3887,7 +3887,8 @@ Euer Camerata Vorstand (${GLOBAL::ORGANIZER})
     }
     $options = [
       ClientRequestOptions::HEADERS => [
-        'User-Agent' => 'Mozilla/5.0  (X11; Linux x86_64; rv:150.0)',//  Gecko/20100101 Firefox/150.0', // 'Orgacloud/1.0',
+        // 'User-Agent' => 'Orgacloud/1.0',
+        'User-Agent' => 'Mozilla/5.0  (X11; Linux x86_64; rv:150.0)',//  Gecko/20100101 Firefox/150.0',
         'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
       ],
       ClientRequestOptions::HTTP_ERRORS => false,
