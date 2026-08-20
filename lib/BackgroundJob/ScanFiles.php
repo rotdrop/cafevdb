869ddd2052150e27a485ce1c4c4bb6e965fc16c5
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-08-20 20:58:19 +0200

    BackoundJob\ScanFiles, actually inherit the class from the files-app.

--- lib/BackgroundJob/ScanFiles.php
+++ lib/BackgroundJob/ScanFiles.php
@@ -5,7 +5,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2022, 2023, 2024 Claus-Justus Heine
+ * @copyright 2022, 2023, 2024, 2026 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -24,10 +24,8 @@
 
 namespace OCA\CAFEVDB\BackgroundJob;
 
-use OC\Files\Utils\Scanner;
+use OCA\Files\BackgroundJob\ScanFiles as FilesAppScanFiles;
 use OCP\AppFramework\Utility\ITimeFactory;
-use OCP\BackgroundJob\TimedJob;
-use OCP\DB\QueryBuilder\IQueryBuilder;
 use OCP\EventDispatcher\IEventDispatcher;
 use OCP\IConfig;
 use OCP\IDBConnection;
@@ -42,83 +40,28 @@ use Psr\Log\LoggerInterface;
  * which is triggered by a JS timer via Ajax from the front-end in order to
  * get authenticated background jobs.
  */
-class ScanFiles extends TimedJob
+class ScanFiles extends FilesAppScanFiles
 {
-  /** Amount of users that should get scanned per execution */
-  public const USERS_PER_SESSION = 500;
-
-  /**
-   * @param IConfig $config
-   * @param IEventDispatcher $dispatcher
-   * @param LoggerInterface $logger
-   * @param IDBConnection $connection
-   */
+  /** {@inheritdoc} */
   public function __construct(
-    private IConfig $config,
-    private IEventDispatcher $dispatcher,
-    private LoggerInterface $logger,
-    private IDBConnection $connection,
+    IConfig $config,
+    IEventDispatcher $dispatcher,
+    LoggerInterface $logger,
+    IDBConnection $connection,
     ITimeFactory $time,
   ) {
-    parent::__construct($time);
-    // Run once per 10 minutes
-    $this->setInterval(60 * 10);
-  }
-
-  /**
-   * @param string $user
-   *
-   * @return void
-   */
-  protected function runScanner(string $user)
-  {
-    try {
-      $scanner = new Scanner(
-        $user,
-        null,
-        $this->dispatcher,
-        $this->logger
-      );
-      $scanner->backgroundScan('');
-    } catch (\Exception $e) {
-      $this->logger->error($e->getMessage(), ['exception' => $e, 'app' => 'files']);
-    }
-    \OC_Util::tearDownFS();
-  }
-
-  /**
-   * Find a storage which have unindexed files and return a user with access to the storage
-   *
-   * @return string|false
-   */
-  private function getUserToScan()
-  {
-    $query = $this->connection->getQueryBuilder();
-    $query->select('user_id')
-      ->from('filecache', 'f')
-      ->innerJoin('f', 'mounts', 'm', $query->expr()->eq('storage_id', 'storage'))
-      ->where($query->expr()->lt('size', $query->createNamedParameter(0, IQueryBuilder::PARAM_INT)))
-      ->andWhere($query->expr()->gt('parent', $query->createNamedParameter(-1, IQueryBuilder::PARAM_INT)))
-      ->setMaxResults(1);
-
-    return $query->execute()->fetchOne();
+    parent::__construct(
+      config: $config,
+      dispatcher: $dispatcher,
+      logger: $logger,
+      connection: $connection,
+      time: $time,
+    );
   }
 
   /** {@inheritdoc} */
   public function run($argument)
   {
-    $usersScanned = 0;
-    $lastUser = '';
-    $user = $this->getUserToScan();
-    while ($user && $usersScanned < self::USERS_PER_SESSION && $lastUser !== $user) {
-      $this->runScanner($user);
-      $lastUser = $user;
-      $user = $this->getUserToScan();
-      $usersScanned += 1;
-    }
-
-    if ($lastUser === $user) {
-      $this->logger->warning("User $user still has unscanned files after running background scan, background scan might be stopped prematurely");
-    }
+    parent::run($argument);
   }
 }
