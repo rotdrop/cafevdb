3db4522b880961d149833cf566f2c9c8ec111d74
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-08-22 19:06:56 +0200

    logger.ts, use extended cafevdb class, not the vanilla toolkit class.

--- src/logger.ts
+++ src/logger.ts
@@ -4,7 +4,7 @@
  * CAFEVDB -- Camerata Academica Freiburg e.V. DataBase.
  *
  * @author Claus-Justus Heine
- * @copyright 2025 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2025, 2026 Claus-Justus Heine <himself@claus-justus-heine.de>
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -21,8 +21,9 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import Console from './toolkit/util/console.ts';
+import Console from './util/console.ts';
 
-const logger = new Console('CAFeVDB');
+const COMPONENT = 'CAFEVDB';
+const logger = new Console(COMPONENT);
 
 export default logger;
